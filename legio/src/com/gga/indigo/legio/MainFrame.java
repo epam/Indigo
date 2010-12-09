a8e841a486ff4d5e317d69f6964f7e40ce10d025
Andrey Churinov <achurinov@ggasoftware.com>
2010-12-09 23:05:32 +0300

    render-comment-margins -> render-margins

--- legio/src/com/gga/indigo/legio/MainFrame.java
+++ legio/src/com/gga/indigo/legio/MainFrame.java
@@ -34,7 +34,6 @@ public class MainFrame extends javax.swing.JFrame
       
       indigo.setOption("filename-encoding", "UTF-8");
       indigo.setOption("render-margins", "5,2");
-      indigo.setOption("render-comment-margins", "5,2");
       indigo.setOption("molfile-saving-mode", "3000");
 
       initComponents();
