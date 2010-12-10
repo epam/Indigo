cc253e741644634ee74cbfa28261e0925999b785
Andrey Churinov <achurinov@ggasoftware.com>
2010-12-10 22:22:22 +0300

    pdb file added to libs copying (for indigoDbgBreakpoint())

--- api/python/copy-libs-dbg.bat
+++ api/python/copy-libs-dbg.bat
@@ -1,6 +1,7 @@
 mkdir lib\Win\x86
 mkdir lib\Win\x64
 copy ..\dll\Win32\Debug\indigo.dll lib\Win\x86\
+copy ..\dll\Win32\Debug\indigo.pdb lib\Win\x86\
 copy ..\dll\x64\Debug\indigo.dll lib\Win\x64\
 copy ..\renderer\dll\Win32\Debug\indigo-renderer.dll lib\Win\x86\
 copy ..\renderer\dll\x64\Debug\indigo-renderer.dll lib\Win\x64\
