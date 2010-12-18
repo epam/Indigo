2f0edd8b57426b0b2a9eb71bed63fab00b40c7a3
Dmitry Pavlov <dpavlov@ggasoftware.com>
2010-12-19 02:44:24 +0300

    gen_exe batfiles updated to work on x64 systems too

--- utils/chemdiff/installer/gen_exe.bat
+++ utils/chemdiff/installer/gen_exe.bat
@@ -1,3 +1,11 @@
+@echo off
+if "%ProgramFiles(x86)%" == "" goto L1
+set MAKENSIS="%ProgramFiles(x86)%\NSIS\makensis.exe"
+goto L2
+:L1
+set MAKENSIS="%ProgramFiles%\NSIS\makensis.exe"
+:L2
+
 cd ..\
 
 echo start javaw -jar -Xss10m chemdiff.jar > dist\launch.bat
@@ -6,6 +14,6 @@ mkdir dist\lib
 copy ..\..\api\java\dist\indigo-java.jar dist\lib\
 copy ..\..\api\renderer\java\dist\indigo-renderer-java.jar dist\lib\
 
-"%ProgramFiles%\NSIS\makensis.exe" installer\chemdiff_installer.nsi
+%MAKENSIS% installer\chemdiff_installer.nsi
 
 cd installer
