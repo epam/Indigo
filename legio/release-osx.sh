8df32ba1bacd79c134b1ab1931b5c52fc6f03e78
Dmitry Pavlov <dpavlov@ggasoftware.com>
2010-12-26 15:52:42 +0300

    ChemDiff and Legio release scripts for OS X

---
+++ legio/release-osx.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+version=$1
+
+if [ -z $version ]; then
+  echo "specify version";
+  exit;
+fi
+
+name=legio-$version-osx
+
+rm -rf $name $name.zip
+mkdir -p $name/lib/Mac/10.5
+mkdir -p $name/lib/Mac/10.6
+cd ../api/
+
+for osxver in '10.5' '10.6'; do
+  cd jni
+  rm -rf build
+  xcodebuild -sdk macosx$osxver -configuration Release -alltargets
+  cp build/Release/libindigo-jni.dylib ../../legio/$name/lib/Mac/$osxver
+  cd ../renderer/jni
+  rm -rf build
+  xcodebuild -sdk macosx$osxver -configuration Release -alltargets 
+  cp build/Release/libindigo-renderer-jni.dylib ../../../legio/$name/lib/Mac/$osxver
+  cd ../../
+done
+
+cd java
+chmod +x compile.sh
+./compile.sh
+cd ../renderer/java
+chmod +x compile.sh
+./compile.sh
+cd ../../../legio/src
+
+javac -cp ../../api/java/dist/indigo-java.jar:../../api/renderer/java/dist/indigo-renderer-java.jar com/gga/indigo/legio/*.java
+jar cvfm ../legio.jar ../manifest.mf com/gga/indigo/legio/*.class
+cd ..
+
+
+cp LICENSE.GPL $name/
+cp legio.jar $name/
+cp ../api/java/dist/indigo-java.jar $name/lib/
+cp ../api/renderer/java/dist/indigo-renderer-java.jar $name/lib/
+cp legio.sh $name/legio
+cp -r tests $name/
+
+zip -r -9 $name.zip $name
