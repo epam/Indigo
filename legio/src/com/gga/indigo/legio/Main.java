d7428394bb0ac43e3e41cb47a206de4b8f034697
Mikhail Rybalkin <rybalkin@ggasoftware.com>
2010-12-28 04:27:10 +0300

    Legio main window now centered

--- legio/src/com/gga/indigo/legio/Main.java
+++ legio/src/com/gga/indigo/legio/Main.java
@@ -4,6 +4,7 @@ public class Main
 {
     public static void main(String[] args) {
        MainFrame mf = new MainFrame();
+       mf.setLocationRelativeTo(null);
        mf.setVisible(true);
     }
 }
