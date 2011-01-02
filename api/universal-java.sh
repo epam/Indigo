b784e7dcb59ffbf120fc25484477b0384a0ae464
Dmitry Pavlov <dpavlov@ggasoftware.com>
2011-01-02 04:20:58 +0300

    universal-java script updated to include Solaris libraries

--- api/universal-java.sh
+++ api/universal-java.sh
@@ -1,20 +1,29 @@
+#!/bin/bash
+
 if [ -z $1 ]; then
   echo "specify version";
   exit;
 fi
 
-wget http://ggasoftware.com/downloads/indigo-java-api-$1-windows.zip
-wget http://ggasoftware.com/downloads/indigo-java-api-$1-linux.zip
-wget http://ggasoftware.com/downloads/indigo-java-api-$1-osx.zip
+wget -c http://ggasoftware.com/downloads/indigo-java-api-$1-windows.zip
+wget -c http://ggasoftware.com/downloads/indigo-java-api-$1-linux.zip
+wget -c http://ggasoftware.com/downloads/indigo-java-api-$1-osx.zip
+wget -c http://ggasoftware.com/downloads/indigo-java-api-$1-sun.zip
+rm -rf indigo-java-api-$1-{windows,linux,osx,sun,universal}
 unzip indigo-java-api-$1-windows.zip
 unzip indigo-java-api-$1-linux.zip
 unzip indigo-java-api-$1-osx.zip
-mv indigo-java-api-$1-linux indigo-java-api-$1-universal
+unzip indigo-java-api-$1-sun.zip
+mv indigo-java-api-$1-sun indigo-java-api-$1-universal
 cp -r indigo-java-api-$1-osx/lib/* indigo-java-api-$1-universal/lib
 cp -r indigo-java-api-$1-windows/lib/* indigo-java-api-$1-universal/lib
+cp -r indigo-java-api-$1-linux/lib/* indigo-java-api-$1-universal/lib
+cp -r indigo-java-api-$1-linux/indigo-renderer-java.jar indigo-java-api-$1-universal
 zip -r -9 indigo-java-api-$1-universal.zip indigo-java-api-$1-universal
 rm indigo-java-api-$1-windows.zip
 rm indigo-java-api-$1-linux.zip
 rm indigo-java-api-$1-osx.zip
+rm indigo-java-api-$1-sun.zip
 rm -r indigo-java-api-$1-windows
 rm -r indigo-java-api-$1-osx
+rm -r indigo-java-api-$1-linux
