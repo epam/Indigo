dafc2024e17807a1c5ca677b3897b2e7198055a0
Dmitry Pavlov <dpavlov@ggasoftware.com>
2011-01-02 03:58:50 +0300

    indigo-deco and indigo-cano release scripts for Solaris

---
+++ utils/indigo-cano/all-release-sun.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+version=$1
+
+if [ -z $version ]; then
+  echo "specify version";
+  exit;
+fi
+
+cd ../../graph
+make -f Makefile.sun.32
+make -f Makefile.sun.64
+cd ../molecule
+make -f Makefile.sun.32
+make -f Makefile.sun.64
+cd ../reaction
+make -f Makefile.sun.32
+make -f Makefile.sun.64
+cd ../layout
+make -f Makefile.sun.32
+make -f Makefile.sun.64
+cd ../api
+make -f Makefile.sun.32
+make -f Makefile.sun.64
+cd ../utils/indigo-cano
+make -f Makefile.sun.32
+make -f Makefile.sun.64
+./release-unix.sh indigo-cano-$version-sun32 sparc32/indigo-cano
+./release-unix.sh indigo-cano-$version-sun64 sparc64/indigo-cano
