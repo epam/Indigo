ddd16095b1576166713065dd09d36d814038f0a4
Andrey Churinov <achurinov@ggasoftware.com>
2011-03-01 17:33:49 +0300

    CompareFinishEvent class added

--- utils/chemdiff/dll-copy.bat
+++ utils/chemdiff/dll-copy.bat
@@ -5,9 +5,13 @@ cd ..\..\api
 
 if "%ProgramFiles(x86)%" == "" goto L1
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+copy "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\redist\x64\Microsoft.VC100.CRT\msvcr100.dll" lib\Win\x64\
+copy "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\redist\x86\Microsoft.VC100.CRT\msvcr100.dll" lib\Win\x64\
 goto L2
 :L1
 call "%ProgramFiles%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+copy "%ProgramFiles%\Microsoft Visual Studio 10.0\VC\redist\x86\Microsoft.VC100.CRT\msvcr100.dll" lib\Win\x86\
+copy "%ProgramFiles%\Microsoft Visual Studio 10.0\VC\redist\x86\Microsoft.VC100.CRT\msvcr100.dll" lib\Win\x86\
 :L2
 devenv indigo-api.sln /build "ReleaseDLL|x64"
 devenv indigo-api.sln /build "ReleaseDLL|Win32"
---
+++ utils/chemdiff/src/com/ggasoftware/indigo/chemdiff/CompareFinishEvent.java
@@ -0,0 +1,7 @@
+package com.ggasoftware.indigo.chemdiff;
+
+import com.ggasoftware.indigo.gui.MolEvent;
+
+public class CompareFinishEvent extends MolEvent
+{
+}
