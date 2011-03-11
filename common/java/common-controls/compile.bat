d0a141c9518f5597bd2ad7e9085a632b23812898
Dmitry Pavlov <dpavlov@ggasoftware.com>
2011-03-11 21:13:04 +0300

    forgotten batfile

---
+++ common/java/common-controls/compile.bat
@@ -0,0 +1,7 @@
+@echo off
+mkdir dist
+cd src
+javac -cp ../../../../api/java/dist/indigo.jar;../../../../api/renderer/java/dist/indigo-renderer.jar com/ggasoftware/indigo/controls/*.java
+jar cvf ../dist/common-controls.jar com/ggasoftware/indigo/controls/*.class
+del /Q com\ggasoftware\indigo\controls\*.class
+cd ..
