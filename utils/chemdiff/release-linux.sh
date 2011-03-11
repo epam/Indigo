a3fcf6d86debcbb7096318d536348f0de95dadf7
Dmitry Pavlov <dpavlov@ggasoftware.com>
2011-03-11 21:18:37 +0300

    ChemDiff build script for Linux updated

--- utils/chemdiff/release-linux.sh
+++ utils/chemdiff/release-linux.sh
@@ -12,32 +12,32 @@ make CONF=ReleaseShared32
 make CONF=ReleaseShared64
 cd java
 ./compile.sh
+./pack-libs-linux.sh
 cd ../renderer
 make CONF=ReleaseShared32
 make CONF=ReleaseShared64
 cd java
 ./compile.sh
+./pack-libs-linux.sh
+cd ../../../common/java/common-controls
+./compile.sh
 cd ../../../utils/chemdiff/src
 
-javac -cp ../../../common/jna/jna.jar:../../../api/java/dist/indigo-java.jar:../../../api/renderer/java/dist/indigo-renderer-java.jar com/ggasoftware/indigo/chemdiff/*.java
+javac -cp ../../../common/jna/jna.jar:../../../api/java/dist/indigo.jar:../../../api/renderer/java/dist/indigo-renderer.jar:../../../common/java/common-controls/dist/common-controls.jar com/ggasoftware/indigo/chemdiff/*.java
 jar cvfm ../chemdiff.jar ../manifest.mf com/ggasoftware/indigo/chemdiff/*.class
 cd ..
 
 name=chemdiff-$version-linux
 
 rm -rf $name $name.zip
-mkdir -p $name/lib/Linux/x86
-mkdir -p $name/lib/Linux/x64
+mkdir -p $name/lib
 
 cp LICENSE.GPL $name/
 cp chemdiff.jar $name/
 cp ../../common/jna/jna.jar $name/lib/
-cp ../../api/java/dist/indigo-java.jar $name/lib/
-cp ../../api/renderer/java/dist/indigo-renderer-java.jar $name/lib/
-cp ../../api/dist/ReleaseShared32/GNU-Linux-x86/libindigo.so $name/lib/Linux/x86
-cp ../../api/dist/ReleaseShared64/GNU-Linux-x86/libindigo.so $name/lib/Linux/x64
-cp ../../api/renderer/dist/ReleaseShared32/GNU-Linux-x86/libindigo-renderer.so $name/lib/Linux/x86
-cp ../../api/renderer/dist/ReleaseShared64/GNU-Linux-x86/libindigo-renderer.so $name/lib/Linux/x64
+cp ../../api/java/dist/indigo.jar $name/lib/
+cp ../../api/renderer/java/dist/indigo-renderer.jar $name/lib/
+cp ../../common/java/common-controls/dist/common-controls.jar $name/lib/
 cp chemdiff.sh $name/chemdiff
 cp -r tests $name/
 
