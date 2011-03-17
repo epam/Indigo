7c48466e2b026e237ec40b3881dd0233b8546029
Andrey Churinov <achurinov@ggasoftware.com>
2011-03-17 22:02:27 +0300

    test code removed

--- utils/chemdiff/src/com/ggasoftware/indigo/chemdiff/Main.java
+++ utils/chemdiff/src/com/ggasoftware/indigo/chemdiff/Main.java
@@ -1,17 +1,8 @@
 package com.ggasoftware.indigo.chemdiff;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-      List<Integer> ints = new ArrayList<Integer>();
-      List<? extends Number> nums = ints; // ok
-      Integer q = 10;
-      ints.add(q);
-
-
         MainFrame.setNativeLookAndFeel();
         MainFrame mf = new MainFrame();
         mf.setLocationRelativeTo(null);
