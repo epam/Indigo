3e87b9bc9459bed8e531cdf0922dffc01df6596e
Mikhail Rybalkin <rybalkin@ggasoftware.com>
2011-03-17 14:57:05 +0300

    skip_date for reactants

--- reaction/src/rxnfile_saver.cpp
+++ reaction/src/rxnfile_saver.cpp
@@ -194,6 +194,7 @@ void RxnfileSaver::_writeMol (MolfileSaver &saver, int index) {
    saver.reactionAtomMapping = &_brxn->getAAMArray(index);
    saver.reactionAtomInversion = &_brxn->getInversionArray(index);
    saver.reactionBondReactingCenter = &_brxn->getReactingCenterArray(index);
+   saver.skip_date = skip_date;
 
    if (_qrxn != 0)
       saver.reactionAtomExactChange = &_qrxn->getExactChangeArray(index);
