6ff7b1303899c1ae5eedaa116620a00baa7f66e6
Mikhail Kozhevnikov <kozhevnikov@ggasoftware.com>
2011-03-22 01:21:32 +0100

    ignore copies of the multiple group core when checking if layout is necessary

--- render2d/src/render_params.cpp
+++ render2d/src/render_params.cpp
@@ -71,8 +71,22 @@ void RenderParams::clear ()
 
 bool RenderParamInterface::needsLayoutSub (BaseMolecule& mol)
 {
+   QS_DEF(RedBlackSet<int>, atomsToIgnore);
+   atomsToIgnore.clear();
+   for (int i = 0; i < mol.multiple_groups.size(); ++i) {
+      const Array<int>& atoms = mol.multiple_groups[i].atoms;
+      const Array<int>& patoms = mol.multiple_groups[i].parent_atoms;
+      for (int j = 0; j < atoms.size(); ++j)
+         atomsToIgnore.insert(atoms[j]);
+      for (int j = 0; j < patoms.size(); ++j)
+         atomsToIgnore.remove(patoms[j]);
+   }
    for (int i = mol.vertexBegin(); i < mol.vertexEnd(); i = mol.vertexNext(i)) {
+      if (atomsToIgnore.find(i))
+         continue;
       for (int j = mol.vertexNext(i); j < mol.vertexEnd(); j = mol.vertexNext(j)) {
+         if (atomsToIgnore.find(j))
+            continue;
          const Vec3f& v = mol.getAtomXyz(i);
          const Vec3f& w = mol.getAtomXyz(j);
          Vec3f d;
