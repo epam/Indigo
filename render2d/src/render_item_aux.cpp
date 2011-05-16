40ad96f7c1f971a85ef1a4a3f9c591de5dcf1575
Mikhail Kozhevnikov <kozhevnikov@ggasoftware.com>
2011-05-16 22:55:02 +0200

    show R-group labels for non-query molecules

--- render2d/src/render_item_aux.cpp
+++ render2d/src/render_item_aux.cpp
@@ -49,93 +49,87 @@ void RenderItemAuxiliary::_drawText ()
 
 void RenderItemAuxiliary::_drawRGroupLabel ()
 {
-   QUERY_MOL_BEGIN(mol);
+   BaseMolecule& bm = *mol;
+   MoleculeRGroups& rgs = bm.rgroups;
+   RGroup& rg = rgs.getRGroup(rLabelIdx);
+
+   TextItem tiR;
+   tiR.fontsize = FONT_SIZE_LABEL;
+   tiR.color = CWC_BASE;
+   bprintf(tiR.text, "R%d=", rLabelIdx);
+   _rc.setTextItemSize(tiR);
+   referenceY = tiR.bbsz.y / 2;
+   tiR.bbp.set(0,0);
+   _rc.drawTextItemText(tiR);
+
+   float ypos = tiR.bbp.y + tiR.bbsz.y + _settings.bondLineWidth;
+
+   if (rg.occurrence.size() > 0)
    {
-      MoleculeRGroups& rgs = qmol.rgroups;
-      RGroup& rg = rgs.getRGroup(rLabelIdx);
-
-      TextItem tiR;
-      tiR.fontsize = FONT_SIZE_LABEL;
-      tiR.color = CWC_BASE;
-      bprintf(tiR.text, "R%d=", rLabelIdx); 
-      _rc.setTextItemSize(tiR);
-      referenceY = tiR.bbsz.y / 2;
-      tiR.bbp.set(0,0);
-      _rc.drawTextItemText(tiR);
-
-      float ypos = tiR.bbp.y + tiR.bbsz.y + _settings.bondLineWidth;
-
-      if (rg.occurrence.size() > 0)
+      TextItem tiOccurrence;
+      tiOccurrence.fontsize = FONT_SIZE_RGROUP_LOGIC_INDEX;
+      tiOccurrence.color = CWC_BASE;
+      ArrayOutput output(tiOccurrence.text);
+      for (int i = 0; i < rg.occurrence.size(); ++i)
       {
-         TextItem tiOccurrence;
-         tiOccurrence.fontsize = FONT_SIZE_RGROUP_LOGIC_INDEX;
-         tiOccurrence.color = CWC_BASE;
-         ArrayOutput output(tiOccurrence.text);
-         for (int i = 0; i < rg.occurrence.size(); ++i)
-         {
-            int v = rg.occurrence[i];
-            int a = (v >> 16) & 0xFFFF;
-            int b = v & 0xFFFF;
-            if (i > 0)
-               output.printf(", ");
-            if (a == b)
-               output.printf("%d", a);
-            else if (a == 0)
-               output.printf("<%d", b+1);
-            else if (b == 0xFFFF)
-               output.printf(">%d", a-1);
-            else
-               output.printf("%d-%d", a, b);
-         }
-         output.writeByte(0);
-
-         _rc.setTextItemSize(tiOccurrence);
-         tiOccurrence.bbp.set(0, ypos);
-         _rc.drawTextItemText(tiOccurrence);
-
-         ypos += tiOccurrence.bbsz.y + _settings.bondLineWidth;
+         int v = rg.occurrence[i];
+         int a = (v >> 16) & 0xFFFF;
+         int b = v & 0xFFFF;
+         if (i > 0)
+            output.printf(", ");
+         if (a == b)
+            output.printf("%d", a);
+         else if (a == 0)
+            output.printf("<%d", b+1);
+         else if (b == 0xFFFF)
+            output.printf(">%d", a-1);
+         else
+            output.printf("%d-%d", a, b);
       }
+      output.writeByte(0);
 
-      if (rg.rest_h > 0)
-      {
-         TextItem tiRestH;
-         tiRestH.fontsize = FONT_SIZE_RGROUP_LOGIC_INDEX;
-         tiRestH.color = CWC_BASE;
-         bprintf(tiRestH.text, "RestH");
+      _rc.setTextItemSize(tiOccurrence);
+      tiOccurrence.bbp.set(0, ypos);
+      _rc.drawTextItemText(tiOccurrence);
 
-         _rc.setTextItemSize(tiRestH);
-         tiRestH.bbp.set(0, ypos);
-         _rc.drawTextItemText(tiRestH);
-      }
+      ypos += tiOccurrence.bbsz.y + _settings.bondLineWidth;
    }
-   QUERY_MOL_END;
+
+   if (rg.rest_h > 0)
+   {
+      TextItem tiRestH;
+      tiRestH.fontsize = FONT_SIZE_RGROUP_LOGIC_INDEX;
+      tiRestH.color = CWC_BASE;
+      bprintf(tiRestH.text, "RestH");
+
+      _rc.setTextItemSize(tiRestH);
+      tiRestH.bbp.set(0, ypos);
+      _rc.drawTextItemText(tiRestH);
+   }
 }
 
 void RenderItemAuxiliary::_drawRIfThen ()
 {
-   QUERY_MOL_BEGIN(mol);
+   BaseMolecule& bm = *mol;
+   MoleculeRGroups& rgs = bm.rgroups;
+
+   float ypos = 0;
+   for (int i = 1; i <= rgs.getRGroupCount(); ++i)
    {
-      MoleculeRGroups& rgs = qmol.rgroups;
-
-      float ypos = 0;
-      for (int i = 1; i <= rgs.getRGroupCount(); ++i)
+      const RGroup& rg = rgs.getRGroup(i);
+      if (rg.if_then > 0)
       {
-         const RGroup& rg = rgs.getRGroup(i);
-         if (rg.if_then > 0)
-         {
-            TextItem tiIfThen;
-            tiIfThen.fontsize = FONT_SIZE_RGROUP_LOGIC;
-            tiIfThen.color = CWC_BASE;
-            bprintf(tiIfThen.text, "IF R%d THEN R%d", i, rg.if_then); 
-            _rc.setTextItemSize(tiIfThen);
-            tiIfThen.bbp.set(0, ypos);
-            _rc.drawTextItemText(tiIfThen);
+         TextItem tiIfThen;
+         tiIfThen.fontsize = FONT_SIZE_RGROUP_LOGIC;
+         tiIfThen.color = CWC_BASE;
+         bprintf(tiIfThen.text, "IF R%d THEN R%d", i, rg.if_then); 
+         _rc.setTextItemSize(tiIfThen);
+         tiIfThen.bbp.set(0, ypos);
+         _rc.drawTextItemText(tiIfThen);
 
-            ypos += tiIfThen.bbsz.y + _settings.rGroupIfThenInterval;
-         }
+         ypos += tiIfThen.bbsz.y + _settings.rGroupIfThenInterval;
       }
    }
-   QUERY_MOL_END;
 }
 
 void RenderItemAuxiliary::_drawPlus ()
