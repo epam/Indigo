5f41611320960a7503665e5917e742f20e463827
Mikhail Rybalkin <rybalkin@ggasoftware.com>
2011-07-14 16:05:05 +0400

    Python testing for 32 bit Linux

--- api/python/symlink-libs-linux32.sh
+++ api/python/symlink-libs-linux32.sh
@@ -1,9 +1,7 @@
 mkdir -p lib/Linux/x64
 mkdir -p lib/Linux/x86
-cd lib/Linux/x32
+cd lib/Linux/x86
 ln -sf ../../../../dist/ReleaseShared32/GNU-Linux-x86/libindigo.so .
-ln -sf ../../../../renderer/dist/ReleaseShared32/GNU-Linux-x86/libindigo-renderer.so .
-cd ../x86
 ln -sf ../../../../renderer/dist/ReleaseShared32/GNU-Linux-x86/libindigo-renderer.so .
 cd ../../..
 
--- api/python/symlink-libs-linux64.sh
+++ api/python/symlink-libs-linux64.sh
@@ -3,7 +3,5 @@ mkdir -p lib/Linux/x86
 cd lib/Linux/x64
 ln -sf ../../../../dist/ReleaseShared64/GNU-Linux-x86/libindigo.so .
 ln -sf ../../../../renderer/dist/ReleaseShared64/GNU-Linux-x86/libindigo-renderer.so .
-cd ../x86
-ln -sf ../../../../renderer/dist/ReleaseShared64/GNU-Linux-x86/libindigo-renderer.so .
 cd ../../..
 
