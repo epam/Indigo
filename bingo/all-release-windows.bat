1bf015587efe1a829b6cab5b15d26b2bef22b842
Mikhail Rybalkin <rybalkin@ggasoftware.com>
2011-07-26 12:32:41 +0400

    Universal Bingo for SQL Server is now build only for 64-bit configuration, because it takes result files from 32-bit configuration. It might be better to add separate solution configuration for building Bingo for SQL Server.

--- bingo/all-release-windows.bat
+++ bingo/all-release-windows.bat
@@ -4,8 +4,8 @@ goto L2
 :L1
 call "%ProgramFiles%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 :L2
-devenv bingo.sln /build "Release|x64"
 devenv bingo.sln /build "Release|Win32"
+devenv bingo.sln /build "Release|x64"
 call release-win-32.bat bingo-oracle-%1-win32
 call release-win-64.bat bingo-oracle-%1-win64
 call release-sqlserver.bat bingo-sqlserver-%1
