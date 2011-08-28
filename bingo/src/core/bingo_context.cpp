5512d1c86a99d510294d80091740dcedf900036b
Mikhail Rybalkin <rybalkin@ggasoftware.com>
2011-08-28 13:56:57 +0400

    Stable Bingo release number has be updated

--- bingo/src/core/bingo_context.cpp
+++ bingo/src/core/bingo_context.cpp
@@ -17,7 +17,7 @@
 #include "core/bingo_error.h"
 #include "molecule/elements.h"
 
-const char * bingo_version_string = "1.6.0";
+const char * bingo_version_string = "1.6.1";
 
 TL_DEF(BingoContext, PtrArray<BingoContext>, _instances);
 
