a569f6083fd8c405acd0417cb84a3ab8f4544c86
Alexander Savelyev <asavelyev@ggasoftware.com>
2011-12-02 14:13:05 +0400

    postgresql: BINGO_PG_DIR32 and BINGO_PG_DIR64 were added for the windows build

--- bingo/postgres/all-release-windows.bat
+++ bingo/postgres/all-release-windows.bat
@@ -1,10 +1,13 @@
-if "%ProgramFiles(x86)%" == "" goto L1
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
-goto L2
-:L1
-call "%ProgramFiles%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
-:L2
+@echo off 
+
+if "%1" == "" goto ERROR_0
 
 call release-win-32.bat bingo-postgres-%1-win32
 call release-win-64.bat bingo-postgres-%1-win64
+goto EXIT_S
 
+:ERROR_0
+echo Please specify version
+goto EXIT_S
+
+:EXIT_S
--- bingo/postgres/release-win-64.bat
+++ bingo/postgres/release-win-64.bat
@@ -1,3 +1,6 @@
+@echo off
+if "%1" == "" goto ERROR_0
+
 if "%ProgramFiles(x86)%" == "" goto L1
 call "%ProgramFiles(x86)%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 goto L2
@@ -5,5 +8,25 @@ goto L2
 call "%ProgramFiles%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 :L2
 
+if "%BINGO_PG_DIR64%"=="" goto SET_DIR
+goto CALL_BUILD
+
+:SET_DIR
+if "%BINGO_PG_DIR%" == "" goto ERROR_1
+set BINGO_PG_DIR64=%BINGO_PG_DIR%
+goto CALL_BUILD
+
+:CALL_BUILD
 devenv bingo_postgres.sln /build "Release|x64"
 bingo-release.bat %1 dist\x64\bingo_postgres.dll
+goto EXIT_S
+
+:ERROR_0
+echo Please specify version
+goto EXIT_S
+
+:ERROR_1
+echo BINGO_PG_DIR is not specified
+goto EXIT_S
+
+:EXIT_S
