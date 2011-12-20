076fa5f3d7353a9cf179d56cab7eed43485f6b3a
Alexander Savelyev <asavelyev@ggasoftware.com>
2011-12-20 16:54:52 +0400

    deconvolution tests skip date

--- api/tests/tests/deco/decompose_molecules.py
+++ api/tests/tests/deco/decompose_molecules.py
@@ -3,7 +3,7 @@ sys.path.append('../../common')
 from env_indigo import *
 
 indigo = Indigo()
-
+indigo.setOption("molfile-saving-skip-date", True)
 
 def testHighlightDeco(structures):
    mols = []
