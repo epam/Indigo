bd1cad35af5ae0f2e757c9ada1fb510b255f20e3
Michael Kvyatkovskiy <mkvyatkovskiy@ggasoftware.com>
2011-12-28 17:30:24 +0400

    postgres windows build script parameters usage fixed

--- bingo/postgres/all-release-windows.bat
+++ bingo/postgres/all-release-windows.bat
@@ -3,8 +3,8 @@
 if "%1" == "" goto ERROR_0
 if "%2" == "" goto ERROR_1
 
-call release-win-32.bat bingo-postgres%2-%1-win32
-call release-win-64.bat bingo-postgres%2-%1-win64
+call release-win-32.bat bingo-postgres%2-%1-win32 %2
+call release-win-64.bat bingo-postgres%2-%1-win64 %2
 goto EXIT_S
 
 :ERROR_0
