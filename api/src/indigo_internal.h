970eb7ea10cc82a9bf002416eb63de940e5200d2
Mikhail Rybalkin <rybalkin@ggasoftware.com>
2012-01-11 15:10:12 +0400

    indigo-api: version has been changed to 1.1-beta7.1

--- api/src/indigo_internal.h
+++ api/src/indigo_internal.h
@@ -31,7 +31,7 @@
 
 /* When Indigo internal code is used dynamically this define should be
  * compared with indigoVersion() to ensure libraries binary compatibility. */
-#define INDIGO_VERSION "1.1-beta7"
+#define INDIGO_VERSION "1.1-beta7.1"
 
 using namespace indigo;
 
