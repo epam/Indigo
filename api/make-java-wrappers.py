5afc6636337013fa6d81d155f59a67e71faae623
Mikhail Rybalkin <rybalkin@ggasoftware.com>
2012-01-25 16:17:34 +0400

    indigo-build: java projects copies jar to the dist directory

--- api/make-java-wrappers.py
+++ api/make-java-wrappers.py
@@ -1,26 +1,32 @@
 import os
 import shutil
+from os.path import *
 
-root = os.getcwd()
+api_dir = abspath(dirname(__file__))
+root = join(api_dir, "..")
 
-os.chdir(os.path.join(root, "dist"))
+dist_dir = join(root, "dist")
+if not os.path.exists(dist_dir):
+    os.mkdir(dist_dir)
+
+os.chdir(dist_dir)
 if os.path.exists("java"):
     shutil.rmtree("java")
     
-os.chdir(os.path.join(root, "java"))
+os.chdir(os.path.join(api_dir, "java"))
 os.system("ant clean")
 os.system("ant jar")
 
-os.chdir(os.path.join(root, "renderer", "java"))
+os.chdir(os.path.join(api_dir, "renderer", "java"))
 os.system("ant clean")
 os.system("ant jar")
 
-os.chdir(os.path.join(root, "plugins", "inchi", "java"))
+os.chdir(os.path.join(api_dir, "plugins", "inchi", "java"))
 os.system("ant clean")
 os.system("ant jar")
 
-os.chdir(os.path.join(root, "dist"))
-shutil.copy(os.path.join(root, "LICENSE.GPL"), "java")
+os.chdir(dist_dir)
+shutil.copy(os.path.join(api_dir, "LICENSE.GPL"), "java")
 
 os.rename("java", "indigo-java-1.1-beta8")
 os.system("zip -r -9 -m indigo-java-1.1-beta8.zip indigo-java-1.1-beta8")
