b3c42b63c096950a0ef9376fca269d5a4a3746ba
Michael Kvyatkovskiy <mkvyatkovskiy@ggasoftware.com>
2012-01-30 14:04:30 +0400

    indigo-tests reorganized

--- api/tests/tests/arom/arom_d_orbital.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append('../../common')
-from env_indigo import *
-
-indigo = Indigo()
-for m in indigo.iterateSDFile(joinPath('molecules', 'mols.sdf')):
-   print(m.canonicalSmiles())
--- api/tests/tests/formats/mol_to_cml.py
+++
@@ -1,8 +0,0 @@
-
-import sys
-sys.path.append('../../common')
-from env_indigo import *
-
-indigo = Indigo()
-mol = indigo.loadMoleculeFromFile(joinPath('molecules/disconnected-0106.mol'))
-print(mol.cml())
