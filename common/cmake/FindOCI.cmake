910d4dadcc41bcde92fd367de0cd49eb19dce34f
Mikhail Kvyatkovskiy <mkvyatkovskiy@ggasoftware.com>
2012-02-07 13:36:47 +0300

    cmake: bingo-oracle build scripts in progress

---
+++ common/cmake/FindOCI.cmake
@@ -0,0 +1,26 @@
+# Locate oci
+# This module defines
+# OCI_LIBRARY_DIRS
+# OCI_INCLUDE_DIRS, where to find the headers
+#
+
+FIND_PATH(OCI_INCLUDE_DIRS oci.h
+    $ENV{ORACLE_HOME}/rdbms/public/
+)
+
+IF (WIN32)
+  SET(LIBORASDK "orasdk.lib")
+ELSEIF(UNIX)
+  SET(LIBORASDK "liboci.so")
+ELSEIF(APPLE)
+  SET(LIBORASDK "liborasdk.dylib")
+ENDIF()
+
+FIND_PATH(OCI_LIBRARY_DIRS ${LIBORASDK}
+    $ENV{ORACLE_HOME}/lib
+)
+
+SET(OCI_FOUND "NO")
+IF(OCI_LIBRARY_DIRS AND OCI_INCLUDE_DIRS)
+    SET(OCI_FOUND "YES")
+ENDIF(OCI_LIBRARY_DIRS AND OCI_INCLUDE_DIRS)
