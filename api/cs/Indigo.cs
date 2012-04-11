bc5559fbcce522fb44be99c34ca8c92cd1545231
Mikhail Kvyatkovskiy <mkvyatkovskiy@ggasoftware.com>
2012-04-11 15:46:47 +0400

    indigo ironpython testing added; rendering tests stub added

--- api/cs/Indigo.cs
+++ api/cs/Indigo.cs
@@ -452,7 +452,7 @@ namespace com.ggasoftware.indigo
          IndigoDllLoader dll_loader = IndigoDllLoader.Instance;
 
          dll_loader.loadLibrary(lib_path, "msvcr100.dll", "com.ggasoftware.indigo.Properties.Resources", false);
-         dll_loader.loadLibrary(lib_path, "zlib.dll", "com.ggasoftware.indigo.Properties.Resources", false);
+         //dll_loader.loadLibrary(lib_path, "zlib.dll", "com.ggasoftware.indigo.Properties.Resources", false);
          dll_loader.loadLibrary(lib_path, "indigo.dll", "com.ggasoftware.indigo.Properties.Resources", false);
 
          // Save instance id to check if session was allocated for this instance
