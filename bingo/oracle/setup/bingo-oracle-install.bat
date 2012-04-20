04ac25232f113f1965a8caf1026e1f7cec5670e6
Mikhail Rybalkin <rybalkin@ggasoftware.com>
2012-04-20 14:06:45 +0400

    indigo-oracle: Oracle project cleanup

---
+++ bingo/oracle/setup/bingo-oracle-install.bat
@@ -0,0 +1,165 @@
+@rem Copyright (C) 2009-2011 GGA Software Services LLC
+@rem 
+@rem This file is part of Indigo toolkit.
+@rem 
+@rem This file may be distributed and/or modified under the terms of the
+@rem GNU General Public License version 3 as published by the Free Software
+@rem Foundation and appearing in the file LICENSE.GPL included in the
+@rem packaging of this file.
+@rem 
+@rem This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+@rem WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+@echo off
+set libdir=%ORACLE_HOME%\bin
+set dbaname=system
+set dbapass=
+set instance=
+set bingoname=bingo
+set bingopass=bingo
+set y=
+
+goto L2
+
+:L1
+shift
+
+:L2
+if "%1" == "" goto L3
+if "%1" == "-libdir" goto got_libdir
+if "%1" == "-dbaname" goto got_dbaname
+if "%1" == "-dbapass" goto got_dbapass
+if "%1" == "-instance" goto got_instance
+if "%1" == "-bingoname" goto got_bingoname
+if "%1" == "-bingopass" goto got_bingopass
+if "%1" == "-y" goto got_y
+
+if "%1" == "-help" goto usage
+if "%1" == "-?" goto usage
+if "%1" == "/?" goto usage
+goto badparam
+
+:got_libdir
+  shift
+  set libdir=%1
+  goto L1
+
+:got_dbaname
+  shift
+  set dbaname=%1
+  goto L1
+
+:got_dbapass
+  shift
+  set dbapass=%1
+  goto L1
+
+:got_instance
+  shift
+  set instance=%1
+  goto L1
+
+:got_bingoname
+  shift
+  set bingoname=%1
+  goto L1
+
+:got_bingopass
+  shift
+  set bingopass=%1
+  goto L1
+
+:got_y
+  set y=1
+  goto L1
+  
+:badparam
+  echo Unknown parameter: %1
+  goto end
+
+:L3
+
+echo Target directory  : %libdir%
+echo DBA name          : %dbaname%
+if not "%dbapass%" == "" echo DBA password      : %DBApass%
+if "%instance%"=="" echo Oracle instance   : [default]
+if not "%instance%"=="" echo Oracle instance   : %instance%
+echo Bingo name        : %BINGOname%
+echo Bingo password    : %BINGOpass%
+
+if "%y%"=="1" goto L4
+
+set /p proceed=Proceed (y/N)? 
+
+if "%proceed%"=="y" goto L4
+if "%proceed%"=="Y" goto L4
+
+echo Aborting
+goto end
+
+:L4
+
+echo set verify off >sql\bingo\bingo_lib.sql 
+echo spool bingo_lib; >>sql\bingo\bingo_lib.sql 
+echo create or replace LIBRARY bingolib AS '%libdir%\bingo-oracle.dll' >>sql\bingo\bingo_lib.sql 
+echo / >>sql\bingo\bingo_lib.sql 
+echo spool off; >>sql\bingo\bingo_lib.sql 
+
+md %libdir%
+copy lib\bingo-oracle.dll %libdir% /y
+if not %errorlevel%==0 goto end
+
+if not "%instance%"=="" set instance=@%instance%
+
+cd sql\system
+if "%dbapass%"=="" goto emptypass
+sqlplus %dbaname%/%dbapass%%instance% @bingo_init.sql %bingoname% %bingopass%
+goto L5
+:emptypass
+sqlplus %dbaname%%instance% @bingo_init.sql %bingoname% %bingopass%
+
+:L5
+cd ..\bingo
+sqlplus %bingoname%/%bingopass%%instance% @makebingo.sql
+sqlplus %bingoname%/%bingopass%%instance% @bingo_config.sql
+
+cd ..
+sqlplus %bingoname%/%bingopass%%instance% @dbcheck.sql
+cd ..
+goto end
+
+:usage
+echo Usage: bingo-oracle-install.bat [parameters]
+echo Parameters:
+echo   -?, -help
+echo     Print this help message
+echo   -libdir path
+echo     Target directory to install bingo-oracle.dll (defaut %%ORACLE_HOME%%\bin).
+echo     If the directory does not exist, it will be created.
+echo   -dbaname name
+echo     Database administrator login (default "system").
+echo   -dbapass password
+echo     Database administrator password (no default).
+echo     If the password is not specified, you will have to enter it later.
+echo   -instance instance
+echo     Database instance (default instance by default).
+echo     You can specify full address like "server:1521/instance" as well.
+echo   -bingoname name
+echo     Name of cartridge pseudo-user (default "bingo").
+echo   -bingopass password
+echo     Password of the pseudo-user (default "bingo").
+echo   -y
+echo     Do not ask for confirmation.
+goto end
+
+
+:end
+
+set libdir=
+set dbaname=
+set dbapass=
+set instance=
+set bingoname=
+set bingopass=
+set y=
+set initsql=
---
+++ bingo/oracle/setup/bingo-oracle-install.sh
@@ -0,0 +1,147 @@
+#!/bin/sh
+# Copyright (C) 2009-2011 GGA Software Services LLC
+# 
+# This file is part of Indigo toolkit.
+# 
+# This file may be distributed and/or modified under the terms of the
+# GNU General Public License version 3 as published by the Free Software
+# Foundation and appearing in the file LICENSE.GPL included in the
+# packaging of this file.
+# 
+# This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+# WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+libdir=$ORACLE_HOME/lib
+dbaname="system"
+dbapass=
+instance=
+bingoname="bingo"
+bingopass="bingo"
+y=
+
+usage ()
+{
+echo 'Usage: bingo-oracle-install.sh [parameters]'
+echo 'Parameters:'
+echo '  -?, -help'
+echo '    Print this help message'
+echo '  -libdir path'
+echo '    Target directory to install libbingo-oracle'$libext' (defaut $ORACLE_HOME/lib).'
+echo '    If the directory does not exist, it will be created.'
+echo '  -dbaname name'
+echo '    Database administrator login (default "system").'
+echo '  -dbapass password'
+echo '    Database administrator password (no default).'
+echo '    If the password is not specified, you will have to enter it later.'
+echo '  -instance instance'
+echo '    Database instance (default instance by default).'
+echo '    You can specify full address like "server:1521/instance" as well.'
+echo '  -bingoname name'
+echo '    Name of cartridge pseudo-user (default "bingo").'
+echo '  -bingopass password'
+echo '    Password of the pseudo-user (default "bingo").'
+echo '  -y'
+echo '    Do not ask for confirmation.'
+}
+
+libext=".so"
+if [ -f "lib/libbingo-oracle.dylib" ]; then
+  libext=".dylib"
+fi
+
+while [ "$#" != 0 ]; do
+  case "$1" in
+     -help | '-?' | '/?')
+        usage
+        exit 0
+        ;;
+     -libdir)
+        shift
+        libdir=$1
+        ;;
+	   -dbaname)
+        shift
+        dbaname=$1
+        ;;
+     -dbapass)
+        shift
+        dbapass=$1
+        ;;
+     -instance)
+        shift
+        instance=$1
+        ;;
+     -bingoname)
+        shift
+        bingoname=$1
+        ;;
+     -bingopass)
+        shift
+        bingopass=$1
+        ;;
+     -y)
+        y=1
+        ;;
+     *)
+        echo "Unknown parameter: $1";
+        usage;
+        exit -1
+  esac
+  shift
+done
+
+echo "Target directory  : $libdir";
+echo "DBA name          : $dbaname";
+if [ ! "$dbapass" = "" ]; then
+  echo "DBA password      : $dbapass";
+fi
+if [ ! "$instance" = "" ]; then
+  echo "Oracle instance   : $instance";
+else
+  echo "Oracle instance   : <default>";
+fi
+echo "Bingo name        : $bingoname";
+echo "Bingo password    : $bingopass";
+
+if [ "$y" != "1" ]; then
+  echo "Proceed (y/N)?"
+  read proceed
+
+  if [ "$proceed" != "y" ] && [ "$proceed" != "Y" ]; then
+    echo 'Aborting';
+    exit 0;
+  fi
+fi
+
+if [ ! "$instance" = "" ]; then
+  instance=@$instance
+fi
+
+mkdir -p $libdir
+
+echo set verify off >sql/bingo/bingo_lib.sql 
+echo spool bingo_lib\; >>sql/bingo/bingo_lib.sql 
+echo create or replace LIBRARY bingolib AS \'$libdir/libbingo-oracle$libext\' >>sql/bingo/bingo_lib.sql 
+echo / >>sql/bingo/bingo_lib.sql 
+echo spool off\; >>sql/bingo/bingo_lib.sql 
+
+cp lib/libbingo-oracle$libext $libdir
+if [ $? != 0 ]; then
+  echo 'Cannot copy libbingo-oracle'$libext' to '$libdir
+  exit
+fi
+
+cd sql/system
+if [ "$dbapass" = "" ]; then
+  sqlplus $dbaname$instance @bingo_init.sql $bingoname $bingopass
+else
+  sqlplus $dbaname/$dbapass$instance @bingo_init.sql $bingoname $bingopass
+fi
+
+cd ../bingo
+sqlplus $bingoname/$bingopass$instance @makebingo.sql
+sqlplus $bingoname/$bingopass$instance @bingo_config.sql
+cd ..
+sqlplus $bingoname/$bingopass$instance @dbcheck.sql
+cd ..
+
