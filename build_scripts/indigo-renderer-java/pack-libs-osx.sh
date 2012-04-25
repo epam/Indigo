fb0c46c302288264166ab189575c745ffbf1dbf8
Mikhail Rybalkin <rybalkin@ggasoftware.com>
2012-04-25 14:59:59 +0400

    unnecessary files are removed from build_scripts

--- build_scripts/indigo-renderer-java/pack-libs-osx.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p com/ggasoftware/indigo/Mac/10.5
-mkdir -p com/ggasoftware/indigo/Mac/10.6
-cp ../indigo-renderer/dist/Mac/10.5/lib/Release/libindigo-renderer.dylib com/ggasoftware/indigo/Mac/10.5/
-cp ../indigo-renderer/dist/Mac/10.6/lib/Release/libindigo-renderer.dylib com/ggasoftware/indigo/Mac/10.6/
-jar uf dist/indigo-renderer.jar com/ggasoftware/indigo/Mac/10.5/libindigo-renderer.dylib
-jar uf dist/indigo-renderer.jar com/ggasoftware/indigo/Mac/10.6/libindigo-renderer.dylib
-rm -rf com
-
