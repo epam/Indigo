77b65e4ddcbf71d2f5440bc256c035cda9ea39d7
Alexander Savelyev <asavelyev@ggasoftware.com>
2012-11-02 17:48:44 +0400

    bingo-postgres: cost estimation was updated for the 9.2 support

--- bingo/postgres/src/pg_am/pg_bingo_costestimate.c
+++ bingo/postgres/src/pg_am/pg_bingo_costestimate.c
@@ -317,6 +317,234 @@ genericcostestimate(PlannerInfo *root,
 	*indexCorrelation = -1.0;
 }
 
+static void
+genericcostestimate92(PlannerInfo *root,
+					IndexPath *path,
+					double loop_count,
+					double numIndexTuples,
+					Cost *indexStartupCost,
+					Cost *indexTotalCost,
+					Selectivity *indexSelectivity,
+					double *indexCorrelation)
+{
+	IndexOptInfo *index = path->indexinfo;
+	List	   *indexQuals = path->indexquals;
+	List	   *indexOrderBys = path->indexorderbys;
+	double		numIndexPages;
+	double		num_sa_scans;
+	double		num_outer_scans;
+	double		num_scans;
+	QualCost	index_qual_cost;
+	double		qual_op_cost;
+	double		qual_arg_cost;
+	double		spc_random_page_cost;
+	List	   *selectivityQuals;
+	ListCell   *l;
+
+	/*
+	 * If the index is partial, AND the index predicate with the explicitly
+	 * given indexquals to produce a more accurate idea of the index
+	 * selectivity.
+	 */
+/*
+	selectivityQuals = add_predicate_to_quals(index, indexQuals);
+*/
+
+	/*
+	 * Check for ScalarArrayOpExpr index quals, and estimate the number of
+	 * index scans that will be performed.
+	 */
+	num_sa_scans = 1;
+	foreach(l, indexQuals)
+	{
+		RestrictInfo *rinfo = (RestrictInfo *) lfirst(l);
+
+		if (IsA(rinfo->clause, ScalarArrayOpExpr))
+		{
+			ScalarArrayOpExpr *saop = (ScalarArrayOpExpr *) rinfo->clause;
+			int			alength = estimate_array_length(lsecond(saop->args));
+
+			if (alength > 1)
+				num_sa_scans *= alength;
+		}
+	}
+
+	/* Estimate the fraction of main-table tuples that will be visited */
+	*indexSelectivity = clauselist_selectivity(root, selectivityQuals,
+											   index->rel->relid,
+											   JOIN_INNER,
+											   NULL);
+
+	/*
+	 * If caller didn't give us an estimate, estimate the number of index
+	 * tuples that will be visited.  We do it in this rather peculiar-looking
+	 * way in order to get the right answer for partial indexes.
+	 */
+	if (numIndexTuples <= 0.0)
+	{
+		numIndexTuples = *indexSelectivity * index->rel->tuples;
+
+		/*
+		 * The above calculation counts all the tuples visited across all
+		 * scans induced by ScalarArrayOpExpr nodes.  We want to consider the
+		 * average per-indexscan number, so adjust.  This is a handy place to
+		 * round to integer, too.  (If caller supplied tuple estimate, it's
+		 * responsible for handling these considerations.)
+		 */
+		numIndexTuples = rint(numIndexTuples / num_sa_scans);
+	}
+   numIndexTuples = 1.0;
+
+	/*
+	 * We can bound the number of tuples by the index size in any case. Also,
+	 * always estimate at least one tuple is touched, even when
+	 * indexSelectivity estimate is tiny.
+	 */
+	if (numIndexTuples > index->tuples)
+		numIndexTuples = index->tuples;
+	if (numIndexTuples < 1.0)
+		numIndexTuples = 1.0;
+
+	/*
+	 * Estimate the number of index pages that will be retrieved.
+	 *
+	 * We use the simplistic method of taking a pro-rata fraction of the total
+	 * number of index pages.  In effect, this counts only leaf pages and not
+	 * any overhead such as index metapage or upper tree levels. In practice
+	 * this seems a better approximation than charging for access to the upper
+	 * levels, perhaps because those tend to stay in cache under load.
+	 */
+/*
+	if (index->pages > 1 && index->tuples > 1)
+		numIndexPages = ceil(numIndexTuples * index->pages / index->tuples);
+	else
+*/
+		numIndexPages = 1.0;
+
+	/* fetch estimated page cost for schema containing index */
+	get_tablespace_page_costs(index->reltablespace,
+							  &spc_random_page_cost,
+							  NULL);
+
+	/*
+	 * Now compute the disk access costs.
+	 *
+	 * The above calculations are all per-index-scan.  However, if we are in a
+	 * nestloop inner scan, we can expect the scan to be repeated (with
+	 * different search keys) for each row of the outer relation.  Likewise,
+	 * ScalarArrayOpExpr quals result in multiple index scans.	This creates
+	 * the potential for cache effects to reduce the number of disk page
+	 * fetches needed.	We want to estimate the average per-scan I/O cost in
+	 * the presence of caching.
+	 *
+	 * We use the Mackert-Lohman formula (see costsize.c for details) to
+	 * estimate the total number of page fetches that occur.  While this
+	 * wasn't what it was designed for, it seems a reasonable model anyway.
+	 * Note that we are counting pages not tuples anymore, so we take N = T =
+	 * index size, as if there were one "tuple" per page.
+	 */
+	num_outer_scans = loop_count;
+	num_scans = num_sa_scans * num_outer_scans;
+
+	if (num_scans > 1)
+	{
+		double		pages_fetched;
+
+		/* total page fetches ignoring cache effects */
+		pages_fetched = numIndexPages * num_scans;
+
+		/* use Mackert and Lohman formula to adjust for cache effects */
+		pages_fetched = index_pages_fetched(pages_fetched,
+											index->pages,
+											(double) index->pages,
+											root);
+
+		/*
+		 * Now compute the total disk access cost, and then report a pro-rated
+		 * share for each outer scan.  (Don't pro-rate for ScalarArrayOpExpr,
+		 * since that's internal to the indexscan.)
+		 */
+		*indexTotalCost = (pages_fetched * spc_random_page_cost)
+			/ num_outer_scans;
+	}
+	else
+	{
+		/*
+		 * For a single index scan, we just charge spc_random_page_cost per
+		 * page touched.
+		 */
+		*indexTotalCost = numIndexPages * spc_random_page_cost;
+	}
+
+	/*
+	 * A difficulty with the leaf-pages-only cost approach is that for small
+	 * selectivities (eg, single index tuple fetched) all indexes will look
+	 * equally attractive because we will estimate exactly 1 leaf page to be
+	 * fetched.  All else being equal, we should prefer physically smaller
+	 * indexes over larger ones.  (An index might be smaller because it is
+	 * partial or because it contains fewer columns; presumably the other
+	 * columns in the larger index aren't useful to the query, or the larger
+	 * index would have better selectivity.)
+	 *
+	 * We can deal with this by adding a very small "fudge factor" that
+	 * depends on the index size.  The fudge factor used here is one
+	 * spc_random_page_cost per 10000 index pages, which should be small
+	 * enough to not alter index-vs-seqscan decisions, but will prevent
+	 * indexes of different sizes from looking exactly equally attractive.
+	 */
+	*indexTotalCost += index->pages * spc_random_page_cost / 100000.0;
+
+	/*
+	 * CPU cost: any complex expressions in the indexquals will need to be
+	 * evaluated once at the start of the scan to reduce them to runtime keys
+	 * to pass to the index AM (see nodeIndexscan.c).  We model the per-tuple
+	 * CPU costs as cpu_index_tuple_cost plus one cpu_operator_cost per
+	 * indexqual operator.	Because we have numIndexTuples as a per-scan
+	 * number, we have to multiply by num_sa_scans to get the correct result
+	 * for ScalarArrayOpExpr cases.  Similarly add in costs for any index
+	 * ORDER BY expressions.
+	 *
+	 * Note: this neglects the possible costs of rechecking lossy operators
+	 * and OR-clause expressions.  Detecting that that might be needed seems
+	 * more expensive than it's worth, though, considering all the other
+	 * inaccuracies here ...
+	 */
+	cost_qual_eval(&index_qual_cost, indexQuals, root);
+	qual_arg_cost = index_qual_cost.startup + index_qual_cost.per_tuple;
+	cost_qual_eval(&index_qual_cost, indexOrderBys, root);
+	qual_arg_cost += index_qual_cost.startup + index_qual_cost.per_tuple;
+	qual_op_cost = cpu_operator_cost *
+		(list_length(indexQuals) + list_length(indexOrderBys));
+	qual_arg_cost -= qual_op_cost;
+	if (qual_arg_cost < 0)		/* just in case... */
+		qual_arg_cost = 0;
+
+	*indexStartupCost = qual_arg_cost;
+	*indexTotalCost += qual_arg_cost;
+	*indexTotalCost += numIndexTuples * num_sa_scans * (cpu_index_tuple_cost + qual_op_cost);
+
+	/*
+	 * We also add a CPU-cost component to represent the general costs of
+	 * starting an indexscan, such as analysis of btree index keys and initial
+	 * tree descent.  This is estimated at 100x cpu_operator_cost, which is a
+	 * bit arbitrary but seems the right order of magnitude. (As noted above,
+	 * we don't charge any I/O for touching upper tree levels, but charging
+	 * nothing at all has been found too optimistic.)
+	 *
+	 * Although this is startup cost with respect to any one scan, we add it
+	 * to the "total" cost component because it's only very interesting in the
+	 * many-ScalarArrayOpExpr-scan case, and there it will be paid over the
+	 * life of the scan node.
+	 */
+	*indexTotalCost += num_sa_scans * 100.0 * cpu_operator_cost;
+   *indexTotalCost = 1.0;
+
+	/*
+	 * Generic assumption about index correlation: there isn't any.
+	 */
+	*indexCorrelation = -1.0;
+}
+
 
 PG_FUNCTION_INFO_V1(bingo_costestimate);
 PGDLLEXPORT Datum bingo_costestimate(PG_FUNCTION_ARGS);
@@ -324,6 +552,19 @@ PGDLLEXPORT Datum bingo_costestimate(PG_FUNCTION_ARGS);
 Datum
 bingo_costestimate(PG_FUNCTION_ARGS) {
    
+#if PG_VERSION_NUM / 100 == 902
+   PlannerInfo *root = (PlannerInfo *) PG_GETARG_POINTER(0);
+	IndexPath  *path = (IndexPath *) PG_GETARG_POINTER(1);
+	double		loop_count = PG_GETARG_FLOAT8(2);
+	Cost	   *indexStartupCost = (Cost *) PG_GETARG_POINTER(3);
+	Cost	   *indexTotalCost = (Cost *) PG_GETARG_POINTER(4);
+	Selectivity *indexSelectivity = (Selectivity *) PG_GETARG_POINTER(5);
+	double	   *indexCorrelation = (double *) PG_GETARG_POINTER(6);
+
+	genericcostestimate92(root, path, loop_count, 1.0,
+						indexStartupCost, indexTotalCost,
+						indexSelectivity, indexCorrelation);
+#else
 
    struct PlannerInfo *root;
    struct IndexOptInfo *index;
@@ -347,9 +588,11 @@ bingo_costestimate(PG_FUNCTION_ARGS) {
    elog(INFO, "bingo cost estimate %f", root->);
 */
 
+
    genericcostestimate(root, index, indexQuals, outer_rel, 1.0,
    					indexStartupCost, indexTotalCost,
    					indexSelectivity, indexCorrelation);
+#endif
 /*
    elog(INFO, "start up cost %f", *indexStartupCost);
    elog(INFO, "start total cost %f", *indexTotalCost);
