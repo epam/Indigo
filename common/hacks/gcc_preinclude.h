dfa5f8f8e7343c7d6c3a4790b7bd40a402995fdb
Mikhail Kvyatkovskiy <mkvyatkovskiy@ggasoftware.com>
2013-05-31 18:14:39 +0400

    indigo-api: problem with memcpy in GLIBC >= 2.14 fixed

---
+++ common/hacks/gcc_preinclude.h
@@ -0,0 +1,25 @@
+/****************************************************************************
+ * Copyright (C) 2009-2013 GGA Software Services LLC
+ *
+ * This file is part of Indigo toolkit.
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 3 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ ***************************************************************************/
+
+/* Old
+/* http://rjpower.org/wordpress/fun-with-shared-libraries-version-glibc_2-14-not-found/ */
+
+#ifndef  __gcc_preinclude_h__
+#define __gcc_preinclude_h__
+
+#if defined(__GLIBC__) && defined(__x86_64__)
+__asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
+#endif
+
+#endif
