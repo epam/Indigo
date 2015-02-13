852a7ac3f068c68afeaf8754e98792b13c618e71
Andrei Churinov <Andrei_Churinov@epam.com>
2015-02-13 20:40:26 +0300

    rindigo: checkSub() buf fixed + some arguments validity checks were added

--- api/r/R/indigo.R
+++ api/r/R/indigo.R
@@ -3,21 +3,40 @@ version <- function() {
 }
 
 canonicalSmiles <- function(data) {
-    .Call("canonicalSmiles", data)
+    stopifnot(is.character(data))
+    stopifnot(length(data) == 1)
+
+    return(.Call("canonicalSmiles", data))
 }
 
 checkSub <- function(query, target, mode) {
-    (.Call("checkSub", query, target, mode) != 0)
+    stopifnot(is.character(query), is.character(target), is.character(mode))
+    stopifnot(length(query) == 1, length(target) == 1, length(mode) == 1)
+    
+    result <- .Call("checkSub", query, target, mode)
+    if (result == -1)
+       warning(paste("checkSub(\"", query, "\", \"", target, "\", \"", mode, "\") was failed", sep = "", collapse = NULL))
+
+    return(result > 0)
 }
 
 fingerprint <- function(item, mode) {
-    .Call("fingerprint", item, mode)
+    stopifnot(is.character(item), is.character(mode))
+    stopifnot(length(item) == 1, length(mode) == 1)
+
+    return(.Call("fingerprint", item, mode))
 }
 
 fingerprintQuery <- function(item, mode) {
-    .Call("fingerprintQuery", item, mode)
+    stopifnot(is.character(item), is.character(mode))
+    stopifnot(length(item) == 1, length(mode) == 1)
+
+    return(.Call("fingerprintQuery", item, mode))
 }
 
 molecularWeight <- function(item) {
-    .Call("molecularWeight", item)
+    stopifnot(is.character(item))
+    stopifnot(length(item) == 1)
+
+    return(.Call("molecularWeight", item))
 }
--- api/r/src/rindigo.cpp
+++ api/r/src/rindigo.cpp
@@ -24,6 +24,7 @@ extern "C"
    SEXP canonicalSmiles(SEXP data)
    {
       SEXP result = PROTECT(allocVector(STRSXP, 1));
+      
       int m = indigoLoadMoleculeFromString(CHAR(STRING_ELT(data, 0)));
       SET_STRING_ELT(result, 0, mkChar(indigoCanonicalSmiles(m)));
       indigoFree(m);
@@ -37,39 +38,69 @@ extern "C"
       int t = indigoLoadMoleculeFromString(CHAR(STRING_ELT(target, 0)));
       int matcher = indigoSubstructureMatcher(t, CHAR(STRING_ELT(mode, 0)));
       SEXP smatch;
-      int* match;
       PROTECT( smatch = NEW_INTEGER(1)) ;
-      match = INTEGER(smatch);
-      match[0] = indigoMatch(matcher, q);
+      
+      int match_res = indigoMatch(matcher, q);
+      
+      INTEGER(smatch)[0] = match_res;
+      
       indigoFree(q);
       indigoFree(t);
       indigoFree(matcher);
-      indigoFree(match[0]);
+      indigoFree(match_res);
+      
       UNPROTECT(1);
       return smatch;
+   }
+   
+   const char * _makeOnesListStr(int mol, const char *mode)
+   {
+      if (mol == -1)
+         return NULL;
+     
+      int fp = indigoFingerprint(mol, mode);
+      if (fp == -1)
+         return NULL;    
+      
+      const char *ones_list = indigoOneBitsList(fp);
+      
+      indigoFree(fp);
+      return ones_list;
    }
 
    SEXP fingerprint(SEXP item, SEXP mode)
    {
       SEXP result = PROTECT(allocVector(STRSXP, 1));
+      SET_STRING_ELT(result, 0, mkChar(""));
+      
       int i = indigoLoadMoleculeFromString(CHAR(STRING_ELT(item, 0)));
-      int fp = indigoFingerprint(i, CHAR(STRING_ELT(mode, 0)));
-      SET_STRING_ELT(result, 0, mkChar(indigoOneBitsList(fp)));
-      indigoFree(i);
-      indigoFree(fp);
+
+      const char *ones_list =  _makeOnesListStr(i, CHAR(STRING_ELT(mode, 0)));
+
+      if (ones_list != NULL)
+         SET_STRING_ELT(result, 0, mkChar(ones_list));
+               
+      indigoFree(i);  
       UNPROTECT(1);
+      
       return result;
    }
    
    SEXP fingerprintQuery(SEXP item, SEXP mode)
    {
       SEXP result = PROTECT(allocVector(STRSXP, 1));
+      SET_STRING_ELT(result, 0, mkChar(""));
+      
       int i = indigoLoadQueryMoleculeFromString(CHAR(STRING_ELT(item, 0)));
-      int fp = indigoFingerprint(i, CHAR(STRING_ELT(mode, 0)));
-      SET_STRING_ELT(result, 0, mkChar(indigoOneBitsList(fp)));
-      indigoFree(i);
-      indigoFree(fp);
+
+      const char *ones_list =  _makeOnesListStr(i, CHAR(STRING_ELT(mode, 0)));
+
+      if (ones_list != NULL)
+         SET_STRING_ELT(result, 0, mkChar(ones_list));
+               
+      indigoFree(i);  
       UNPROTECT(1);
+      
       return result;
    }
 
