b54ba84ad74910bb86bcdecb123b2e616f453714
Dmitry Kuzminov <dkuzminov@ggasoftware.com>
2015-03-16 17:45:41 +0300

    indigo-core: Tautomer enumeration, lost files: inchi_wrapper, inchi_parser added

---
+++ molecule/inchi_parser.h
@@ -0,0 +1,57 @@
+/****************************************************************************
+* Copyright (C) 2015 GGA Software Services LLC
+*
+* This file is part of Indigo toolkit.
+*
+* This file may be distributed and/or modified under the terms of the
+* GNU General Public License version 3 as published by the Free Software
+* Foundation and appearing in the file LICENSE.GPL included in the
+* packaging of this file.
+*
+* This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+* WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+***************************************************************************/
+
+#ifndef __inchi_parser_h__
+#define __inchi_parser_h__
+
+#include "base_cpp/array.h"
+
+namespace indigo {
+   // Molecule InChI code constructor class 
+   class InChICodeParser
+   {
+   public:
+      enum
+      {
+         STATIC = 1,
+         MOBILE = 2
+      };
+
+      explicit InChICodeParser(const char *inchi_code);
+
+      int staticHydrogenPositionBegin()         { return _nextElement(STATIC, -1); }
+      int staticHydrogenPositionNext(int index) { return _nextElement(STATIC, index); }
+      int staticHydrogenPositionEnd()           { return _hydrogens.size(); }
+
+      int mobileHydrogenPositionBegin()         { return _nextElement(MOBILE, -1); }
+      int mobileHydrogenPositionNext(int index) { return _nextElement(MOBILE, index); }
+      int mobileHydrogenPositionEnd()           { return _hydrogens.size(); }
+
+      int mobileHydrogenCount() const   { return _mobileCount; }
+
+      int getHydrogen(int index)  { return _hydrogens.at(index); }
+
+      DECL_ERROR;
+
+   private:
+      Array<int> _hydrogens;
+      Array<int> _types;
+      int _mobileCount;
+
+      int _nextElement(int type, int index);
+   };
+
+}
+
+#endif // __inchi_parser_h__
