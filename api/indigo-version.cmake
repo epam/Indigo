7a932e8ea1593d04d30610de15f1e4b991f3f733
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2016-02-17 21:26:46 +0300

    indigo-build: auto version

--- api/indigo-version.cmake
+++ api/indigo-version.cmake
@@ -1,5 +1,11 @@
-SET(INDIGO_VERSION "1.2.2beta-r37")
-# Do not forget to launch build_scripts/indigo-update-version.py after changing the version because it should be changed in the Java and .NET files as well
+find_package(Git)
+if(GIT_EXECUTABLE) 
+    EXEC_PROGRAM(${GIT_EXECUTABLE} ARGS describe --long --tags --match "indigo*" OUTPUT_VARIABLE INDIGO_VERSION)
+else()
+	SET(INDIGO_VERSION "1.2.2beta-r37")
+endif()
+message(status ${INDIGO_VERSION})
+# Do not forget to launch build_scripts/indigo-update-version.py after changing the version because it should be ${RV} changed in the Java and .NET files as well
 
 IF($ENV{BUILD_NUMBER})
    SET(INDIGO_BUILD_VERSION $ENV{BUILD_NUMBER})
