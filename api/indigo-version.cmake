32eeb77c20ac96947cad5819a00830d379fa2ada
Alexander Savelyev <aleksandr_savelev@epam.com>
2016-02-25 15:48:32 +0300

    indigo-build: fix auto version for #17. Use execute_process, use regex-replace.

--- api/indigo-version.cmake
+++ api/indigo-version.cmake
@@ -1,10 +1,17 @@
 find_package(Git)
 if(GIT_EXECUTABLE) 
-    EXEC_PROGRAM(${GIT_EXECUTABLE} ARGS describe --long --tags --match "indigo*" OUTPUT_VARIABLE INDIGO_VERSION)
+   EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} describe --long --tags --match "indigo-*" 
+                    OUTPUT_VARIABLE INDIGO_FULL_VERSION
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+   string(REGEX REPLACE "indigo-(.+)-(.+)-(.+)" "\\1-r\\2-\\3" INDIGO_FULL_VERSION ${INDIGO_FULL_VERSION})
+   string(REGEX REPLACE "(.+)-(.+)-(.+)" "\\1-\\2" INDIGO_VERSION ${INDIGO_FULL_VERSION})
 else()
 	SET(INDIGO_VERSION "1.2.2beta-r37")
+	SET(INDIGO_FULL_VERSION ${INDIGO_VERSION})
 endif()
-message(status ${INDIGO_VERSION})
+
+message(STATUS "Indigo full version: " ${INDIGO_FULL_VERSION})
+
 # Do not forget to launch build_scripts/indigo-update-version.py after changing the version because it should be ${RV} changed in the Java and .NET files as well
 
 IF($ENV{BUILD_NUMBER})
@@ -13,4 +20,4 @@ ELSE()
    SET(INDIGO_BUILD_VERSION 0)
 ENDIF()
 
-SET(INDIGO_VERSION_EXT "${INDIGO_VERSION}.${INDIGO_BUILD_VERSION} ${PACKAGE_SUFFIX}")
+SET(INDIGO_VERSION_EXT "${INDIGO_FULL_VERSION} ${PACKAGE_SUFFIX}")
