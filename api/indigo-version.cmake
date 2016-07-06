a715954540d462bf3a82afc6d8b7427123238b2e
taras <komp-w@yandex.ru>
2016-07-06 13:52:59 +0300

    fix bug build indigo project for ios

--- api/indigo-version.cmake
+++ api/indigo-version.cmake
@@ -1,17 +1,3 @@
-find_package(Git)
-if(GIT_EXECUTABLE) 
-   EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} describe --long --tags --match "indigo-*" 
-                    OUTPUT_VARIABLE INDIGO_FULL_VERSION
-                    OUTPUT_STRIP_TRAILING_WHITESPACE)
-   string(REGEX REPLACE "indigo-(.+)-(.+)-(.+)" "\\1.r\\2-\\3" INDIGO_FULL_VERSION ${INDIGO_FULL_VERSION})
-   string(REGEX REPLACE "(.+)-(.+)" "\\1" INDIGO_VERSION ${INDIGO_FULL_VERSION})
-else()
-	SET(INDIGO_VERSION "1.2.2beta.r0")
-	SET(INDIGO_FULL_VERSION "1.2.2beta.r0-31cdd494")
-endif()
-
-message(STATUS "Indigo full version: " ${INDIGO_FULL_VERSION})
-
 # Do not forget to launch build_scripts/indigo-update-version.py after changing the version because it should be ${RV} changed in the Java and .NET files as well
 
 IF($ENV{BUILD_NUMBER})
