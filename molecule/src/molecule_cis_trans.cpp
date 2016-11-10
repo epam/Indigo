f0725ebb802d5978d88cc7e917ed6569a7786a06
Iurii Puzanov <Iurii_Puzanov@epam.com>
2016-11-10 13:35:49 +0300

    indigo-core: fixed cis-trans issue for ignored bonds

--- molecule/src/molecule_cis_trans.cpp
+++ molecule/src/molecule_cis_trans.cpp
@@ -418,6 +418,15 @@ void MoleculeCisTrans::build (int *exclude_bonds)
 
       int *substituents = _bonds[i].substituents;
 
+
+      // Ignore only bonds that can be cis-trans ?
+      // Ignore bonds marked bonds
+      if (exclude_bonds != 0 && exclude_bonds[i])
+      {
+         _bonds[i].ignored = 1;
+         continue;
+      }
+
       bool have_xyz = true;
       // If bond is marked with ignore flag then read this flag 
       // even if coordinates are not valid.
@@ -426,12 +435,6 @@ void MoleculeCisTrans::build (int *exclude_bonds)
       if (!isGeomStereoBond(mol, i, substituents, have_xyz))
          continue;
 
-      // Ignore only bonds that can be cis-trans
-      if (exclude_bonds != 0 && exclude_bonds[i])
-      {
-         _bonds[i].ignored = 1;
-         continue;
-      }
 
       if (!sortSubstituents(mol, substituents, 0))
          continue;
