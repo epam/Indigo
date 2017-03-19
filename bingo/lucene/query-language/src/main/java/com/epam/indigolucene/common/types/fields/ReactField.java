4f21e2e11d2e8b2e6b093045587fed3a7234461e
Filipp Pisarev <filipp_pisarev@epam.com>
2017-03-19 14:19:49 +0300

    Added field & value classes necessary for reaction search

---
+++ bingo/lucene/query-language/src/main/java/com/epam/indigolucene/common/types/fields/ReactField.java
@@ -0,0 +1,19 @@
+package com.epam.indigolucene.common.types.fields;
+import com.epam.indigo.IndigoObject;
+import com.epam.indigolucene.common.types.fields.Field;
+import com.epam.indigolucene.common.types.values.ReactValue;
+
+public class ReactField<S> extends Field<S, IndigoObject, ReactValue> {
+    public ReactField(String name, boolean isMultiple) {
+        super(name, isMultiple);
+    }
+
+    @Override
+    public ReactValue<S> createValue(IndigoObject from) {
+        return new ReactValue<>(from, this);
+    }
+
+    public final String getFingerPrintFieldName() {
+        return name + "_fingerprint";
+    }
+}
---
+++ bingo/lucene/query-language/src/main/java/com/epam/indigolucene/common/types/values/ReactValue.java
@@ -0,0 +1,30 @@
+package com.epam.indigolucene.common.types.values;
+
+import com.epam.indigo.IndigoObject;
+import com.epam.indigolucene.common.types.fields.ReactField;
+import com.epam.indigolucene.common.utils.Utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by floatingpoint on 19.03.17.
+ */
+public class ReactValue<S> extends Value<ReactField<S>>{
+    IndigoObject obj;
+
+    public ReactValue(IndigoObject from,ReactField<S> f) {
+        super(f);
+        obj = from;
+    }
+
+    @Override
+    public Map<String, Object> toMap() {
+        Map<String, Object> res = new HashMap<>();
+        obj.aromatize();
+        res.put(field.getName(), obj.serialize());
+        IndigoObject fingerprint = obj.fingerprint();
+        res.put(field.getFingerPrintFieldName(), Utils.fingerprintToBitNums(fingerprint));
+        return res;
+    }
+}
