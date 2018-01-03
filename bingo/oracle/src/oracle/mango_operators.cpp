a19cf5d616049c439e4284060fe211efed0185a1
Aleksandr Savelev <aleksandr_savelev@epam.com>
2018-01-03 17:47:51 -0500

    bingo-core: fix errors throwing for search methods (reject_invalid_structures)

--- bingo/oracle/src/oracle/mango_operators.cpp
+++ bingo/oracle/src/oracle/mango_operators.cpp
@@ -139,7 +139,7 @@ static OCINumber * _mangoCommon (OCIExtProcContext *ctx, int context_id,
 {
    OCINumber *result = NULL;
 
-   ORABLOCK_BEGIN
+   ORA_SAFEBLOCK_BEGIN("search")
    {
       *return_ind = OCI_IND_NULL;
 
@@ -153,6 +153,7 @@ static OCINumber * _mangoCommon (OCIExtProcContext *ctx, int context_id,
       if (target_ind == OCI_IND_NOTNULL)
       {
          MangoOracleContext &context = MangoOracleContext::get(env, context_id, false);
+         block_throw_error = context.context().reject_invalid_structures.get();
 
          QS_DEF(Array<char>, query_buf);
          QS_DEF(Array<char>, target_buf);
@@ -172,7 +173,7 @@ static OCINumber * _mangoCommon (OCIExtProcContext *ctx, int context_id,
       else
          *return_ind = OCI_IND_NOTNULL;
    }
-   ORABLOCK_END
+   ORA_SAFEBLOCK_END
 
    return result;
 }
