e963440338ca526ebae252104a5738884e697726
Aleksandr Savelev <Aleksandr_Savelev@epam.com>
2018-03-13 12:02:09 +0300

    indigo-core: add rings to the pattern layout. Restore missing templates

--- layout/src/templates/generate_layout_patterns.py
+++ layout/src/templates/generate_layout_patterns.py
@@ -1,23 +1,39 @@
 import os
 
-templates_file = open('layout_patterns.inc', 'w')
 
-names = list(os.listdir('molecules'))
-
-templates_file.write('// Templates from the following files:\n')
-for fname in names:
-    templates_file.write('//   {}\n'.format(fname))
-
-templates_file.write('static const char* layout_templates[] =\n')
-templates_file.write('{\n')
-
-for fname in names:
-    content = open(os.path.join('molecules', fname)).read().split("\n")
+def process_ctab(fname, input, output, replace_header=True):
+    content = input.split("\n")
     # replace first line with file name
-    content[0] = fname
-    lines = ['  "' + line + '\\n"' for line in content]
+    if replace_header:
+        content[0] = fname
+    lines = ['  "' + line.strip('\n').strip('\r') + '\\n"' for line in content]
 
-    templates_file.write('\n'.join(lines))
-    templates_file.write(',\n\n')
+    output.write('\n'.join(lines))
+    output.write(',\n\n')
 
-templates_file.write("};\n")
+names = sorted(list(os.listdir('molecules')), key=str.lower)
+
+with open('layout_patterns.inc', 'w') as templates_file:
+    templates_file.write('// Templates from the following files:\n')
+    for fname in names:
+        templates_file.write('//   {}\n'.format(fname))
+
+    templates_file.write('static const char* layout_templates[] =\n')
+    templates_file.write('{\n')
+
+    for fname in names:
+        if fname.endswith('mol'):
+            print(fname)
+            with open(os.path.join('molecules', fname)) as content:
+                process_ctab(fname, content.read(), templates_file)
+        elif fname.endswith('sdf'):
+            print(fname)
+            with open(os.path.join('molecules', fname)) as sdf:
+                reader = sdf.read()
+                for i, content in enumerate(reader.split('$$$$')):
+                    # print(content)
+                    process_ctab(fname, content.strip(), templates_file, False)
+        else:
+            print("unknown molecule format")
+
+    templates_file.write("};\n")
