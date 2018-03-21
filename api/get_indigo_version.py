a924fe2565486cf77946359eb505ea913baa36e3
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2018-03-21 20:42:25 +0300

    build: fix

--- api/get_indigo_version.py
+++ api/get_indigo_version.py
@@ -6,7 +6,7 @@ def getIndigoVersion():
     version = "unknown"
     indigo_version_h = os.path.join(os.path.dirname(__file__), "src", "indigo_version.h")
     if os.path.exists(indigo_version_h):
-        for line in open():
+        for line in open(indigo_version_h):
             m = re.search('INDIGO_VERSION "(.*)-.*"', line)
             if m:
                 version = m.group(1)
