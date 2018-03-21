676cc2bb4f2be5e092c9517bdc23df2a1d5b5135
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2018-03-21 20:04:38 +0300

    build: fix for multi-stage build

--- api/get_indigo_version.py
+++ api/get_indigo_version.py
@@ -4,9 +4,13 @@ import re
 
 def getIndigoVersion():
     version = "unknown"
+    indigo_version_h = os.path.join(os.path.dirname(__file__), "src", "indigo_version.h")
+    if os.path.exists(indigo_version_h):
+        for line in open():
+            m = re.search('INDIGO_VERSION "(.*)-.*"', line)
+            if m:
+                version = m.group(1)
+    elif 'INDIGO_VERSION' in os.environ:
+        version = os.environ['INDIGO_VERSION']
 
-    for line in open(os.path.join(os.path.dirname(__file__), "src", "indigo_version.h")):
-        m = re.search('INDIGO_VERSION "(.*)-.*"', line)
-        if m:
-            version = m.group(1)
     return version
