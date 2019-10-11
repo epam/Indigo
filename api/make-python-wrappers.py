6f11d41c316ec46f4669b4bf39be9800090e0195
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2019-10-12 00:14:30 +0300

    build: api: python: script for auto-creating wheels [WIP]

--- api/make-python-wrappers.py
+++ api/make-python-wrappers.py
@@ -1,10 +1,11 @@
 import os
+import runpy
 import shutil
+import sys
+from optparse import OptionParser
 from os.path import *
 
-from optparse import OptionParser
-
-parser = OptionParser(description='Indigo Java libraries build script')
+parser = OptionParser(description='Indigo Python libraries build script')
 parser.add_option('--suffix', '-s', help='archive suffix', default="")
 
 (args, left_args) = parser.parse_args()
@@ -41,3 +42,36 @@ shutil.make_archive(archive_name, 'zip', os.path.dirname(archive_name), archive_
 shutil.rmtree(archive_name)
 full_archive_name = os.path.normpath(os.path.join(dist_dir, archive_name))
 print('Archive {}.zip created'.format(full_archive_name))
+
+
+def make_wheels(api_dir, dest):
+    os.makedirs(os.path.join(dest), exist_ok=True)
+    os.makedirs(os.path.join(dest, 'indigo'), exist_ok=True)
+
+    shutil.copy(os.path.join(api_dir, "LICENSE"), dest)
+    shutil.copy(os.path.join(api_dir, "python", "indigo.py"), os.path.join(dest, 'indigo', '__init__.py'))
+
+    shutil.copy(os.path.join(api_dir, "plugins", "renderer", "python", "indigo_renderer.py"), os.path.join(dest, 'indigo', 'renderer.py'))
+    shutil.copy(os.path.join(api_dir, "plugins", "inchi", "python", "indigo_inchi.py"), os.path.join(dest, 'indigo', 'inchi.py'))
+    shutil.copy(os.path.join(api_dir, "plugins", "bingo", "python", "bingo.py"), os.path.join(dest, 'indigo', 'bingo.py'))
+    shutil.copy(os.path.join(api_dir, "python", "setup.py"), dest)
+    shutil.copytree(os.path.join(api_dir, "libs", "shared"), os.path.join(dest, 'indigo', "lib"), ignore=shutil.ignore_patterns("*.lib"))
+    curdir = os.path.abspath(os.curdir)
+    os.chdir(dest)
+    sysargv = sys.argv
+    sys.argv[1:] = ['bdist_wheel', '--plat-name=win32']
+    runpy.run_path('setup.py')
+    sys.argv[1:] = ['bdist_wheel', '--plat-name=win_amd64']
+    runpy.run_path('setup.py')
+    sys.argv[1:] = ['bdist_wheel', '--plat-name=manylinux1_x86_64']
+    runpy.run_path('setup.py')
+    sys.argv[1:] = ['bdist_wheel', '--plat-name=manylinux1_i686']
+    runpy.run_path('setup.py')
+    sys.argv[1:] = ['bdist_wheel', '--plat-name=macosx_10_7']
+    runpy.run_path('setup.py')
+    sys.argv[1:] = sysargv[1:]
+    os.chdir(curdir)
+
+
+if __name__ == '__main__':
+    make_wheels(api_dir, os.path.join(dist_dir, 'epam.indigo'))
--- api/python/setup.py
+++ api/python/setup.py
@@ -4,17 +4,49 @@ import sys
 
 from setuptools import setup
 
+CLASSIFIERS = """\
+Development Status :: 4 - Beta
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved :: Apache Software License
+Programming Language :: C
+Programming Language :: C++
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.0
+Programming Language :: Python :: 3.1
+Programming Language :: Python :: 3.2
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: Implementation :: CPython
+Topic :: Software Development
+Topic :: Scientific/Engineering :: Chemistry
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX :: Linux
+Operating System :: MacOS
+"""
+
+
 INDIGO_LIBS = None
 
 if sys.argv[1] == 'bdist_wheel':
-    for opt in sys.argv[1:]:
+    for opt in sys.argv[2:]:
         if opt.startswith('--plat-name'):
             name = opt.split('=')[1]
-            INDIGO_LIBS = 'lib/Mac/10.7/*.dylib' if name.startswith('macosx_10_7') else \
-                'lib/Linux/x64/*.so' if name == 'manylinux1_x86_64' else \
-                    'lib/Linux/x86/*.so' if name == 'manylinux1_i686' else \
-                        'lib/Win/x64/*.dll' if name == 'win_amd64' else \
-                            'lib/Win/x86/*.dll' if name == 'win32' else None
+            if name.startswith('macosx_10_7'):
+                INDIGO_LIBS = 'lib/Mac/10.7/*.dylib'
+            elif name == 'manylinux1_x86_64':
+                INDIGO_LIBS = 'lib/Linux/x64/*.so'
+            elif name == 'manylinux1_i686':
+                INDIGO_LIBS = 'lib/Linux/x86/*.so'
+            elif name == 'win_amd64':
+                INDIGO_LIBS = 'lib/Win/x64/*.dll'
+            elif name == 'win32':
+                INDIGO_LIBS = 'lib/Win/x86/*.dll'
             break
 
 if not INDIGO_LIBS:
@@ -26,16 +58,18 @@ if os.path.exists('indigo_chem.egg-info'):
     distutils.dir_util.remove_tree('indigo_chem.egg-info')
 
 setup(
-    name='indigo-chem',
+    name='epam.indigo',
     version='1.4.0-beta',
     description='Indigo universal cheminformatics toolkit',
     author='EPAM Systems Lifescience Department',
     author_email='lifescience.opensource@epam.com',
     maintainer='Mikhail Kviatkovskii',
     maintainer_email='Mikhail_Kviatkovskii@epam.com',
-    packages=['indigo', 'indigo_inchi', 'indigo_renderer', 'bingo'],
+    packages=['indigo', ],
     license="Apache-2.0",
-    url="http://github.com/epam/indigo",
+    url="http://github.com/epam/indigo.git",
     package_dir={'indigo': 'indigo'},
     package_data={'indigo': [INDIGO_LIBS, ]},
+    classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+    platforms=["Windows", "Linux", "Mac OS-X"],
 )
