0329e5b3dde804cae340193574e8bb5f3ae1e79e
Mikhail_Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2019-10-24 16:43:37 +0300

    api: dotnet: update to support netstandard2.0 and net20 targets

---
+++ api/dotnet/IndigoNativeLibraryLoader.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Reflection;
+
+namespace com.epam.indigo
+{   
+    // Singleton Native Library loader
+    public class IndigoNativeLibraryLoader
+    {
+        private static volatile IndigoNativeLibraryLoader _instance;
+        private static object _global_sync_root = new Object();
+
+        public static IndigoNativeLibraryLoader Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_global_sync_root)
+                    {
+                        if (_instance == null)
+                            _instance = new IndigoNativeLibraryLoader();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        class DllData
+        {
+            public string file_name;
+            public string lib_path;
+        }
+
+        // Mapping from the DLL name to the handle.
+        // DLL handles in the loading order
+        // Local synchronization object
+        Object _sync_object = new Object();
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        struct utsname
+        {
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public string sysname;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public string nodename;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public string release;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public string version;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public string machine;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 1024)]
+            public string extraJustInCase;
+
+        }
+
+        [DllImport("libc")]
+        private static extern void uname(out utsname uname_struct);
+
+        private static string detectUnixKernel()
+        {
+            utsname uts;
+            uname(out uts);
+            return uts.sysname.ToString();
+        }
+
+        static public bool isMac()
+        {
+            return (detectUnixKernel() == "Darwin");
+        }
+
+        public void loadLibrary(string path, string filename)
+        {
+            lock (_sync_object)
+            {
+                var data = new DllData();
+                data.lib_path = path;
+                data.file_name = _getPathToBinary(path, filename);
+                string newEnvPath = Directory.GetParent(data.file_name).ToString();
+                var pathEnv = Environment.GetEnvironmentVariable("PATH");
+                if (!pathEnv.Contains(newEnvPath))
+                {
+                    string envPathSep = (Environment.OSVersion.Platform == PlatformID.Win32NT) ? ";" : ":"; 
+                    pathEnv += envPathSep + Directory.GetParent(data.file_name);                    
+                    Environment.SetEnvironmentVariable("PATH", pathEnv, EnvironmentVariableTarget.Process);
+                }
+            }
+        }
+
+        ~IndigoNativeLibraryLoader()
+        {
+            lock (_global_sync_root)
+            {
+                _instance = null;
+            }
+        }
+
+        public bool isValid()
+        {
+            return (_instance != null);
+        }
+
+        string _getPathToBinary(string path, string filename)
+        {
+            return _extractFromAssembly(path, filename);
+        }
+
+        string _getTemporaryDirectory(Assembly resource_assembly)
+        {
+            string dir;
+            dir = Path.Combine(Path.GetTempPath(), "EPAM_indigo");
+            dir = Path.Combine(dir, resource_assembly.GetName().Name);
+            dir = Path.Combine(dir, resource_assembly.GetName().Version.ToString());
+            return dir;
+        }
+
+        string _extractFromAssembly(string inputPath, string filename)
+        {
+            string resource = string.Format("{0}.{1}", inputPath, filename);
+            Stream fs = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource);
+            if (fs == null)
+                throw new IndigoException("Internal error: there is no resource " + resource);
+            byte[] ba = new byte[fs.Length];
+            fs.Read(ba, 0, ba.Length);
+            fs.Close();
+            if (ba == null)
+                throw new IndigoException("Internal error: there is no resource " + resource);
+
+            string tmpdir_path = _getTemporaryDirectory(Assembly.GetCallingAssembly());
+            // Make per-version-unique dependent dll name
+            string outputPath = Path.Combine(tmpdir_path, inputPath);
+            outputPath = Path.Combine(outputPath, filename);
+            string dir = Path.GetDirectoryName(outputPath);
+            string name = Path.GetFileName(outputPath);
+
+            string new_dll_name = name;
+
+            // This temporary file is used to avoid inter-process
+            // race condition when concurrently stating many processes
+            // on the same machine for the first time.
+            string tmp_filename = Path.GetTempFileName();
+            string new_full_path = Path.Combine(dir, new_dll_name);
+            FileInfo file = new FileInfo(new_full_path);
+            if (!file.Directory.Exists)
+                file.Directory.Create();
+            // Check if file already exists
+            if (!file.Exists || file.Length == 0) {
+                File.WriteAllBytes(tmp_filename, ba);
+                // file is ready to be moved.. lets check again
+                if (!file.Exists || file.Length == 0) {
+                    File.Move(tmp_filename, file.FullName);
+                } else {
+                    File.Delete(tmp_filename);
+                }
+            }
+            return file.FullName;
+        }
+    }
+}
---
+++ api/dotnet/prebuild.ps1
@@ -0,0 +1,33 @@
+function touchIfNotExists {param(${checkedPath})
+    if (![System.IO.File]::Exists(${checkedPath})) {
+        echo "Creating stub for library ${checkedPath}"        
+        New-Item -ItemType File -Force -Path ${checkedPath}
+    } else {
+        if ((Get-Item ${checkedPath}).Length -eq 0) {
+            echo "Found stub for library ${checkedPath}"
+        } else {
+            echo "Found native library ${checkedPath}"
+            ${global:result} += 1            
+        }
+        
+    }
+}
+
+${resourceFolder} = [IO.Path]::GetFullPath("${PSScriptRoot}/Resource")
+echo "Checking for native Indigo libraries in Resource folder ${resourceFolder}..."
+${result} = 0
+touchIfNotExists([IO.Path]::GetFullPath("${resourceFolder}/Linux/x64/libindigo.so"))
+touchIfNotExists([IO.Path]::GetFullPath("${resourceFolder}/Linux/x86/libindigo.so"))
+touchIfNotExists([IO.Path]::GetFullPath("${resourceFolder}/Mac/10.7/libindigo.dylib"))
+touchIfNotExists([IO.Path]::GetFullPath("${resourceFolder}/Win/x64/concrt140.dll"))
+touchIfNotExists([IO.Path]::GetFullPath("${resourceFolder}/Win/x64/indigo.dll"))
+touchIfNotExists([IO.Path]::GetFullPath("${resourceFolder}/Win/x64/msvcp140.dll"))
+touchIfNotExists([IO.Path]::GetFullPath("${resourceFolder}/Win/x64/vcruntime140.dll"))
+touchIfNotExists([IO.Path]::GetFullPath("${resourceFolder}/Win/x86/concrt140.dll"))
+touchIfNotExists([IO.Path]::GetFullPath("${resourceFolder}/Win/x86/indigo.dll"))
+touchIfNotExists([IO.Path]::GetFullPath("${resourceFolder}/Win/x86/msvcp140.dll"))
+touchIfNotExists([IO.Path]::GetFullPath("${resourceFolder}/Win/x86/vcruntime140.dll"))
+
+if (!${result}) {
+    throw "No native libraries found in resource folder '${resourceFolder}', only stubs created. Please add native libraries first"
+}
