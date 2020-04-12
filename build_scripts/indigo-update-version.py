bc7faa9182133cf42f6a008eecf1a4514866da60
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2020-04-12 17:13:54 +0300

    api: build: update Indigo.Net wrappers versions during build

--- build_scripts/indigo-update-version.py
+++ build_scripts/indigo-update-version.py
@@ -1,5 +1,4 @@
 import os
-import re
 import sys
 import xml.etree.cElementTree as ElementTree
 
@@ -19,13 +18,37 @@ def updatePomVersion(pomFile, newVersion):
     tree.write(pomFile)
 
 
+def updateCsProjVersion(csProjFile, newVersion):
+    tree = ElementTree.parse(csProjFile)
+    root = tree.getroot()
+    for child in root:
+        print(child.tag)
+        if child.tag == 'PropertyGroup':
+            for cc in child:
+                if cc.tag == 'Version':
+                    print('Updating Indigo version from {0} to {1} in {2}...'.format(cc.text, newVersion, csProjFile))
+                    cc.text = newVersion
+                    break
+    tree.write(csProjFile)
+
+
 def main(newVersion):
     indigoVersion = newVersion
     updatePomVersion(os.path.join(INDIGO_PATH, 'api', 'java', 'pom.xml'), indigoVersion)
     updatePomVersion(os.path.join(INDIGO_PATH, 'api', 'plugins', 'bingo', 'java', 'pom.xml'), indigoVersion)
     updatePomVersion(os.path.join(INDIGO_PATH, 'api', 'plugins', 'inchi', 'java', 'pom.xml'), indigoVersion)
     updatePomVersion(os.path.join(INDIGO_PATH, 'api', 'plugins', 'renderer', 'java', 'pom.xml'), indigoVersion)
+    updateCsProjVersion(os.path.join(INDIGO_PATH, 'api', 'dotnet', 'Indigo.Net.csproj'), indigoVersion)
+    updateCsProjVersion(os.path.join(INDIGO_PATH, 'api', 'plugins', 'bingo', 'dotnet', 'Bingo.Net.csproj'), indigoVersion)
+    updateCsProjVersion(os.path.join(INDIGO_PATH, 'api', 'plugins', 'inchi', 'dotnet', 'IndigoInchi.Net.csproj'), indigoVersion)
+    updateCsProjVersion(os.path.join(INDIGO_PATH, 'api', 'plugins', 'renderer', 'dotnet', 'IndigoRenderer.Net.csproj'), indigoVersion)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    if len(sys.argv) > 1:
+        version = sys.argv[1]
+    else:
+        sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'api'))
+        from get_indigo_version import getIndigoVersion
+        version = getIndigoVersion()
+    main(version)
