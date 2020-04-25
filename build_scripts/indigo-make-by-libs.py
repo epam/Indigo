f98a0a2dcb09eb242b49fbe417ad149f697a591c
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2020-04-25 14:13:17 +0300

    build: api: indigo-make-by-libs.py: call 'indigo-update-version.py' before building wrappers

--- build_scripts/indigo-make-by-libs.py
+++ build_scripts/indigo-make-by-libs.py
@@ -1,29 +1,25 @@
-# This module assumes that you have installed all the
-# libs files in the <source root>/dist directory
-from contextlib import contextmanager
-from optparse import OptionParser
 import os
 import re
+import runpy
 import shutil
 import subprocess
 import sys
 import zipfile
+from contextlib import contextmanager
+from optparse import OptionParser
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "api"))
-from get_indigo_version import getIndigoVersion
 
-
-def flatten_directory(dir):
-    todelete = []
-    for f in os.listdir(dir):
+def flatten_directory(dir_):
+    to_delete = []
+    for f in os.listdir(dir_):
         if f.find("python") != -1 or f.find("java") != -1 or f.find("dotnet") != -1:
             continue
-        dir2 = os.path.join(dir, f)
+        dir2 = os.path.join(dir_, f)
         if os.path.isdir(dir2):
             for f2 in os.listdir(dir2):
                 f2full = os.path.join(dir2, f2)
-                shutil.move(f2full, dir)
-            todelete.append(dir2)
+                shutil.move(f2full, dir_)
+            to_delete.append(dir2)
             os.rmdir(dir2)
 
 
@@ -49,12 +45,12 @@ def copytree(src, dst, symlinks=False, ignore=None):
             shutil.copy(s, d)
 
 
-def join_archives(names, destname):
-    if os.path.exists(destname):
-        shutil.rmtree(destname)
-    dest_zfname = '{}.zip'.format(destname)
-    if os.path.exists(dest_zfname):
-        os.remove(dest_zfname)
+def join_archives(names, dest_name):
+    if os.path.exists(dest_name):
+        shutil.rmtree(dest_name)
+    dest_zf_name = '{}.zip'.format(dest_name)
+    if os.path.exists(dest_zf_name):
+        os.remove(dest_zf_name)
     for name in names:
         zf_name = '{}.zip'.format(name)
         if not os.path.exists(zf_name):
@@ -64,24 +60,24 @@ def join_archives(names, destname):
         os.makedirs(name)
         with zipfile.ZipFile(zf_name) as zf:
             zf.extractall(name)
-        copytree(name, destname)
+        copytree(name, dest_name)
         shutil.rmtree(name)
         # os.remove(zf_name)
-    shutil.make_archive(destname, 'zip', os.path.dirname(destname), destname)
-    shutil.rmtree(destname)
+    shutil.make_archive(dest_name, 'zip', os.path.dirname(dest_name), dest_name)
+    shutil.rmtree(dest_name)
 
 
-def join_archives_by_pattern(pattern, destname):
+def join_archives_by_pattern(pattern, dest_name):
     archives = []
     for f in os.listdir(os.curdir):
         if re.match(pattern, f):
             archives.append(os.path.splitext(f)[0])
     if len(archives) == 0:
         return
-    join_archives(archives, destname)
+    join_archives(archives, dest_name)
 
 
-def clear_libs():
+def clear_libs(libs_dir):
     for f in os.listdir(libs_dir):
         if f == "readme.txt":
             continue
@@ -92,13 +88,14 @@ def clear_libs():
             os.remove(ffull)
 
 
-def unpack_to_libs(name):
+def unpack_to_libs(name, libs_dir):
     with zipfile.ZipFile('{}.zip'.format(name)) as zf:
         zf.extractall(libs_dir)
         unzipped_folder = os.path.join(libs_dir, os.path.basename(name))
         for os_name in os.listdir(os.path.join(unzipped_folder, 'shared')):
             shutil.copytree(os.path.join(unzipped_folder, 'shared', os_name), os.path.join(libs_dir, 'shared', os_name))
         shutil.rmtree(unzipped_folder, ignore_errors=True)
+
 
 @contextmanager
 def cwd(path):
@@ -110,26 +107,27 @@ def cwd(path):
         os.chdir(oldpwd)
 
 
-arc_joins = [
-    ("./indigo-libs-%ver%-linux-shared", "indigo-libs-%ver%-linux.+-shared"),
-    ("./indigo-libs-%ver%-win-shared", "indigo-libs-%ver%-win.+-shared"),
-    ("./indigo-libs-%ver%-mac-shared", "indigo-libs-%ver%-mac.+-shared"),
-    ("./indigo-libs-%ver%-linux-static", "indigo-libs-%ver%-linux.+-static"),
-    ("./indigo-libs-%ver%-win-static", "indigo-libs-%ver%-win.+-static"),
-    ("./indigo-libs-%ver%-mac-static", "indigo-libs-%ver%-mac.+-static"),
-]
+def main():
+    arc_joins = [
+        ("./indigo-libs-%ver%-linux-shared", "indigo-libs-%ver%-linux.+-shared"),
+        ("./indigo-libs-%ver%-win-shared", "indigo-libs-%ver%-win.+-shared"),
+        ("./indigo-libs-%ver%-mac-shared", "indigo-libs-%ver%-mac.+-shared"),
+        ("./indigo-libs-%ver%-linux-static", "indigo-libs-%ver%-linux.+-static"),
+        ("./indigo-libs-%ver%-win-static", "indigo-libs-%ver%-win.+-static"),
+        ("./indigo-libs-%ver%-mac-static", "indigo-libs-%ver%-mac.+-static"),
+    ]
 
-wrappers = [
-    ("win", ["win"]),
-    ("linux", ["linux"]),
-    ("mac", ["mac"]),
-    ("universal", ["win", "linux", "mac"]),
-]
+    wrappers = [
+        ("win", ["win"]),
+        ("linux", ["linux"]),
+        ("mac", ["mac"]),
+        ("universal", ["win", "linux", "mac"]),
+    ]
 
-wrappers_gen = ["make-java-wrappers.py", "make-python-wrappers.py", 'make-dotnet-wrappers.py']
+    wrappers_gen = ["make-java-wrappers.py", "make-python-wrappers.py", 'make-dotnet-wrappers.py']
 
+    runpy.run_path(os.path.join(os.path.dirname(__file__), 'indigo-update-version.py'), run_name="__main__")
 
-if __name__ == '__main__':
     parser = OptionParser(description='Indigo libraries repacking')
     parser.add_option('--libonlyname', help='extract only the library into api/lib')
     parser.add_option('--config', default="Release", help='project configuration')
@@ -148,19 +146,21 @@ if __name__ == '__main__':
     if args.config.lower() != "release":
         suffix = "-" + args.config.lower()
 
-    need_join_archieves = args.libonlyname is None
+    need_join_archives = args.libonlyname is None
     need_gen_wrappers = args.libonlyname is None
 
-    # Find indigo version
-    version = getIndigoVersion()
+    # Find Indigo version
+    sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, "api"))
+    get_indigo_version = __import__('get_indigo_version').getIndigoVersion
+    version = get_indigo_version()
     with cwd(os.path.join(os.path.split(__file__)[0], '..', 'dist')):
 
-        if need_join_archieves:
+        if need_join_archives:
             flatten_directory(".")
 
-        if need_join_archieves:
+        if need_join_archives:
             for dest, pattern in arc_joins:
-                p = pattern.replace("%ver%", version) + "\.zip"
+                p = pattern.replace("%ver%", version) + ".zip"
                 d = dest.replace("%ver%", version) + suffix
                 join_archives_by_pattern(p, d)
 
@@ -170,7 +170,7 @@ if __name__ == '__main__':
         libs_dir = os.path.join(api_dir, "libs")
 
         for w, libs in wrappers:
-            clear_libs()
+            clear_libs(libs_dir)
             if args.libonlyname and w != args.libonlyname:
                 continue
             any_exists = True
@@ -178,7 +178,7 @@ if __name__ == '__main__':
                 name = "indigo-libs-%s-%s-shared%s" % (version, lib, suffix)
                 if os.path.exists(name + ".zip"):
                     any_exists = any_exists and True
-                    unpack_to_libs(name)
+                    unpack_to_libs(name, libs_dir)
                 else:
                     any_exists = any_exists and False
             if not any_exists:
@@ -191,3 +191,7 @@ if __name__ == '__main__':
                                 command = '"%s" "%s" -s "%s"' % (sys.executable, os.path.join(api_dir, gen), w)
                                 print(command)
                                 subprocess.check_call(command, shell=True)
+
+
+if __name__ == '__main__':
+    main()
--- build_scripts/indigo-update-version.py
+++ build_scripts/indigo-update-version.py
@@ -1,80 +1,76 @@
-import re
 import os
+import re
 import sys
 import xml.etree.cElementTree as ElementTree
 
 INDIGO_PATH = os.path.normpath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.path.pardir))
 
 
-def updatePomVersion(pomFile, newVersion):
+def update_pom_version(pomFile, newVersion):
     tree = ElementTree.parse(pomFile)
     ElementTree.register_namespace('', 'http://maven.apache.org/POM/4.0.0')
     root = tree.getroot()
-    versionChanged = False
+    version_changed = False
     for child in root:
         if child.tag.endswith('version'):
-            oldVersion = child.text
-            if oldVersion != newVersion:
-                print('Updating Indigo version from {0} to {1} in {2}...'.format(oldVersion, newVersion, pomFile))
+            old_version = child.text
+            if old_version != newVersion:
+                print('Updating Indigo version from {0} to {1} in {2}...'.format(old_version, newVersion, pomFile))
                 child.text = newVersion
-                versionChanged = True
+                version_changed = True
             else:
                 print('Indigo version in {0} remains {1}...'.format(pomFile, newVersion))
             break
-    if versionChanged:
+    if version_changed:
         tree.write(pomFile)
 
 
-def updateCsProjVersion(csProjFile, newVersion):
-    tree = ElementTree.parse(csProjFile)
+def update_csproj_version(csproj_file, new_version):
+    tree = ElementTree.parse(csproj_file)
     root = tree.getroot()
-    versionChanged = False
+    version_changed = False
     for child in root:
         if child.tag == 'PropertyGroup':
             for cc in child:
                 if cc.tag == 'Version':
-                    oldVersion = cc.text
-                    if oldVersion != newVersion:
-                        print('Updating Indigo version from {0} to {1} in {2}...'.format(oldVersion, newVersion, csProjFile))
-                        cc.text = newVersion
-                        versionChanged = True
+                    old_version = cc.text
+                    if old_version != new_version:
+                        print('Updating Indigo version from {0} to {1} in {2}...'.format(old_version, new_version,
+                                                                                         csproj_file))
+                        cc.text = new_version
+                        version_changed = True
                     else:
-                        print('Indigo version in {0} remains {1}...'.format(csProjFile, newVersion))
+                        print('Indigo version in {0} remains {1}...'.format(csproj_file, new_version))
                     break
-    if versionChanged:
-        tree.write(csProjFile)
+    if version_changed:
+        tree.write(csproj_file)
 
 
-def updateSetupPyVersion(setupPyFile, newVersion):
-    with open(setupPyFile) as f:
+def update_setup_py_version(setup_py_file, new_version):
+    with open(setup_py_file) as f:
         data = f.read()
     vr = re.compile("version='(.+)'")
     m = vr.search(data)
-    oldVersion = m.group(1)
-    if oldVersion != newVersion:
-        data = vr.subn("version='{}'".format(newVersion), data)[0]
-        print('Updating Indigo version from {0} to {1} in {2}...'.format(oldVersion, newVersion, setupPyFile))
-        with open(setupPyFile, 'w') as f:
+    old_version = m.group(1)
+    if old_version != new_version:
+        data = vr.subn("version='{}'".format(new_version), data)[0]
+        print('Updating Indigo version from {0} to {1} in {2}...'.format(old_version, new_version, setup_py_file))
+        with open(setup_py_file, 'w') as f:
             f.write(data)
     else:
-        print('Indigo version in {0} remains {1}...'.format(setupPyFile, newVersion))
+        print('Indigo version in {0} remains {1}...'.format(setup_py_file, new_version))
 
 
-def main(newVersion):
-    indigoVersion = newVersion
-    updatePomVersion(os.path.join(INDIGO_PATH, 'api', 'java', 'pom.xml'), indigoVersion)
-    updatePomVersion(os.path.join(INDIGO_PATH, 'api', 'plugins', 'bingo', 'java', 'pom.xml'), indigoVersion)
-    updatePomVersion(os.path.join(INDIGO_PATH, 'api', 'plugins', 'inchi', 'java', 'pom.xml'), indigoVersion)
-    updatePomVersion(os.path.join(INDIGO_PATH, 'api', 'plugins', 'renderer', 'java', 'pom.xml'), indigoVersion)
-    updateCsProjVersion(os.path.join(INDIGO_PATH, 'api', 'dotnet', 'Indigo.Net.csproj'), indigoVersion)
-    updateSetupPyVersion(os.path.join(INDIGO_PATH, 'api', 'python', 'setup.py'), indigoVersion)
+def main():
+    sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'api'))
+    new_version = __import__('get_indigo_version').getIndigoVersion()
+    update_pom_version(os.path.join(INDIGO_PATH, 'api', 'java', 'pom.xml'), new_version)
+    update_pom_version(os.path.join(INDIGO_PATH, 'api', 'plugins', 'bingo', 'java', 'pom.xml'), new_version)
+    update_pom_version(os.path.join(INDIGO_PATH, 'api', 'plugins', 'inchi', 'java', 'pom.xml'), new_version)
+    update_pom_version(os.path.join(INDIGO_PATH, 'api', 'plugins', 'renderer', 'java', 'pom.xml'), new_version)
+    update_csproj_version(os.path.join(INDIGO_PATH, 'api', 'dotnet', 'Indigo.Net.csproj'), new_version)
+    update_setup_py_version(os.path.join(INDIGO_PATH, 'api', 'python', 'setup.py'), new_version)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        version = sys.argv[1]
-    else:
-        sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'api'))
-        from get_indigo_version import getIndigoVersion
-        version = getIndigoVersion()
-    main(version)
+    main()
