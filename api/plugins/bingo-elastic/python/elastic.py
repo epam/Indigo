ddb29ea8f8e093831e3c833833e769f43a462abf
Ruslan Khyurri <ruslan.khyurri@gmail.com>
2020-10-07 09:07:47 +0300

    Add python elastic draft implementation

---
+++ api/plugins/bingo-elastic/python/elastic.py
@@ -0,0 +1,10 @@
+class ElasticRepository:
+
+    def __init__(self, host: str, port: int, scheme: str):
+        self.el_client = ''
+
+    def create_index(self):
+        pass
+
+    def filter(self, obj):
+        pass
