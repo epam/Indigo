2affc533bfe344d2feb50a0ed69b5edf67f50a91
Konstantin Perikov <pericov.ko@gmail.com>
2020-10-25 21:44:53 +0000

    fix format with isort&black

--- api/python/copy-libs.py
+++ api/python/copy-libs.py
@@ -6,6 +6,6 @@ cur_dir = split(__file__)[0]
 lib_dir = join(cur_dir, "lib")
 if exists(lib_dir):
     shutil.rmtree(lib_dir)
-    
+
 srclibs_dir = join(cur_dir, "..", "libs", "shared")
 shutil.copytree(srclibs_dir, lib_dir)
