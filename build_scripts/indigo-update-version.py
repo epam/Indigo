d25a663457fd57515abf39a76262381d79e29ef6
Mikhail Kviatkovskii <mkviatkovskii@users.noreply.github.com>
2020-10-26 13:15:41 +0300

    build: indigo-update-version: fixed quotes issue

--- build_scripts/indigo-update-version.py
+++ build_scripts/indigo-update-version.py
@@ -49,11 +49,11 @@ def update_csproj_version(csproj_file, new_version):
 def update_setup_py_version(setup_py_file, new_version):
     with open(setup_py_file) as f:
         data = f.read()
-    vr = re.compile("version='(.+)'")
+    vr = re.compile('version="(.+)"')
     m = vr.search(data)
     old_version = m.group(1)
     if old_version != new_version:
-        data = vr.subn("version='{}'".format(new_version), data)[0]
+        data = vr.subn('version="{}"'.format(new_version), data)[0]
         print('Updating Indigo version from {0} to {1} in {2}...'.format(old_version, new_version, setup_py_file))
         with open(setup_py_file, 'w') as f:
             f.write(data)
