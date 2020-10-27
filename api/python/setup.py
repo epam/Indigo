1a68280ba2026c2e5cb9793f7af36ef8c9188d5d
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2020-10-27 10:29:59 +0000

    deprecate unserialize in python,dotnet,java (#201)
    * deprecate unserialize in python,dotnet,java
    
    * fix format with isort&black
    
    * build: indigo-update-version: fixed quotes issue
    
    Co-authored-by: Mikhail Kviatkovskii <mkviatkovskii@users.noreply.github.com>

--- api/python/setup.py
+++ api/python/setup.py
@@ -30,57 +30,65 @@ Operating System :: POSIX :: Linux
 Operating System :: MacOS
 """
 
-LONG_DESCRIPTION = 'Indigo is a universal molecular toolkit that can be used for molecular fingerprinting, substructure search, and molecular visualization.\
+LONG_DESCRIPTION = "Indigo is a universal molecular toolkit that can be used for molecular fingerprinting, substructure search, and molecular visualization.\
 Also capable of performing a molecular similarity search, it is 100% open source and provides enhanced stereochemistry support for end users, \
-as well as a documented API for developers.'
+as well as a documented API for developers."
 
 INDIGO_LIBS = None
 
-if sys.argv[1] == 'bdist_wheel':
+if sys.argv[1] == "bdist_wheel":
     for opt in sys.argv[2:]:
-        if opt.startswith('--plat-name'):
-            name = opt.split('=')[1]
-            if name.startswith('macosx_10_7_intel'):
-                INDIGO_LIBS = 'lib/Mac/10.7/*.dylib'
-            elif name == 'manylinux1_x86_64':
-                INDIGO_LIBS = 'lib/Linux/x64/*.so'
-            elif name == 'manylinux1_i686':
-                INDIGO_LIBS = 'lib/Linux/x86/*.so'
-            elif name == 'win_amd64':
-                INDIGO_LIBS = 'lib/Win/x64/*.dll'
-            elif name == 'win32':
-                INDIGO_LIBS = 'lib/Win/x86/*.dll'
+        if opt.startswith("--plat-name"):
+            name = opt.split("=")[1]
+            if name.startswith("macosx_10_7_intel"):
+                INDIGO_LIBS = "lib/Mac/10.7/*.dylib"
+            elif name == "manylinux1_x86_64":
+                INDIGO_LIBS = "lib/Linux/x64/*.so"
+            elif name == "manylinux1_i686":
+                INDIGO_LIBS = "lib/Linux/x86/*.so"
+            elif name == "win_amd64":
+                INDIGO_LIBS = "lib/Win/x64/*.dll"
+            elif name == "win32":
+                INDIGO_LIBS = "lib/Win/x86/*.dll"
             break
 
 if not INDIGO_LIBS:
-    raise ValueError('Wrong --plat-name value! Should be one of: macosx_10_7_intel, manylinux1_x86_64, manylinux1_i686, win_amd64, win32')
+    raise ValueError(
+        "Wrong --plat-name value! Should be one of: macosx_10_7_intel, manylinux1_x86_64, manylinux1_i686, win_amd64, win32"
+    )
 
-if os.path.exists('build'):
-    distutils.dir_util.remove_tree('build')
-if os.path.exists('indigo_chem.egg-info'):
-    distutils.dir_util.remove_tree('indigo_chem.egg-info')
+if os.path.exists("build"):
+    distutils.dir_util.remove_tree("build")
+if os.path.exists("indigo_chem.egg-info"):
+    distutils.dir_util.remove_tree("indigo_chem.egg-info")
 
 setup(
-    name='epam.indigo',
-    version='1.4.0-beta.63',
-    description='Indigo universal cheminformatics toolkit',
-    author='EPAM Systems Life Science Department',
-    author_email='lifescience.opensource@epam.com',
-    maintainer='Mikhail Kviatkovskii',
-    maintainer_email='Mikhail_Kviatkovskii@epam.com',
-    packages=['indigo', ],
+    name="epam.indigo",
+    version="1.4.0-beta.63",
+    description="Indigo universal cheminformatics toolkit",
+    author="EPAM Systems Life Science Department",
+    author_email="lifescience.opensource@epam.com",
+    maintainer="Mikhail Kviatkovskii",
+    maintainer_email="Mikhail_Kviatkovskii@epam.com",
+    packages=[
+        "indigo",
+    ],
     license="Apache-2.0",
     url="https://lifescience.opensource.epam.com/indigo/index.html",
-    package_dir={'indigo': 'indigo'},
-    package_data={'indigo': [INDIGO_LIBS, ]},
-    classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+    package_dir={"indigo": "indigo"},
+    package_data={
+        "indigo": [
+            INDIGO_LIBS,
+        ]
+    },
+    classifiers=[_f for _f in CLASSIFIERS.split("\n") if _f],
     platforms=["Windows", "Linux", "Mac OS-X"],
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/plain',
+    long_description_content_type="text/plain",
     project_urls={
         "Bug Tracker": "https://github.com/epam/indigo/issues",
         "Documentation": "https://lifescience.opensource.epam.com/indigo/api/index.html",
         "Source Code": "https://github.com/epam/indigo/",
     },
-    download_url='https://pypi.org/project/epam.indigo',
+    download_url="https://pypi.org/project/epam.indigo",
 )
