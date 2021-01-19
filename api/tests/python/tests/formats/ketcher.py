12a51791c4cba49cd97327b96e39b6ab20602dd3
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2021-01-19 19:24:24 +0300

    api: tests: python: format/ketcher: simplified test

--- api/tests/python/tests/formats/ketcher.py
+++ api/tests/python/tests/formats/ketcher.py
@@ -3,24 +3,21 @@ import sys
 sys.path.append(os.path.normpath(os.path.join(os.path.abspath(__file__), '..', '..', '..', "common")))
 from env_indigo import *
 
-indigo = Indigo()
-indigo.setOption("molfile-saving-skip-date", "1")
 
-
-def exact_check(m1, m2):
+def exact_check(indigo, m1, m2):
     assert m1.countAtoms() == m2.countAtoms()
     for i in range(m1.countAtoms()):
         assert m1.getAtom(i).symbol() == m2.getAtom(i).symbol()
     assert m1.countBonds() == m2.countBonds()
     for i in range(m2.countBonds()):
         assert m1.getBond(i).bondOrder() == m2.getBond(i).bondOrder()
-        print(i, m1.getBond(i).bondStereo(), m2.getBond(i).bondStereo() )
         assert m1.getBond(i).bondStereo() == m2.getBond(i).bondStereo()
     assert m1.isChiral() == m2.isChiral()
     assert indigo.exactMatch(m1, m2)
+    return True
 
 
-def test_simple_load_save_load():
+def test_simple_load_save_load(indigo):
     """Check load-save-load for some simple SMILES"""
     structs = ("C", "C1=CC=CC=C1", "Nc1[nH]cnc-2ncnc1-2")
     for struct in structs:
@@ -28,33 +25,28 @@ def test_simple_load_save_load():
         m1.layout()
         js = m1.json()
         m2 = indigo.loadMolecule(js)
-        assert exact_check(m1, m2)
+        assert exact_check(indigo, m1, m2)
 
 
-def test_complex_load_save_load():
+def test_simple_cis_trans_load(indigo):
+    m1 = indigo.loadMoleculeFromFile(joinPath("molecules/cis_trans.mol"))
+    m2 = indigo.loadMoleculeFromFile(joinPath("molecules/cis_trans.ket.json"))
+    exact_check(indigo, m1, m2)
+
+
+def test_complex_load_save_load(indigo):
     """Check load-save-load for some files"""
     paths = ("molecules/1.ket.json", "molecules/sgroups.mol", "molecules/all_features_mol.mol")
     for path in paths:
         m1 = indigo.loadMoleculeFromFile(joinPath(path))
         js = m1.json()
         m2 = indigo.loadMolecule(js)
-        exact_check(m1, m2)
-
-
-def test_simple_cis_trans_load():
-    m1 = indigo.loadMoleculeFromFile(joinPath("molecules/cis_trans.mol"))
-    m2 = indigo.loadMoleculeFromFile(joinPath("molecules/cis_trans.ket.json"))
-    exact_check(m1, m2)
-
-
-def test_all_features_mol_ket():
-    m1 = indigo.loadMoleculeFromFile(joinPath("molecules/all_features_mol.mol"))
-    m2 = indigo.loadMoleculeFromFile(joinPath("molecules/all_features_mol.ket.json"))
-    exact_check(m1, m2)
+        exact_check(indigo, m1, m2)
 
 
 if __name__ == '__main__':
-    test_simple_cis_trans_load()
-    test_all_features_mol_ket()
-    test_simple_load_save_load()
-    test_complex_load_save_load
+    indigo = Indigo()
+    indigo.setOption("molfile-saving-skip-date", "1")
+    test_simple_load_save_load(indigo)
+    test_simple_cis_trans_load(indigo)
+    test_complex_load_save_load(indigo)
