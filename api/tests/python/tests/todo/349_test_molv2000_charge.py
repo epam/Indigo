4550691a301806aa056fdccc270a5c0817ac7b9f
Iurii Solntsev <iurii_solntsev@epam.com>
2021-03-11 20:10:43 +0300

    #349: Python test to reproduce the issue.

---
+++ api/tests/python/tests/todo/349_test_molv2000_charge.py
@@ -0,0 +1,19 @@
+import os
+import sys
+sys.path.append(os.path.normpath(os.path.join(os.path.abspath(__file__), '..', '..', '..', "common")))
+from env_indigo import *
+
+indigo = Indigo()
+indigo.setOption("ignore-stereochemistry-errors", True)
+
+    
+def testMolv2000Charge(filename):
+    
+    print(relativePath(filename))
+    try:
+        mol = indigo.loadMoleculeFromFile(joinPath(filename))
+        print("loaded.")
+    except IndigoException as e:
+        print("caught " + getIndigoExceptionText(e))
+
+testMolv2000Charge("molecules/test_molv2000_charge.mol")
