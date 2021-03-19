8bc68522603520fe170adfec48faeef07fab4e24
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2021-03-19 22:25:21 +0300

    #246: build: fixes

--- api/python/indigo/__init__.py
+++ api/python/indigo/__init__.py
@@ -2332,7 +2332,7 @@ class Indigo(object):
 
     # Python embeds path into .pyc code if method is marked with @staticmethod
     # This causes an error when Indigo is loaded from different places by relative path
-    def _initStatic(self):
+    def _initStatic(self, _=None):
         indigo_found = False
         system_name = platform.system().lower()
         machine_name = platform.machine().lower()
