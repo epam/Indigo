ac938922fe998e4fe72e9d35d2671946a6ecacbb
Mikhail Kviatkovskii <mikhail_kviatkovskii@epam.com>
2021-06-16 22:24:07 +0300

    #427: api: tests: integration: rendering tests updated

---
+++ api/tests/integration/tests/rendering/render_bold_bonds.py
@@ -0,0 +1,30 @@
+import sys
+import errno
+
+sys.path.append('../../common')
+from env_indigo import *
+from rendering import *
+
+indigo = Indigo()
+renderer = IndigoRenderer(indigo)
+out_dir = joinPath("out")
+
+if not os.path.exists(out_dir):
+    try:
+        os.makedirs(out_dir)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+indigo.setOption("render-background-color", "255, 255, 255")
+indigo.setOption("render-coloring", "true")
+indigo.setOption("ignore-stereochemistry-errors", True)
+
+m = indigo.loadMoleculeFromFile(joinPath("molecules", "bold-bonds.mol"))
+
+for v in [ "unset", "true", "false" ]:
+    if v != "unset":
+        indigo.setOption("render-bold-bond-detection", v)
+
+    renderer.renderToFile(m, joinPath("out/bold-bonds-%s.png" % v))
+    print(checkImageSimilarity('bold-bonds-%s.png' % v))
