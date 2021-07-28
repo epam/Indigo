b24be2eb842614afebb53a015914c78a18e6eaa1
Roman Porozhnetov <roman.porozhnetov@gmail.com>
2021-07-28 14:03:06 +0300

    stereo desc

--- api/tests/integration/tests/basic/molfile_stereo_desc.py
+++ api/tests/integration/tests/basic/molfile_stereo_desc.py
@@ -1,17 +1,17 @@
-import sys, threading
+import sys, os, threading
 sys.path.append('../../common')
 
 from env_indigo import *
 
 threading.stack_size(2*1024*1024)
-def stereo_desc_test():
+def stereo_desc_test( py_file ):
     indigo = Indigo()
     indigo.setOption("molfile-saving-skip-date", "1")
     indigo.setOption("molfile-saving-add-stereo-desc", "1")
     print("This test checks correct determination and saving of CIP stereo descriptors")
-
-    for filename in sorted(os.listdir(joinPath("molecules/CIP"))):
-        mol = indigo.loadMoleculeFromFile(os.path.join(joinPath("molecules/CIP"), filename))
+    print( joinPathPy( "molecules/CIP", py_file ) )
+    for filename in sorted(os.listdir(joinPathPy("molecules/CIP", py_file))):
+        mol = indigo.loadMoleculeFromFile( joinPathPy("molecules/CIP/" + filename , py_file) )
         print("%s" % filename[:-4])
         print(mol.molfile())
 
@@ -19,5 +19,5 @@ def stereo_desc_test():
     rxn = indigo.loadReactionFromFile(joinPath("reactions/crazystereo.rxn"))
     print(rxn.rxnfile())
 
-test_thread = threading.Thread( target = stereo_desc_test )
+test_thread = threading.Thread( target = stereo_desc_test, args=( os.path.abspath(__file__) ,) )
 test_thread.start()
