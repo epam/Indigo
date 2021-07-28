395b8900fe00e733b51267c0b79d402a36024978
Roman Porozhnetov <roman.porozhnetov@gmail.com>
2021-07-28 17:12:18 +0300

    tau2

--- api/tests/integration/tests/substructure/tau_sub2.py
+++ api/tests/integration/tests/substructure/tau_sub2.py
@@ -1,13 +1,15 @@
-import sys
+import sys, os
 import json
 
 sys.path.append('../../common')
 from env_indigo import *
 indigo = Indigo()
 
-mol_iter = indigo.iterateSDFile( joinPathPy( 'molecules/tautomers/targets.sdf', __file__))
+base_tau = os.path.abspath(__file__)
+
+mol_iter = indigo.iterateSDFile( joinPathPy( 'molecules/tautomers/targets.sdf', base_tau))
 target_list = [mol for mol in mol_iter]
-query_iter = indigo.iterateSDFile( joinPathPy( 'molecules/tautomers/queries.sdf', __file__))
+query_iter = indigo.iterateSDFile( joinPathPy( 'molecules/tautomers/queries.sdf', base_tau))
 queries_list = [indigo.loadQueryMolecule(q.smiles()) for q in query_iter]
 
 matches = dict()
