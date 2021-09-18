ce1c91f5c23acf2a6bf7e76c7a6acbe89a97ad9b
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2021-09-18 23:44:27 +0300

    api: cpp: tests: two sessions per thread in rendering test

--- api/cpp/tests/rendering/threads.cpp
+++ api/cpp/tests/rendering/threads.cpp
@@ -11,9 +11,9 @@ using namespace indigo_cpp;
 namespace
 {
     static std::string choices[] = {"C", "CC", "CCC", "CCCC", "CCCCC", "CCCCCC"};
-    static std::random_device rd;     // only used once to initialise (seed) engine
-    static std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
-    static std::uniform_int_distribution<int> uni(0,5);
+    static std::random_device rd;
+    static std::mt19937 rng(rd());
+    static std::uniform_int_distribution<int> uni(0, 6);
 
     std::string randomSmiles()
     {
@@ -22,12 +22,15 @@ namespace
 
     void testRender()
     {
-        const auto& session = IndigoSession();
-        const auto& renderer = IndigoRenderer(session);
         const auto& smiles = randomSmiles();
-        const auto& m = session.loadMolecule(smiles);
-        const auto& result_1 = renderer.png(m);
-        const auto& result_2 = renderer.png(m);
+        const auto& session_1 = IndigoSession();
+        const auto& renderer_1 = IndigoRenderer(session_1);
+        const auto& session_2 = IndigoSession();
+        const auto& renderer_2 = IndigoRenderer(session_2);
+        const auto& m_1 = session_1.loadMolecule(smiles);
+        const auto& m_2 = session_2.loadMolecule(smiles);
+        const auto& result_1 = renderer_1.png(m_1);
+        const auto& result_2 = renderer_2.png(m_2);
         ASSERT_EQ(result_1, result_2);
     }
 }
@@ -35,8 +38,8 @@ namespace
 TEST(RenderingThreads, Basic)
 {
     std::vector<std::thread> threads;
-    threads.reserve(1000);
-    for (auto i = 0; i < 1000; i++)
+    threads.reserve(100);
+    for (auto i = 0; i < 100; i++)
     {
         threads.emplace_back(testRender);
     }
