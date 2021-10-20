3812644fee025c4e3bc482ace09de466ab61923c
even1024 <roman.porozhnetov@gmail.com>
2021-10-20 17:41:30 +0300

    query features fix

--- core/indigo-core/molecule/molecule_savers.h
+++ core/indigo-core/molecule/molecule_savers.h
@@ -33,8 +33,8 @@ namespace indigo
     {
     public:
         static int getHCount(BaseMolecule& mol, int index, int atom_number, int atom_charge);
-        static bool MoleculeSavers::getRingBondCountFlagValue(QueryMolecule& qmol, int idx, int& value);
-        static bool MoleculeSavers::getSubstitutionCountFlagValue(QueryMolecule& qmol, int idx, int& value);
+        static bool getRingBondCountFlagValue(QueryMolecule& qmol, int idx, int& value);
+        static bool getSubstitutionCountFlagValue(QueryMolecule& qmol, int idx, int& value);
     };
 
 } // namespace indigo
