abc04e522ee353b7f2298a046fef29efa987d046
Aleksandr Savelev <Aleksandr_Savelev@epam.com>
2021-11-06 18:05:51 +0300

    fix enum values

--- api/python/indigo/__init__.py
+++ api/python/indigo/__init__.py
@@ -707,12 +707,12 @@ class IndigoObject(object):
 
         Returns:
             int: reacting center enum. One of values
-            RC_NOT_CENTER = -1
-            RC_UNMARKED = 0
-            RC_CENTER = 1
-            RC_UNCHANGED = 2
-            RC_MADE_OR_BROKEN = 4
-            RC_ORDER_CHANGED = 8
+                * RC_NOT_CENTER = -1
+                * RC_UNMARKED = 0
+                * RC_CENTER = 1
+                * RC_UNCHANGED = 2
+                * RC_MADE_OR_BROKEN = 4
+                * RC_ORDER_CHANGED = 8
         """
         value = c_int()
         self.dispatcher._setSessionId()
@@ -731,12 +731,12 @@ class IndigoObject(object):
         Args:
             reaction_bond (IndigoObject): reaction molecule bond
             rc (int): reacting center, one of the following
-                RC_NOT_CENTER = -1
-                RC_UNMARKED = 0
-                RC_CENTER = 1
-                RC_UNCHANGED = 2
-                RC_MADE_OR_BROKEN = 4
-                RC_ORDER_CHANGED = 8
+                * RC_NOT_CENTER = -1
+                * RC_UNMARKED = 0
+                * RC_CENTER = 1
+                * RC_UNCHANGED = 2
+                * RC_MADE_OR_BROKEN = 4
+                * RC_ORDER_CHANGED = 8
 
         Returns:
             int: 1 if there are no errors
@@ -906,10 +906,10 @@ class IndigoObject(object):
 
         Returns:
             int: type of stereocenter
-            ABS = 1
-            OR = 2
-            AND = 3
-            EITHER = 4
+                * ABS = 1
+                * OR = 2
+                * AND = 3
+                * EITHER = 4
         """
         self.dispatcher._setSessionId()
         return self.dispatcher._checkResult(
@@ -943,10 +943,10 @@ class IndigoObject(object):
 
         Args:
             type (int): stereo type.
-                ABS = 1
-                OR = 2
-                AND = 3
-                EITHER = 4
+                * ABS = 1
+                * OR = 2
+                * AND = 3
+                * EITHER = 4
 
         Returns:
             int: 1 if there are no errors
