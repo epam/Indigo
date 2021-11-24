441c7e92066548433a45d11260480e80400f8575
Mikhail Kviatkovskii <mikhail_kviatkovskii@epam.com>
2021-11-24 19:22:32 +0300

    ci: updated version to 1.6.0

--- api/python/setup.py
+++ api/python/setup.py
@@ -87,7 +87,7 @@ if os.path.exists("indigo_chem.egg-info"):
 
 setup(
     name="epam.indigo",
-    version="1.5.1",
+    version="1.6.0",
     description="Indigo universal cheminformatics toolkit",
     author="EPAM Systems Life Science Department",
     author_email="lifescience.opensource@epam.com",
