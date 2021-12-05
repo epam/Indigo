10fe230929e003eec4e6c0ca784a1bb0c7a9ecf8
Aleksandr Savelev <Aleksandr_Savelev@epam.com>
2021-12-05 17:16:51 +0300

    fix import tests

--- bingo/postgres/src/pg_am/pg_bingo_import.cpp
+++ bingo/postgres/src/pg_am/pg_bingo_import.cpp
@@ -464,12 +464,11 @@ public:
 
     void getNextData() override
     {
-
-        const char* data = 0;
         _importData.clear();
 
         for (int col_idx = 0; col_idx < _importColumns.size(); ++col_idx)
         {
+            const char* data = 0;
             try {
                 if (col_idx == 0)
                     data = bingoCore.bingoSDFImportGetNext();
@@ -550,11 +549,11 @@ public:
 
     void getNextData() override
     {
-        const char* data = 0;
         _importData.clear();
 
         for (int col_idx = 0; col_idx < _importColumns.size(); ++col_idx)
         {
+            const char* data = 0;
             try {
                 if (col_idx == 0)
                     data = bingoCore.bingoRDFImportGetNext();
