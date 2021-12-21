b919eebfdaa09ee546a03878b0677cc7dac2e944
SPKorhonen <samuli-petrus.korhonen@iki.fi>
2021-12-21 13:16:23 +0200

    Fix pg sys patch conflict

--- bingo/postgres/src/pg_common/bingo_pg_fix_pre.h
+++ bingo/postgres/src/pg_common/bingo_pg_fix_pre.h
@@ -10,7 +10,11 @@
 // http://www.postgresql.org/message-id/attachment/31194/VS2013_01.patch
 #if (_MSC_VER >= 1800)
 #include <math.h>
-#include <functional>
+// PostgeSQL 14+ patches system files and thus postgres.h must be included
+// before inclusion of <sys/stat.h> or patch fails.
+// Thus <functional> must not be included here as this file is included
+// before postgres.h
+// #include <functional>
 #endif
 
 #endif //#ifndef __bingo_pg_fix_pre_h__
