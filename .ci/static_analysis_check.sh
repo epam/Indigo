22539bb1fb7612bd2d10ba8ff18f80ab0712064a
Mikhail Kviatkovskii <mikhail_kviatkovskii@epam.com>
2021-12-21 13:29:37 +0300

    ci: fix

--- .ci/static_analysis_check.sh
+++ .ci/static_analysis_check.sh
@@ -7,14 +7,18 @@
 #cmake .. -DUSE_CLANG_TIDY=ON
 #cd ..
 #find . -not -path "./third_party/*" -a \( -iname "*.h" -o -iname "*.hpp" -o -iname "*.cpp" -o -iname "*.c" \) -exec clang-tidy -p build-clang-tidy --header-filter="*third-party*" {} \;
+clang-format--version
 find . -not -path "./third_party/*" -a \( -iname "*.h" -o -iname "*.hpp" -o -iname "*.cpp" -o -iname "*.c" \) -exec clang-format --verbose -Werror --dry-run {} \;
 
 # Python
 
 export PYTHONPATH=api/python:api/tests/integration:api/http:bingo/bingo-elastic/python:utils/indigo-service/service:
 
+black --version
 black --verbose --check .
+isort --version
 isort --verbose --check .
+pflake8 --version
 pflake8 --verbose .
 
 mypy api/python
