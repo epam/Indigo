b10cf944ba7b890d6888b1a1f7fd5f8d2ef0e8fa
Mikhail Kviatkovskii <mikhail_kviatkovskii@epam.com>
2021-12-22 13:14:54 +0300

    ci: fix

--- bingo/postgres/src/pg_am/pg_bingo_costestimate.c
+++ bingo/postgres/src/pg_am/pg_bingo_costestimate.c
@@ -1,19 +1,16 @@
-#include <math.h>
-
-#include "postgres.h"
-
-#include "fmgr.h"
+#include <postgres.h>
 
 #if PG_VERSION_NUM / 100 >= 1200
-#include "access/relation.h"
-#include "optimizer/optimizer.h"
+#include <optimizer/optimizer.h>
 #else
-#include "nodes/relation.h"
-#include "optimizer/predtest.h"
+#include <math.h>
+
+#include <nodes/relation.h>
+#include <optimizer/predtest.h>
 #endif
-#include "optimizer/cost.h"
-#include "utils/selfuncs.h"
-#include "utils/spccache.h"
+
+#include <optimizer/cost.h>
+#include <utils/selfuncs.h>
 
 /*
 #include "access/sysattr.h"
