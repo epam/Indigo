04c6bcf055e841330a17ca056b3477dc9a10ebf5
Natalia Odintsova <Natalia_Odintsova@epam.com>
2022-01-10 21:37:01 +0300

    Add documentation

--- api/python/indigo/ml/clustering.py
+++ api/python/indigo/ml/clustering.py
@@ -4,6 +4,16 @@ from sklearn.cluster import SpectralClustering
 def clustering(
     assay_values: list[list], method=SpectralClustering, **kwargs
 ) -> list:
+    """Clustering procedure for given list of assay values.
+
+    Args:
+        assay_values: Ordered list of lists of assay values.
+        method: clustering method, default: SpectralClustering.
+    Kwargs:
+        params for clustering method.
+    Returns:
+        ordered list of cluster labels.
+    """
     cl = method(**kwargs)
     cl.fit(assay_values)
     return [label for label in cl.labels_]
