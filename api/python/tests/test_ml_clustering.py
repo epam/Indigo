102011ee88d3a24dcde0a0f848560e3b4721c765
Natalia Odintsova <Natalia_Odintsova@epam.com>
2022-01-11 15:34:35 +0300

    Add one-dimensional option to test

--- api/python/tests/test_ml_clustering.py
+++ api/python/tests/test_ml_clustering.py
@@ -16,6 +16,15 @@ from indigo.ml.clustering import clustering
             [0, 0, 0, 1, 1],
         ),
         (
+            [[[1], [2], [1], [4], [3]]],
+            {
+                "n_clusters": 2,
+                "assign_labels": "discretize",
+                "random_state": 0,
+            },
+            [0, 0, 0, 1, 1],
+        ),
+        (
             [[[1, 1], [2, 1], [1, 0], [4, 7], [3, 5]]],
             {"method": KMeans, "n_clusters": 2, "random_state": 0},
             [1, 1, 1, 0, 0],
