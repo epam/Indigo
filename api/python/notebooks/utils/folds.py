94f3111727567a5b67605a2afced6f4fc8e65f9c
Aleksei Kornev2 <aleksei_kornev2@epam.com>
2022-01-17 17:13:56 +0300

    refactor: renamed TARGET to ASSAY, added static plots, large pieces of code moved to notebooks/utils

---
+++ api/python/notebooks/utils/folds.py
@@ -0,0 +1,49 @@
+import numpy as np
+from sklearn.model_selection import KFold, train_test_split
+from sklearn.metrics import r2_score
+
+
+def oof(reg, bundle, folds=5, seed=42, assay=''):
+    """Calculate out-of-fold errors of a given model.
+
+    Args:
+        reg : Regression model that should support fit/predict methods
+        bundle (Tuple): contains train and test inputs and predictions
+
+    Returns:
+        Tuple: return list of test errors and list of predictions
+    """
+    kf = KFold(n_splits=folds, shuffle=True, random_state=seed)
+    X_train, X_test, y_train, y_test = bundle
+
+    y_train = y_train[assay].values
+    y_test = y_test[assay].values
+    train_errors = []
+    oof_errors = []
+    errors = []
+
+    for i, (train_idx, test_idx) in enumerate(kf.split(X_train, y_train)):
+        X_tr = X_train[train_idx]
+        y_tr = y_train[train_idx]
+        X_te = X_train[test_idx]
+
+        reg.fit(X_tr, y_tr)
+
+        # predict values given validation, train and test sets
+        oof_pred = reg.predict(X_te)
+        train_pred = reg.predict(X_tr)
+        test_pred = reg.predict(X_test)
+        # calculate r2 scores
+        oof_score = r2_score(y_train[test_idx], oof_pred)
+        train_score = r2_score(y_tr, train_pred)
+        test_score = r2_score(y_test, test_pred)
+        
+        train_errors.append(train_score)
+        oof_errors.append(oof_score)
+        errors.append(test_score)
+
+    print("MEAN TRAIN: ", np.mean(train_errors))
+    print("MEAN VALIDATION: ", np.mean(oof_errors))
+    print("MEAN TEST", np.mean(errors))
+    
+    return errors, test_pred
---
+++ api/python/notebooks/utils/plots.py
@@ -0,0 +1,131 @@
+import base64
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from indigo import Indigo
+from typing import List
+from indigo.renderer import IndigoRenderer
+from bokeh.plotting import figure, show
+from bokeh.models import HoverTool, Legend, ColumnDataSource
+from typing import List
+
+
+indigo = Indigo()
+renderer = IndigoRenderer(indigo)
+indigo.setOption("render-output-format", "svg")
+indigo.setOption("render-image-size", 350, 170)
+
+
+def molecule_image(smiles: str) -> str:
+    mol = indigo.loadMolecule(smiles)
+    mol.aromatize()
+    svg = renderer.renderToString(mol)
+    return "data:image/svg+xml;base64," + base64.b64encode(svg.encode()).decode()
+
+
+def static_avp_avr_graphs(preds, actual, r2: str, title: str = ''):
+    """Static prediction/actual and prediction/residual"""
+    title = title
+    x, y = pd.Series(actual, name="Actual"), pd.Series(preds, name="Prediction")
+    z = pd.Series(preds, name="Residual")
+    fig, ax = plt.subplots(1,2, figsize = (15,5))
+    sns.regplot(x=x, y=y, ax = ax[0])
+    sns.residplot(x=x, y=z, ax = ax[1])
+    ax[0].set_title(f'{title} Actual vs Predicted, r2={r2:.3f}')
+    ax[1].set_title(f'Residual plot for {title}, r2={r2:.3f}')
+
+
+def avp_plot(actual: List[float], predicted: List[float], ids: List[str], smiles_list: List[str], r2: float, title='') -> None:
+    df = pd.DataFrame({'x': actual, 'y': predicted})
+    df["id"] = ids
+    df["image"] = [molecule_image(smiles) for smiles in smiles_list]
+    df['residual'] = actual - predicted
+    plot_height = 400
+    plot_width = int(plot_height * 1.4)
+    plot_figure = figure(
+        title=f"{title} Actual vs Predicted, r2={r2:.3f}",
+        plot_width=plot_width,
+        plot_height=plot_height,
+        x_axis_label="Actual",
+        y_axis_label="Predicted",
+        tools="pan, wheel_zoom, reset",
+    )
+    plot_figure.add_tools(
+        HoverTool(
+        tooltips="""
+            <div>
+            <div>
+            <img src='@image' style='float: left; margin: 5px 5px 5px 5px'/>
+            </div>
+            <div>
+            <span style='font-size: 16px; color: #224499'>Compound:</span>
+            <span style='font-size: 18px'>@id</span>
+            </div>
+            <div>
+            <span style='font-size: 16px; color: #224499'>Residual:</span>
+            <span style='font-size: 18px'>@residual</span>
+            </div>
+            </div>
+            """
+        )
+    )
+    plot_figure.add_layout(Legend(), "right")
+    datasource = ColumnDataSource(df)
+    # pylint: disable=too-many-function-args
+    plot_figure.circle(
+        "x",
+        "y",
+        source=datasource,
+        line_alpha=0.6,
+        fill_alpha=0.6,
+        size=4,
+    )
+    show(plot_figure)
+
+
+def avr_plot(actual: List[float], predicted: List[float], ids: List[str], smiles_list: List[str], r2: float, title='') -> None:
+    df = pd.DataFrame({'x': actual, 'y': actual - predicted})
+    df["id"] = ids
+    df["image"] = [molecule_image(smiles) for smiles in smiles_list]
+    df['residual'] = actual - predicted
+    plot_height = 400
+    plot_width = int(plot_height * 1.4)
+    plot_figure = figure(
+        title=f"{title} Actual vs Residual, r2={r2:.3f}",
+        plot_width=plot_width,
+        plot_height=plot_height,
+        x_axis_label="Actual",
+        y_axis_label="Residual",
+        tools="pan, wheel_zoom, reset",
+    )
+    plot_figure.add_tools(
+        HoverTool(
+        tooltips="""
+            <div>
+            <div>
+            <img src='@image' style='float: left; margin: 5px 5px 5px 5px'/>
+            </div>
+            <div>
+            <span style='font-size: 16px; color: #224499'>Compound:</span>
+            <span style='font-size: 18px'>@id</span>
+            </div>
+            <div>
+            <span style='font-size: 16px; color: #224499'>Residual:</span>
+            <span style='font-size: 18px'>@residual</span>
+            </div>
+            </div>
+            """
+        )
+    )
+    plot_figure.add_layout(Legend(), "right")
+    datasource = ColumnDataSource(df)
+    # pylint: disable=too-many-function-args
+    plot_figure.circle(
+        "x",
+        "y",
+        source=datasource,
+        line_alpha=0.6,
+        fill_alpha=0.6,
+        size=4,
+    )
+    show(plot_figure)
