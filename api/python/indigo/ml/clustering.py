a0ace581861f42b9627cfd7dbe591737f1a085f7
Natalia Odintsova <Natalia_Odintsova@epam.com>
2022-01-27 19:41:52 +0300

    Update clustering notebook. Add sorting cluster coords
    Notebook: increase fp size, unpack bits, tune min_dist and n_neighbors

--- api/python/indigo/ml/clustering.py
+++ api/python/indigo/ml/clustering.py
@@ -1,7 +1,7 @@
-from typing import List, Type, Union
+from typing import List, Type, Union, Tuple
 
 from sklearn.base import ClusterMixin  # type: ignore
-from sklearn.cluster import SpectralClustering  # type: ignore
+from sklearn.cluster import SpectralClustering, KMeans  # type: ignore
 
 
 def clustering(
@@ -22,3 +22,32 @@ def clustering(
     cl = method(**kwargs)
     labels = cl.fit_predict(assay_values)
     return labels.tolist()
+
+
+def kmeans_clustering(
+    assay_values: List[List[Union[int, float]]], **kwargs
+) -> Tuple[List[int], List[List[float]]]:
+    """Clustering procedure with KMeans method.
+
+    Args:
+        assay_values: Ordered list of lists of assay values.
+    Kwargs:
+        params for KMeans clustering method.
+    Returns:
+        tuple with list of cluster labels and list of cluster centers
+        coordinates.
+    """
+    cl = KMeans(**kwargs)
+    labels = cl.fit_predict(assay_values)
+    return labels.tolist(), cl.cluster_centers_.tolist()
+
+
+def average_distance():
+    pass
+
+
+def split_coords_by_clusters(coordinates, clusters, n_clusters):
+    sorted_coords = [[] for _ in range(n_clusters)]
+    for i, cluster in enumerate(clusters):
+        sorted_coords[cluster].append(coordinates[i])
+    return sorted_coords
