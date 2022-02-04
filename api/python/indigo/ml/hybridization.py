f10fddfcefa06c68621866272a14f9938488dec6
Natalia Odintsova <Natalia_Odintsova@epam.com>
2022-02-04 16:46:17 +0300

    Start lone_pairs electrons calculation

--- api/python/indigo/ml/hybridization.py
+++ api/python/indigo/ml/hybridization.py
@@ -11,7 +11,7 @@ for atom in mol.iterateAtoms():
     for nei in atom.iterateNeighbors():
         print(
             f"neighbor atom {nei.symbol()} is connected by {nei.bond().index()}"
-            f"bond"
+            f" bond"
         )
         bonds += nei.bond().bondOrder()
     print(f"Atom {atom.symbol()} has {bonds} bonds")
@@ -24,21 +24,51 @@ def num_bonds(atom):
     return bonds
 
 
-def lone_pairs(atom):
-    pass
+# def val_electrons(atom, n_bonds):
+#     if n_bonds == 1:
+#         return 1
+#     atomic_number = atom.atomicNumber()
+#     if atomic_number <= 2:
+#         return atomic_number
+#     elif atomic_number <= 10:
+#         if n_bonds == atomic_number - 2:
+#             return n_bonds
+#         return atomic_number - 2
+#     elif atomic_number <= 18:
+#         return atomic_number - 10
+
+
+def lone_pairs(atom, n_bonds):
+    if n_bonds == 1:
+        return 0
+    atomic_number = atom.atomicNumber()
+    if atomic_number <= 4:
+        return 0
+    elif atomic_number <= 10:
+        if n_bonds == atomic_number - 2:
+            return 0
+        return atomic_number - 2 - n_bonds
+    elif atomic_number <= 12:
+        return 0
+    elif atomic_number <= 18:
+        if n_bonds == atomic_number - 2:
+            return 0
+        return atomic_number - 2 - n_bonds
 
 
 def get_hybridization(atom):
-    if atom.atomicNumber() == 0:  # if the atomic number is undefined or ambiguous
+    # if the atomic number is undefined or ambiguous
+    if atom.atomicNumber() == 0:
         return None
+
+    # don't bother with the actinides and beyond
     elif atom.atomicNumber() >= 89:
-        # don't bother with the actinides and beyond
         return "s"  # or "unhybridized"?
 
     n_bonds = num_bonds(atom)
-    if bonds >= 8:
+    if n_bonds >= 8:
         return "sp2"  # aromatic
 
-    n_orbs = n_bonds + lone_pairs(atom)
     # number of hybridized orbitals = number of bonded atoms plus number of
     # lone pair electrons
+    n_orbs = n_bonds + lone_pairs(atom, n_bonds)
