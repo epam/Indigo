cdc0ef52823770f3475aa5ed0d1e1eb45eadd929
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2022-06-17 12:33:17 +0400

    Version 1.7.3

--- api/http/setup.py
+++ api/http/setup.py
@@ -19,7 +19,7 @@ entry_points = {
 
 setup_kwargs = {
     "name": "indigo-service",
-    "version": "1.7.2",
+    "version": "1.7.3",
     "description": "",
     "long_description": None,
     "author": "Ruslan Khyurri",
