eed5537b6bcbbaab3b7e32a5f9b5865733141fa2
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2022-07-03 18:04:16 +0400

    Temporarily Added debug symbols

--- cmake/setup.cmake
+++ cmake/setup.cmake
@@ -45,8 +45,9 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake")
 
 # Compile flags
 if (UNIX OR MINGW)
-    string(APPEND CMAKE_C_FLAGS " -rdynamic -fvisibility=hidden $ENV{CFLAGS}")
-    string(APPEND CMAKE_CXX_FLAGS " -rdynamic -fvisibility=hidden -fvisibility-inlines-hidden $ENV{CXXFLAGS}")
+    # TODO: REMOVE -g before merging!
+    string(APPEND CMAKE_C_FLAGS " -g -rdynamic -fvisibility=hidden $ENV{CFLAGS}")
+    string(APPEND CMAKE_CXX_FLAGS " -g -rdynamic -fvisibility=hidden -fvisibility-inlines-hidden $ENV{CXXFLAGS}")
 
     if(BUILD_STANDALONE AND NOT EMSCRIPTEN)
         if (CMAKE_CXX_COMPILER_ID STREQUAL GNU)
