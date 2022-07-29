2c17551cce177d75741303ecfeda03a9c787ccce
Mikhail Kviatkovskii <mikhail_kviatkovskii@epam.com>
2022-07-29 22:47:55 +0400

    #777: utils: indigo-service: reorganize codebase, simplify docker compose, update ui (#796)

--- utils/indigo-service/db/configure_postgres.sh
+++
@@ -1,6 +0,0 @@
-cd /opt/bingo-postgres/
-source bingo-pg-install.sh -y
-psql -U postgres -c "create database indigoservice"
-psql -U postgres -d indigoservice -f ./bingo_install.sql
-psql -U postgres -d indigoservice -f /opt/init_database.sql
-cp /opt/postgresql.conf /var/lib/postgresql/data
