8fc764e360e700ba0865d4f4f00b0e50877e7db2
Roman Porozhnetov <Roman_Porozhnetov@epam.com>
2022-08-05 05:23:16 +0300

    bool & char array separated

--- core/indigo-core/common/base_cpp/array.h
+++ core/indigo-core/common/base_cpp/array.h
@@ -866,24 +866,6 @@ namespace indigo
     private:
         Array(const Array&);                            // no implicit copy
         Array<int>& operator=(const Array<int>& right); // no copy constructor
-
-        template <typename T1, typename T2>
-        class _CmpFunctorCaller
-        {
-        public:
-            _CmpFunctorCaller(int (*cmp)(T1, T2, void*), void* context) : _context(context), _cmp(cmp)
-            {
-            }
-
-            int operator()(T1 arg1, T2 arg2) const
-            {
-                return _cmp(arg1, arg2, _context);
-            }
-
-        private:
-            void* _context;
-            int (*_cmp)(T1, T2, void*);
-        };
     };
 
     template <>
@@ -1227,130 +1209,6 @@ namespace indigo
             return _array + _length;
         }
 
-        // CMP_FUNCTOR has two arguments and returns sign of comparation
-        template <typename CmpFunctor>
-        void insertionSort(int start, int end, CmpFunctor cmp)
-        {
-            int i, j;
-            char tmp[sizeof(T)]; // can't use T directly because it may have destructor
-
-            for (i = start + 1; i <= end; i++)
-            {
-                j = i;
-                while (j > start && cmp(_array[j - 1], _array[j]) > 0)
-                {
-                    T* a1 = _array + j - 1;
-                    T* a2 = a1 + 1;
-                    memcpy(&tmp, a1, sizeof(char));
-                    memcpy(a1, a2, sizeof(char));
-                    memcpy(a2, &tmp, sizeof(char));
-                    j--;
-                }
-            }
-        }
-
-        // CMP_FUNCTOR has two arguments and returns sign of comparation
-        template <typename CmpFunctor>
-        void qsort(int start, int end, CmpFunctor cmp)
-        {
-            // Sort elements from start to end
-            if (start >= end)
-                return;
-            if (end - start < 10)
-                insertionSort(start, end, cmp);
-
-            struct
-            {
-                T *lo, *hi;
-            } stack[32], *sp;
-
-            char tmp[sizeof(T)]; // can't use T directly because it may have destructor
-
-            sp = stack;
-
-            // push our initial values onto the stack
-            sp->lo = _array + start;
-            sp->hi = _array + end + 1;
-            sp++;
-
-            while (sp > stack)
-            {
-                // pop lo and hi off the stack
-                sp--;
-                T *high = sp->hi, *low = sp->lo;
-                T* hi = high - 1;
-                T* lo = low;
-                T* pivot = low;
-
-                while (1)
-                {
-                    while (lo < high && lo != pivot && cmp(*lo, *pivot) < 0)
-                        lo++;
-
-                    while (hi > low && (hi == pivot || cmp(*hi, *pivot) >= 0))
-                        hi--;
-
-                    if (lo < hi)
-                    {
-                        memcpy(&tmp, lo, sizeof(T));
-                        memcpy(lo, hi, sizeof(T));
-                        memcpy(hi, &tmp, sizeof(T));
-
-                        if (lo == pivot)
-                            pivot = hi;
-                        else if (hi == pivot)
-                            pivot = lo;
-
-                        hi--;
-                    }
-                    else
-                    {
-                        hi++;
-
-                        if (hi == high)
-                            // done with this segment
-                            break;
-
-                        // push the larger segment onto the stack and continue
-                        // sorting the smaller segment.
-                        if ((hi - low) > (high - hi))
-                        {
-                            sp->lo = low;
-                            sp->hi = hi;
-                            sp++;
-
-                            hi = high;
-                            low = lo;
-                        }
-                        else
-                        {
-                            sp->hi = high;
-                            sp->lo = hi;
-                            sp++;
-
-                            high = hi;
-                            lo = low;
-                        }
-
-                        pivot = lo;
-                        hi--;
-                    }
-                }
-            }
-        }
-
-        template <typename T1, typename T2>
-        void qsort(int start, int end, int (*cmp)(T1, T2, void*), void* context)
-        {
-            this->qsort(start, end, _CmpFunctorCaller<T1, T2>(cmp, context));
-        }
-
-        template <typename T1, typename T2>
-        void qsort(int (*cmp)(T1, T2, void*), void* context)
-        {
-            this->qsort(0, _length - 1, cmp, context);
-        }
-
         // Array<char>-specific
         void appendString(const char* str, bool keep_zero)
         {
@@ -1410,24 +1268,6 @@ namespace indigo
     private:
         Array(const Array&);                            // no implicit copy
         Array<int>& operator=(const Array<int>& right); // no copy constructor
-
-        template <typename T1, typename T2>
-        class _CmpFunctorCaller
-        {
-        public:
-            _CmpFunctorCaller(int (*cmp)(T1, T2, void*), void* context) : _context(context), _cmp(cmp)
-            {
-            }
-
-            int operator()(T1 arg1, T2 arg2) const
-            {
-                return _cmp(arg1, arg2, _context);
-            }
-
-        private:
-            void* _context;
-            int (*_cmp)(T1, T2, void*);
-        };
     };
 
 } // namespace indigo
