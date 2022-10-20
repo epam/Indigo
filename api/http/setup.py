2bf2ca41b465fa018f2f57024a2c23d8e5e0c6c5
Aleksandr Savelev <Aleksandr_Savelev@epam.com>
2022-10-20 21:53:01 +0300

    bump to 1.8.2 rc.2

--- api/http/setup.py
+++ api/http/setup.py
@@ -19,7 +19,7 @@ entry_points = {
 
 setup_kwargs = {
     "name": "indigo-service",
-    "version": "1.8.0.rc1",
+    "version": "1.8.0.rc2",
     "description": "",
     "long_description": None,
     "author": "Ruslan Khyurri",
