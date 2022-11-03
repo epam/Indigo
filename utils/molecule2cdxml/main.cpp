f234cfed7404e4735bda4591988b5a78286e148a
Mikhail Kviatkovskii <Mikhail_Kviatkovskii@epam.com>
2022-11-03 19:47:49 +0400

    utils: improved smiles2cdxml to molecule2cdmxl to support all formats supported by Indigo

--- utils/molecule2cdxml/main.cpp
+++ utils/molecule2cdxml/main.cpp
@@ -16,11 +16,13 @@
  * limitations under the License.
  ***************************************************************************/
 
+#include <iostream>
 
 #include <base_cpp/output.h>
 #include <base_cpp/scanner.h>
+#include <molecule/molecule.h>
+#include <molecule/molecule_auto_loader.h>
 #include <molecule/molecule_cdxml_saver.h>
-#include <molecule/smiles_loader.h>
 
 using namespace indigo;
 
@@ -28,28 +30,33 @@ using namespace indigo;
 #define EX_USAGE 64
 #endif
 
+std::string molecule2cdxml(const std::string& input)
+{
+    BufferScanner scanner(input.c_str());
+    MoleculeAutoLoader moleculeLoader(scanner);
+    Molecule molecule;
+    moleculeLoader.loadMolecule(molecule);
+    Array<char> array;
+    ArrayOutput output(array);
+    MoleculeCdxmlSaver cdxmlSaver(output);
+    cdxmlSaver.saveMolecule(molecule);
+    return array.ptr();
+}
+
 int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
-        printf("usage: smiles2cdxml <SMILES string>\n");
+        std::cout << "usage: molecule2cdxml <SMILES string>\n";
         return EX_USAGE;
     }
     try
     {
-        BufferScanner scanner(argv[1]);
-        SmilesLoader smilesLoader(scanner);
-        Molecule molecule;
-        smilesLoader.loadMolecule(molecule);
-        Array<char> array;
-        ArrayOutput output(array);
-        MoleculeCdxmlSaver cdxmlSaver(output);
-        cdxmlSaver.saveMolecule(molecule);
-        printf("%s", array.ptr());
+        std::cout << molecule2cdxml(argv[1]);
     }
-    catch (const Exception& e)
+    catch (const Exception &e)
     {
-        printf("Fatal error: %s\n", e.what());
+        std::cout << "Fatal error: " << e.what() << '\n';
         return 1;
     }
 }
