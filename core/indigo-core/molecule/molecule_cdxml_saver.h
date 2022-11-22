1e37c45bf98e3642d806a0484a9f609610dd8cec
Roman Porozhnetov <roman_porozhnetov@epam.com>
2022-11-22 09:07:15 +0100

    cdxml export layout fix

--- core/indigo-core/molecule/molecule_cdxml_saver.h
+++ core/indigo-core/molecule/molecule_cdxml_saver.h
@@ -66,7 +66,7 @@ namespace indigo
 
         static const int SCALE = 30;
         static const int MAX_PAGE_HEIGHT = 64;
-        const float PLUS_HALF_HEIGHT = 7.5/2;
+        const float PLUS_HALF_HEIGHT = 7.5 / 2;
 
         struct Bounds
         {
