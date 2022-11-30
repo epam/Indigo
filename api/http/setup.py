ded13d6dca16f6cf01cfacd1bd3a0c32e9adc9d1
Mikhail Kviatkovskii <mikhail_kviatkovskii@epam.com>
2022-12-01 02:31:17 +0400

    Update version to 1.8.3

--- api/http/setup.py
+++ api/http/setup.py
@@ -19,7 +19,7 @@ entry_points = {
 
 setup_kwargs = {
     "name": "indigo-service",
-    "version": "1.8.2",
+    "version": "1.8.3",
     "description": "",
     "long_description": None,
     "author": "Ruslan Khyurri",
