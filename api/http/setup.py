e04c2a1c3d2a435795d9cd59ca9036377108be91
Roman Porozhnetov <roman_porozhnetov@epam.com>
2022-12-19 12:45:27 +0100

    bump version to 1.9.0.rc.1

--- api/http/setup.py
+++ api/http/setup.py
@@ -19,7 +19,7 @@ entry_points = {
 
 setup_kwargs = {
     "name": "indigo-service",
-    "version": "1.9.0.dev1",
+    "version": "1.9.0.rc1",
     "description": "",
     "long_description": None,
     "author": "Ruslan Khyurri",
--- utils/indigo-ml/setup.py
+++ utils/indigo-ml/setup.py
@@ -95,7 +95,7 @@ if os.path.exists("indigo_chem.egg-info"):
 
 setup(
     name="epam.indigo",
-    version="1.9.0.dev1",
+    version="1.9.0.rc1",
     description="Indigo universal cheminformatics toolkit",
     author="EPAM Systems Life Science Department",
     author_email="lifescience.opensource@epam.com",
