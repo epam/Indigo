744c3d3c05b2f877d28499bb8e36a1fb790d5f69
Roman Porozhnetov <roman_porozhnetov@epam.com>
2022-12-26 10:22:39 +0100

    test fix

--- core/indigo-core/molecule/src/molecule_cdxml_saver.cpp
+++ core/indigo-core/molecule/src/molecule_cdxml_saver.cpp
@@ -1520,11 +1520,6 @@ void MoleculeCdxmlSaver::endDocument()
 {
     if (_is_binary)
     {
-        std::ifstream t("C:\\cdx\\benz.xml");
-        std::stringstream buffer;
-        buffer << t.rdbuf();
-        _doc->Parse(buffer.str().c_str());
-
         _output.writeString(kCDX_HeaderString);
         _output.writeBinaryInt(kCDXMagicNumber);
         _output.write(kCDXReserved, sizeof(kCDXReserved));
