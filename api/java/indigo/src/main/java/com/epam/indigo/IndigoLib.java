9e41c971b23a733f16e0b6f57e9ce077275f218c
Roman Porozhnetov <roman_porozhnetov@epam.com>
2023-01-26 20:56:49 +0100

    b64cdx java&dotnet fix

--- api/java/indigo/src/main/java/com/epam/indigo/IndigoLib.java
+++ api/java/indigo/src/main/java/com/epam/indigo/IndigoLib.java
@@ -140,6 +140,8 @@ public interface IndigoLib extends Library {
 
     Pointer indigoCdxml(int object);
 
+    Pointer indigoCdxBase64(int object);
+
     int indigoSaveMDLCT(int item, int output);
 
     int indigoLoadReaction(int source);
