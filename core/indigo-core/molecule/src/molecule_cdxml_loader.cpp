8b8e3559abf16a5ac1046cb6cae0f112f5c510d2
Roman Porozhnetov <roman_porozhnetov@epam.com>
2023-02-03 08:08:31 +0100

    clang fix

--- core/indigo-core/molecule/src/molecule_cdxml_loader.cpp
+++ core/indigo-core/molecule/src/molecule_cdxml_loader.cpp
@@ -81,7 +81,7 @@ CDXReader::CDXReader(Scanner& scanner) : _scanner(scanner)
 }
 
 MoleculeCdxmlLoader::MoleculeCdxmlLoader(Scanner& scanner, bool is_binary)
-    : _scanner(scanner), _is_binary(is_binary), _has_bounding_box(false), _pmol(nullptr), _pqmol(nullptr), ignore_bad_valence( false )
+    : _scanner(scanner), _is_binary(is_binary), _has_bounding_box(false), _pmol(nullptr), _pqmol(nullptr), ignore_bad_valence(false)
 {
 }
 
