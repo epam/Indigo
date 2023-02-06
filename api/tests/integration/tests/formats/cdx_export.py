7cad99521c93d7f28ac9c9a1b27e27ac12c23131
Roman Porozhnetov <roman_porozhnetov@epam.com>
2023-02-06 12:01:38 +0100

    test fix

--- api/tests/integration/tests/formats/cdx_export.py
+++ api/tests/integration/tests/formats/cdx_export.py
@@ -113,7 +113,7 @@ for filename in files:
         with open(os.path.join(ref_path, filename + ".cdxml"), "r") as file:
             refcdxml = file.read()
         print(filename + (":success" if res == refcdxml else ":failed"))
-        if( filename == "Suzuki_Rxn2" && res != refcdxml):
+        if( filename == "Suzuki_Rxn2" and res != refcdxml):
             print( res )
             print( find_diff( res, refcdxml) )
         # diff = find_diff(res, refcdxml)
