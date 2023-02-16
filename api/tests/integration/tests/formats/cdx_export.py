240e6b556a44a64623a034b2387259909445c402
Roman Porozhnetov <roman_porozhnetov@epam.com>
2023-02-16 09:15:42 +0100

    test fix

--- api/tests/integration/tests/formats/cdx_export.py
+++ api/tests/integration/tests/formats/cdx_export.py
@@ -94,9 +94,9 @@ for filename in files:
         resb64 = rea.b64cdx()
         res = rea.cdxml()
         # with open(os.path.join(ref_path, filename + ".cdxml"), 'w') as file:
-        #   data = file.write(res)
+        #     data = file.write(res)
         # with open(os.path.join(ref_path, filename + ".b64cdx"), 'w') as file:
-        #   data = file.write(resb64)
+        #     data = file.write(resb64)
         with open(os.path.join(ref_path, filename + ".b64cdx"), "r") as file:
             refb64 = file.read()
         print(filename + (":success" if refb64 == resb64 else ":failed"))
