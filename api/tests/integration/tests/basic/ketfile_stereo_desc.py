ab6bf90cd997a3b5a541dd4693f487cccb3df51b
Roman Porozhnetov <roman_porozhnetov@epam.com>
2023-03-07 09:49:31 +0100

    stack fix

--- api/tests/integration/tests/basic/ketfile_stereo_desc.py
+++ api/tests/integration/tests/basic/ketfile_stereo_desc.py
@@ -2,10 +2,8 @@ import difflib
 import os
 import sys
 
-
 def find_diff(a, b):
     return "\n".join(difflib.unified_diff(a.splitlines(), b.splitlines()))
-
 
 sys.path.append(
     os.path.normpath(
@@ -14,51 +12,53 @@ sys.path.append(
 )
 
 from env_indigo import *
+threading.stack_size(2 * 1024 * 1024)
 
-ref_path = joinPathPy("ref/", __file__)
-root = joinPathPy("molecules/CIP/", __file__)
+def stereo_desc_test(py_file, out_queue):
+    ref_path = joinPathPy("ref/", __file__)
+    root = joinPathPy("molecules/CIP/", __file__)
 
-indigo = Indigo()
-indigo.setOption("json-saving-pretty", True)
-indigo.setOption("json-saving-add-stereo-desc", True)
+    indigo = Indigo()
+    indigo.setOption("json-saving-pretty", True)
+    indigo.setOption("json-saving-add-stereo-desc", True)
 
-for filename in sorted(os.listdir(root)):
-    try:
+    for filename in sorted(os.listdir(root)):
         mol = indigo.loadMoleculeFromFile(os.path.join(root, filename))
-    except Exception as e:
-        print(e)
-        continue
-    ketfile = joinPathPy(
-        os.path.join(ref_path, filename[:-4] + ".ket"), __file__
-    )
+        ketfile = joinPathPy(os.path.join(ref_path, filename[:-4] + ".ket"), __file__)
+        with open(ketfile, "r") as file:
+            ket_ref = file.read()
+            diff = find_diff(ket_ref, mol.json())
+            if not diff:
+                print(filename + ":SUCCEED")
+            else:
+                print(filename + ":FAILED")
+                print(diff)
+
+    indigo.setOption("ignore-stereochemistry-errors", "true")
+    filename = "crazystereo.rxn"
+
+    rxn = indigo.loadReactionFromFile(os.path.join(joinPathPy("reactions/", __file__), filename))
+
+    ketfile = joinPathPy(os.path.join(ref_path, "crazystereo.ket"), __file__)
     with open(ketfile, "r") as file:
         ket_ref = file.read()
-        try:
-            # diff = find_diff(ket_ref, mol.json())
-            diff = ''
-        except:
-            print(e)
-            diff = "exception"
+        diff = find_diff(ket_ref, rxn.json())
         if not diff:
             print(filename + ":SUCCEED")
         else:
             print(filename + ":FAILED")
             print(diff)
 
-indigo.setOption("ignore-stereochemistry-errors", "true")
-filename = "crazystereo.rxn"
+if isJython():
+    from Queue import Queue
+else:
+    from queue import Queue
 
-rxn = indigo.loadReactionFromFile(
-    os.path.join(joinPathPy("reactions/", __file__), filename)
+th_queue = Queue()
+test_thread = threading.Thread(
+    target=stereo_desc_test, args=(os.path.abspath(__file__), th_queue)
 )
+test_thread.start()
+test_thread.join()
+#print(th_queue.get())
 
-ketfile = joinPathPy(os.path.join(ref_path, "crazystereo.ket"), __file__)
-with open(ketfile, "r") as file:
-    ket_ref = file.read()
-    # diff = find_diff(ket_ref, rxn.json())
-    diff = ''
-    if not diff:
-        print(filename + ":SUCCEED")
-    else:
-        print(filename + ":FAILED")
-        print(diff)
