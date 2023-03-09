8b67405b6e125a1c5d4fd916dca322d378b7f96c
Roman Porozhnetov <roman_porozhnetov@epam.com>
2023-03-09 05:37:41 +0100

    stringpool fix

--- core/indigo-core/common/base_cpp/string_pool.h
+++ core/indigo-core/common/base_cpp/string_pool.h
@@ -21,7 +21,7 @@
 
 #include "base_cpp/auto_iter.h"
 #include "base_cpp/pool.h"
-#include "base_cpp/ptr_array.h"
+#include "base_cpp/obj_array.h"
 
 #ifdef _WIN32
 #pragma warning(push)
@@ -96,7 +96,7 @@ namespace indigo
         int _add(const char* str, int size);
 
         Pool<int> _pool;
-        PtrArray<Array<char>> _storage;
+        ObjArray<Array<char>> _storage;
 
     private:
         StringPool(const StringPool&); // no implicit copy
