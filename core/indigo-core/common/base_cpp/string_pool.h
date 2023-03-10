afc8d4df211687e059e1a54cfbdc1714007c42b8
Roman Porozhnetov <roman_porozhnetov@epam.com>
2023-03-10 07:27:39 +0100

    clang issues

--- core/indigo-core/common/base_cpp/string_pool.h
+++ core/indigo-core/common/base_cpp/string_pool.h
@@ -20,8 +20,8 @@
 #define __string_pool_h__
 
 #include "base_cpp/auto_iter.h"
-#include "base_cpp/pool.h"
 #include "base_cpp/obj_array.h"
+#include "base_cpp/pool.h"
 
 #ifdef _WIN32
 #pragma warning(push)
