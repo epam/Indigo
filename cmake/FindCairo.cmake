e9d8bf8b75970504775dac12760477de5f975fea
Mikhail Kviatkovskii <mikhail_kviatkovskii@epam.com>
2023-05-21 16:08:34 +0400

    #1123: conda pkg: simplify dependency search

--- cmake/FindCairo.cmake
+++ cmake/FindCairo.cmake
@@ -1,95 +1,35 @@
-# - Try to find Cairo
-# Once done, this will define
-#
-#  CAIRO_FOUND - system has Cairo
-#  CAIRO_INCLUDE_DIRS - the Cairo include directories
-#  CAIRO_LIBRARIES - link these to use Cairo
-#
-# Copyright (C) 2012 Raphael Kubo da Costa <rakuco@webkit.org>
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1.  Redistributions of source code must retain the above copyright
-#     notice, this list of conditions and the following disclaimer.
-# 2.  Redistributions in binary form must reproduce the above copyright
-#     notice, this list of conditions and the following disclaimer in the
-#     documentation and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND ITS CONTRIBUTORS ``AS
-# IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR ITS
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+include(FindPackageHandleStandardArgs)
 
-FIND_PACKAGE(PkgConfig)
-PKG_CHECK_MODULES(PC_CAIRO cairo QUIET)
+find_library(Cairo_LIBRARY NAMES cairo)
+find_path(Cairo_INCLUDE_DIR NAMES cairo.h)
 
-FIND_PATH(CAIRO_INCLUDE_DIRS
-        NAMES cairo.h
-        HINTS ${PC_CAIRO_INCLUDEDIR}
-        ${PC_CAIRO_INCLUDE_DIRS}
-        PATH_SUFFIXES cairo
-        )
 
-FIND_LIBRARY(CAIRO_LIBRARIES
-        NAMES cairo
-        HINTS ${PC_CAIRO_LIBDIR}
-        ${PC_CAIRO_LIBRARY_DIRS}
-        )
+IF (Cairo_INCLUDE_DIRS)
+    IF (EXISTS "${Cairo_INCLUDE_DIRS}/cairo-version.h")
+        FILE(READ "${Cairo_INCLUDE_DIRS}/cairo-version.h" Cairo_VERSION_CONTENT)
 
-IF (CAIRO_INCLUDE_DIRS)
-    IF (EXISTS "${CAIRO_INCLUDE_DIRS}/cairo-version.h")
-        FILE(READ "${CAIRO_INCLUDE_DIRS}/cairo-version.h" CAIRO_VERSION_CONTENT)
+        STRING(REGEX MATCH "#define +CAIRO_VERSION_MAJOR +([0-9]+)" _dummy "${Cairo_VERSION_CONTENT}")
+        SET(Cairo_VERSION_MAJOR "${CMAKE_MATCH_1}")
 
-        STRING(REGEX MATCH "#define +CAIRO_VERSION_MAJOR +([0-9]+)" _dummy "${CAIRO_VERSION_CONTENT}")
-        SET(CAIRO_VERSION_MAJOR "${CMAKE_MATCH_1}")
+        STRING(REGEX MATCH "#define +CAIRO_VERSION_MINOR +([0-9]+)" _dummy "${Cairo_VERSION_CONTENT}")
+        SET(Cairo_VERSION_MINOR "${CMAKE_MATCH_1}")
 
-        STRING(REGEX MATCH "#define +CAIRO_VERSION_MINOR +([0-9]+)" _dummy "${CAIRO_VERSION_CONTENT}")
-        SET(CAIRO_VERSION_MINOR "${CMAKE_MATCH_1}")
+        STRING(REGEX MATCH "#define +CAIRO_VERSION_MICRO +([0-9]+)" _dummy "${Cairo_VERSION_CONTENT}")
+        SET(Cairo_VERSION_MICRO "${CMAKE_MATCH_1}")
 
-        STRING(REGEX MATCH "#define +CAIRO_VERSION_MICRO +([0-9]+)" _dummy "${CAIRO_VERSION_CONTENT}")
-        SET(CAIRO_VERSION_MICRO "${CMAKE_MATCH_1}")
-
-        SET(CAIRO_VERSION "${CAIRO_VERSION_MAJOR}.${CAIRO_VERSION_MINOR}.${CAIRO_VERSION_MICRO}")
+        SET(Cairo_VERSION "${Cairo_VERSION_MAJOR}.${Cairo_VERSION_MINOR}.${Cairo_VERSION_MICRO}")
     ENDIF ()
 ENDIF ()
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Cairo REQUIRED_VARS CAIRO_LIBRARIES CAIRO_INCLUDE_DIRS
-                                        VERSION_VAR CAIRO_VERSION)
-if(Cairo_FOUND)
-    if(NOT TARGET Cairo::Cairo)
-        add_library(Cairo::Cairo UNKNOWN IMPORTED)
-        set_target_properties(Cairo::Cairo PROPERTIES
-                INTERFACE_INCLUDE_DIRECTORIES "${CAIRO_INCLUDE_DIRS}")
+find_package_handle_standard_args(Cairo REQUIRED_VARS Cairo_LIBRARY Cairo_INCLUDE_DIR VERSION_VAR Cairo_VERSION)
 
-        if(Cairo_LIBRARY_RELEASE)
-            set_property(TARGET Cairo::Cairo APPEND PROPERTY
-                    IMPORTED_CONFIGURATIONS RELEASE)
-            set_target_properties(Cairo::Cairo PROPERTIES
-                    IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
-                    IMPORTED_LOCATION_RELEASE "${Cairo_LIBRARY_RELEASE}")
-        endif()
+if (Cairo_FOUND)
+    mark_as_advanced(Cairo_INCLUDE_DIR)
+    mark_as_advanced(Cairo_LIBRARY)
+endif()
 
-        if(Cairo_LIBRARY_DEBUG)
-            set_property(TARGET Cairo::Cairo APPEND PROPERTY
-                    IMPORTED_CONFIGURATIONS DEBUG)
-            set_target_properties(Cairo::Cairo PROPERTIES
-                    IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
-                    IMPORTED_LOCATION_DEBUG "${CAIRO_LIBRARIES}")
-        endif()
-
-        if(NOT Cairo_LIBRARY_RELEASE AND NOT Cairo_LIBRARY_DEBUG)
-            set_target_properties(Cairo::Cairo PROPERTIES
-                    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-                    IMPORTED_LOCATION "${CAIRO_LIBRARIES}")
-        endif()
-    endif()
+if (Cairo_FOUND AND NOT TARGET Cairo::Cairo)
+    add_library(Cairo::Cairo IMPORTED UNKNOWN)
+    set_target_properties(Cairo::Cairo PROPERTIES IMPORTED_LOCATION ${Cairo_LIBRARY})
+    target_include_directories(Cairo::Cairo INTERFACE ${Cairo_INCLUDE_DIR})
 endif()
--- cmake/FindInChI.cmake
+++ cmake/FindInChI.cmake
@@ -1,64 +1,25 @@
-# - Try to find InChI
-# Once done, this will define
-#
-#  INCHI_FOUND - system has InChI
-#  INCHI_INCLUDE_DIRS - the InChI include directories
-#  INCHI_LIBRARIES - link these to use InChI
+include(FindPackageHandleStandardArgs)
 
-FIND_PACKAGE(PkgConfig)
-PKG_CHECK_MODULES(PC_INCHI inchi QUIET)
+find_library(InChI_LIBRARY NAMES cairo)
+find_path(InChI_INCLUDE_DIR NAMES cairo.h)
 
-FIND_PATH(INCHI_INCLUDE_DIRS
-        NAMES inchi_api.h
-        HINTS ${PC_INCHI_INCLUDEDIR}
-        ${PC_INCHI_INCLUDE_DIRS}
-        PATH_SUFFIXES inchi
-        )
-
-FIND_LIBRARY(INCHI_LIBRARIES
-        NAMES inchi
-        HINTS ${PC_INCHI_LIBDIR}
-        ${PC_INCHI_LIBRARY_DIRS}
-        )
-
-if (INCHI_INCLUDE_DIRS)
-    if (EXISTS "${INCHI_INCLUDE_DIRS}/inchi_api.h")
-        file(READ "${INCHI_INCLUDE_DIRS}/inchi_api.h" INCHI_VERSION_CONTENT)
-        string(REGEX MATCH "Software version +([0-9]+\\.[0-9]+)" _dummy "${INCHI_VERSION_CONTENT}")
-        set(INCHI_VERSION "${CMAKE_MATCH_1}")
+if (InChI_INCLUDE_DIRS)
+    if (EXISTS "${InChI_INCLUDE_DIRS}/inchi_api.h")
+        file(READ "${InChI_INCLUDE_DIRS}/inchi_api.h" InChI_VERSION_CONTENT)
+        string(REGEX MATCH "Software version +([0-9]+\\.[0-9]+)" _dummy "${InChI_VERSION_CONTENT}")
+        set(InChI_VERSION "${CMAKE_MATCH_1}")
     endif ()
 endif ()
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(InChI REQUIRED_VARS INCHI_LIBRARIES INCHI_INCLUDE_DIRS
-                                        VERSION_VAR INCHI_VERSION)
+find_package_handle_standard_args(InChI REQUIRED_VARS InChI_LIBRARY InChI_INCLUDE_DIR VERSION_VAR InChI_VERSION)
 
-if(InChI_FOUND)
-    if(NOT TARGET InChI::Cairdisuo)
-        add_library(InChI::InChI UNKNOWN IMPORTED)
-        set_target_properties(InChI::InChI PROPERTIES
-                INTERFACE_INCLUDE_DIRECTORIES "${InChI_INCLUDE_DIRS}")
+if (InChI_FOUND)
+    mark_as_advanced(InChI_INCLUDE_DIR)
+    mark_as_advanced(InChI_LIBRARY)
+endif()
 
-        if(InChI_LIBRARY_RELEASE)
-            set_property(TARGET InChI::InChI APPEND PROPERTY
-                    IMPORTED_CONFIGURATIONS RELEASE)
-            set_target_properties(InChI::InChI PROPERTIES
-                    IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
-                    IMPORTED_LOCATION_RELEASE "${InChI_LIBRARY_RELEASE}")
-        endif()
-
-        if(InChI_LIBRARY_DEBUG)
-            set_property(TARGET InChI::InChI APPEND PROPERTY
-                    IMPORTED_CONFIGURATIONS DEBUG)
-            set_target_properties(InChI::InChI PROPERTIES
-                    IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
-                    IMPORTED_LOCATION_DEBUG "${InChI_LIBRARY_DEBUG}")
-        endif()
-
-        if(NOT InChI_LIBRARY_RELEASE AND NOT InChI_LIBRARY_DEBUG)
-            set_target_properties(InChI::InChI PROPERTIES
-                    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-                    IMPORTED_LOCATION "${INCHI_LIBRARIES}")
-        endif()
-    endif()
+if (InChI_FOUND AND NOT TARGET InChI::InChI)
+    add_library(InChI::InChI IMPORTED UNKNOWN)
+    set_target_properties(InChI::InChI PROPERTIES IMPORTED_LOCATION ${InChI_LIBRARY})
+    target_include_directories(InChI::InChI INTERFACE ${InChI_INCLUDE_DIR})
 endif()
