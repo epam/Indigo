48f6d154f17c460635c20fa815eb691b2c41ef45
Mikhail Kviatkovskii <mikhail_kviatkovskii@epam.com>
2023-05-21 14:49:03 +0400

    #1123: conda pkg [WIP]

---
+++ api/python/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -eux
+
+cmake \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_SYSTEM_PREFIX_PATH=${PREFIX} \
+    -D CMAKE_INSTALL_PREFIX=${PREFIX} \
+    -D BUILD_STANDALONE=OFF \
+    -D BUILD_INDIGO_WRAPPERS_JAVA=OFF \
+    -D BUILD_INDIGO_WRAPPERS_DOTNET=OFF \
+    -D BUILD_INDIGO_UTILS=OFF \
+    -D BUILD_BINGO=OFF \
+    -D BUILD_BINGO_ELASTIC=OFF \
+    .
+
+cmake --build . --target indigo-python
+pip install ./dist/indigo-python*.whl
