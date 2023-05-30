fd66187f7ecbdbfdfe60c93d15e7b1892177aa85
Roman Porozhnetov <roman_porozhnetov@epam.com>
2023-05-30 15:13:10 +0200

    bad stereo flag fix

---
+++ api/tests/integration/tests/formats/ket_bad_stereo.py
@@ -0,0 +1,28 @@
+﻿import difflib
+import os
+import sys
+
+
+def find_diff(a, b):
+    return "\n".join(difflib.unified_diff(a.splitlines(), b.splitlines()))
+
+
+sys.path.append(
+    os.path.normpath(
+        os.path.join(os.path.abspath(__file__), "..", "..", "..", "common")
+    )
+)
+from env_indigo import *  # noqa
+
+indigo = Indigo()
+
+root_rea = joinPathPy("reactions/", __file__)
+
+try:
+    reaction = indigo.loadReactionFromFile(os.path.join(root_rea, "bad_stereo.ket"))
+except IndigoException as e:
+    print(getIndigoExceptionText(e))
+
+indigo.setOption("ignore-stereochemistry-errors", "1")
+reaction = indigo.loadReactionFromFile(os.path.join(root_rea, "bad_stereo.ket"))
+print(reaction.smiles())
