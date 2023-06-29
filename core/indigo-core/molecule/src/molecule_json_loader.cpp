5741df6d9e1dc9f8475b01b51ef42923ac468357
Wadym <vadym_golovko@epam.com>
2023-06-29 09:52:20 +0200

    implements loader

--- core/indigo-core/molecule/src/molecule_json_loader.cpp
+++ core/indigo-core/molecule/src/molecule_json_loader.cpp
@@ -831,27 +831,17 @@ void MoleculeJsonLoader::parseSGroups(const rapidjson::Value& sgroups, BaseMolec
             if (s.HasMember("attachmentPoints"))
             {
                 const Value& attachmentPoints = s["attachmentPoints"];
-                // assert(attachmentPoints.IsArray());
-                // for (SizeType i = 0; i < a.Size(); i++) // Uses SizeType instead of size_t
-                // printf("a[%d] = %d\n", i, a[i].GetInt());
-                std ::cout << "attachmentPoints.Size() : " << attachmentPoints.Size() << std::endl;
+                assert(attachmentPoints.IsArray());
                 for (SizeType j = 0; j < attachmentPoints.Size(); ++j)
                 {
-                    // const Value& a = (const rapidjson::GenericValue<rapidjson::UTF8<> >&)attachmentPoints[j];
-                    // const Value& attachmentAtom = (const rapidjson::GenericValue<rapidjson::UTF8<> >&)attachmentPoints[j];
                     int attachmentAtom = attachmentPoints[j]["attachmentAtom"].GetInt();
-                    std ::cout << "attachmentAtom : " << attachmentAtom << std::endl;
                     int leavingAtom = attachmentPoints[j]["leavingAtom"].GetInt();
-                    std ::cout << "leavingAtom : " << leavingAtom << std::endl;
                     std::string id = attachmentPoints[j]["id"].GetString();
-                    std ::cout << "id : " << id << std::endl;
-
-                    // /home/vg/workspace/indigo/core/indigo-core/molecule/src/molecule_json_loader.cpp:840:38: error: invalid initialization of reference of
-                    // type ‘const Value&’ {aka ‘const rapidjson::GenericValue<rapidjson::UTF8<> >&’} from expression of type ‘const
-                    // indigo::Superatom::_AttachmentPoint’ sg.attachment_points.push(10); auto attachmentPoint = attachmentPoints[j]; // s["atoms"];
-                    // attachmentPoint.
-                    // sg.attachment_points.push();
-                    // sgroup.bonds.push(bonds[j].GetInt());
+                    int ap_idx = sg.attachment_points.add();
+                    Superatom::_AttachmentPoint& ap = sg.attachment_points.at(ap_idx);
+                    ap.aidx = attachmentAtom;
+                    ap.lvidx = leavingAtom;
+                    ap.apid.readString(id.c_str(), true);
                 }
             }
         }
