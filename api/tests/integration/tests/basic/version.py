7446c93905360ac79d0206d861e91bf4e15ae02a
Dmitry Malysh <Dmytro_Malysh@epam.com>
2023-07-27 16:08:02 +0200

    Optimise test script.

--- api/tests/integration/tests/basic/version.py
+++ api/tests/integration/tests/basic/version.py
@@ -11,9 +11,6 @@ from env_indigo import *
 
 indigo = Indigo()
 
-def get_value_by_key(key):
-    return json.loads(indigo.versionInfo())[key]
-
 def test_string_is_readable():
     print("*** Testing string is readable ***")
     try:
@@ -29,53 +26,18 @@ def test_string_is_valid_json():
     except:
         print("String is not valid json")
 
-def test_string_has_major_version():
-    print("*** Testing string contains major version ***")
+def test_string_has_specific_key(key):
+    print("*** Testing string contains '{}' key ***".format(key))
     try:
-        print(get_value_by_key('majorVersion'))
+        print(json.loads(indigo.versionInfo())[key])
     except:
-        print("String does not contain major version")
-
-def test_string_has_minor_version():
-    print("*** Testing string contains minor version ***")
-    try:
-        print(get_value_by_key('minorVersion'))
-    except:
-        print("String does not contain minor version")
-
-def test_string_has_dev_tag():
-    print("*** Testing string contains dev tag ***")
-    try:
-        print(get_value_by_key('devTag'))
-    except:
-        print("String does not contain dev tag")  
-
-def test_string_has_commit_hash():
-    print("*** Testing string contains commit hash ***")
-    try:
-        print(get_value_by_key('commitHash'))
-    except:
-        print("String does not contain commit hash")  
-
-def test_string_has_compiler_version():
-    print("*** Testing string contains compiler version ***")
-    try:
-        print(get_value_by_key('compilerVersion'))
-    except:
-        print("String does not contain compiler version")  
-
-def test_string_has_compiler_platform():
-    print("*** Testing string contains compiler platform ***")
-    try:
-        print(get_value_by_key('compilerPlatform'))
-    except:
-        print("String does not contain compiler platform") 
+        print("String does not contain '{}' key".format(key)) 
 
 test_string_is_readable()
 test_string_is_valid_json()
-test_string_has_major_version()
-test_string_has_minor_version()
-test_string_has_dev_tag()
-test_string_has_commit_hash()
-test_string_has_compiler_version()
-test_string_has_compiler_platform()
+test_string_has_specific_key('majorVersion')
+test_string_has_specific_key('minorVersion')
+test_string_has_specific_key('devTag')
+test_string_has_specific_key('commitHash')
+test_string_has_specific_key('compilerVersion')
+test_string_has_specific_key('compilerPlatform')
