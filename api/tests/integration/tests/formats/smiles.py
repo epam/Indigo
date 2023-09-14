6deab5c877c1a09d5ecac6c5cc6d0ef894a28efd
Roman Porozhnetov <roman_porozhnetov@epam.com>
2023-09-14 15:47:06 +0200

    layout test

--- api/tests/integration/tests/formats/smiles.py
+++ api/tests/integration/tests/formats/smiles.py
@@ -121,5 +121,6 @@ mols_smiles = ["C1C(O)=C(C2C=CC(C)=CC=2N)C(C)=CC=1 |o1:3,r,wU:3.12|"]
 for sm in mols_smiles:
     print("atropisomer:")
     mol = indigo.loadMolecule(sm)
+    print(mol.smiles())
     mol.layout()
     print(mol.smiles())
