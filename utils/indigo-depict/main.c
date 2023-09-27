926835651c965d75b672a23817b5d2c3b72902a4
Roman Porozhnetov <roman_porozhnetov@epam.com>
2023-09-27 08:01:55 +0200

    test fix

--- utils/indigo-depict/main.c
+++ utils/indigo-depict/main.c
@@ -396,7 +396,6 @@ int parseParams(Params* p, int argc, char* argv[])
     indigoSetOptionBool("treat-x-as-pseudoatom", 1);
     indigoSetOptionBool("render-coloring", 1);
     indigoSetOptionBool("render-highlight-color-enabled", 1);
-
     for (; i < argc; i++)
     {
         if (strcmp(argv[i], "-w") == 0)
