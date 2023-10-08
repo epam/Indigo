6089687a71ed17599a88066f853084ac9d65dc98
Roman Porozhnetov <roman_porozhnetov@epam.com>
2023-10-09 01:31:46 +0200

    bingo fixes

--- bingo/tests/test_rsmiles/test_rsmiles.py
+++ bingo/tests/test_rsmiles/test_rsmiles.py
@@ -1,11 +1,15 @@
 import pytest
 
 from ..helpers import assert_calculate_query, query_cases
-
+from ..logger import logger
 
 class TestRsmiles:
     @pytest.mark.parametrize("query_id, expected", query_cases("rsmiles"))
     def test_molecular_weight(self, db, entities, query_id, expected):
         reaction = entities.get(query_id)
         result = db.rsmiles(reaction)
+        if result != expected:
+            logger.info(query_id)
+            logger.info(result)
+            logger.info(expected)
         assert_calculate_query(result, expected)
