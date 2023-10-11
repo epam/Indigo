17cea6ce8926797057d17d6d0007e530d48cb801
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2023-10-11 12:54:35 +0300

    #1303 Return original format
    Fix UT

--- api/tests/integration/tests/basic/get_original_format.py
+++ api/tests/integration/tests/basic/get_original_format.py
@@ -11,21 +11,23 @@ from env_indigo import *  # noqa
 indigo = Indigo()
 
 
-def test_mol(mol, expected):
+def test_mol(molecule, mol, expected):
     original_format = mol.getOriginalFormat()
     if original_format == expected:
-        print('"%s" : got "%s" - OK' % (mol, expected))
+        print('Molecule "%s" : got "%s" - OK' % (molecule, expected))
     else:
         print(
-            '"%s" failed: expected original format "%s", got "%s"'
-            % (mol, expected, original_format)
+            'Molecule "%s" failed: expected original format "%s", got "%s"'
+            % (molecule, expected, original_format)
         )
 
 
 print("******  Test get original format: smiles  ***")
-mol = indigo.loadMolecule("N[CH](C)C(=O)O")
-test_mol(mol, "chemical/x-daylight-smiles")
+molecule = "N[CH](C)C(=O)O"
+mol = indigo.loadMolecule(molecule)
+test_mol(molecule, mol, "chemical/x-daylight-smiles")
 
 print("******  Test get original format: smarts  ***")
-mol = indigo.loadSmarts("([#6]1-[#6]-[#6]-1.[#6])")
-test_mol(mol, "chemical/x-daylight-smarts")
+molecule = "([#6]1-[#6]-[#6]-1.[#6])"
+mol = indigo.loadSmarts(molecule)
+test_mol(molecule, mol, "chemical/x-daylight-smarts")
