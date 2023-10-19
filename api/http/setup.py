4bfe0976e6079892f88f7ce34b82641d6c85d5d5
Aliakasndr Dziarkach <Aliakasndr.Dziarkach@gmail.com>
2023-10-19 09:52:30 +0300

    Bump to 1.15.0-dev.4
    bum version

--- api/http/setup.py
+++ api/http/setup.py
@@ -19,7 +19,7 @@ entry_points = {
 
 setup_kwargs = {
     "name": "indigo-service",
-    "version": "1.15.0.dev3",
+    "version": "1.15.0.dev4",
     "description": "",
     "long_description": None,
     "author": "Ruslan Khyurri",
