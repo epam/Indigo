33d5ce6b2e03ceaf2144078334df4026e7d31bc0
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2023-10-23 14:29:24 +0300

    #1360 Wrong ordering of atoms in atom list
    Fix code, add UT

--- api/tests/integration/tests/formats/smarts.py
+++ api/tests/integration/tests/formats/smarts.py
@@ -184,3 +184,9 @@ test_smarts_load_save_through_ket(
     "[!#6!#7!#8]",
     '"atoms":[{"type":"atom-list","notList":true,"elements":["C","N","O"],"location":[0.0,0.0,0.0]}]',
 )
+smarts = "[!#40!#79!#30]-[#6]-[#6]"
+expected = '"atoms":[{"type":"atom-list","notList":true,"elements":["Zr","Au","Zn"],'
+test_smarts_load_save_through_ket(smarts, expected)
+smarts = "[#40,#79,#30]-[#6]-[#6]"
+expected = '"atoms":[{"type":"atom-list","elements":["Zr","Au","Zn"],'
+test_smarts_load_save_through_ket(smarts, expected)
