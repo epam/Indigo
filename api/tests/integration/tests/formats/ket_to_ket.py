db861799c1ac38e7b937954babe8f60d8a028606
even1024 <roman.porozhnetov@gmail.com>
2023-11-16 13:22:14 +0100

    explicit valence

---
+++ api/tests/integration/tests/formats/ket_to_ket.py
@@ -0,0 +1,46 @@
+import difflib
+import os
+import sys
+
+
+def find_diff(a, b):
+    return "\n".join(difflib.unified_diff(a.splitlines(), b.splitlines()))
+
+
+sys.path.append(
+    os.path.normpath(
+        os.path.join(os.path.abspath(__file__), "..", "..", "..", "common")
+    )
+)
+from env_indigo import *  # noqa
+
+indigo = Indigo()
+indigo.setOption("json-saving-pretty", True)
+indigo.setOption("ignore-stereochemistry-errors", True)
+
+print("*** KET to KET ***")
+
+root = joinPathPy("molecules/", __file__)
+ref_path = joinPathPy("ref/", __file__)
+
+files = [
+    "query_explicit_val",
+]
+
+files.sort()
+for filename in files:
+    try:
+        mol = indigo.loadMoleculeFromFile(os.path.join(root, filename + ".ket"))
+    except IndigoException as e:
+        mol = indigo.loadQueryMoleculeFromFile(os.path.join(root, filename + ".ket"))
+    # with open(os.path.join(ref_path, filename) + ".ket", "w") as file:
+    #     file.write(mol.json())
+    with open(os.path.join(ref_path, filename) + ".ket", "r") as file:
+        ket_ref = file.read()
+    ket = mol.json()
+    diff = find_diff(ket_ref, ket)
+    if not diff:
+        print(filename + ".ket:SUCCEED")
+    else:
+        print(filename + ".ket:FAILED")
+        print(diff)
