88c7c7e86bee912b5138ea34552b846c4b7a99c9
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2023-12-26 22:41:10 +0300

    #1446 Dearomatization does not work with query features
    Add UTs

--- api/tests/integration/tests/arom/query_dearom.py
+++ api/tests/integration/tests/arom/query_dearom.py
@@ -26,3 +26,25 @@ m = indigo.loadSmarts("c1[cX4]cccc1")
 print("Aromatic: %s" % m.smarts())
 m.dearomatize()
 print("Dearomatized: %s\n" % m.smarts())
+
+m = indigo.loadSmarts("[#6]12:[#7;h1]:[#7]:[#6]:[#6]:1:[#6]:[#6]:[#6]:[#6]:2")
+print("Aromatic: %s" % m.smarts())
+m.dearomatize()
+print("Dearomatized: %s\n" % m.smarts())
+
+m = indigo.loadSmarts("[#6]12:[#7;h0]:[#7]:[#6]:[#6]:1:[#6]:[#6]:[#6]:[#6]:2")
+print("Aromatic: %s" % m.smarts())
+m.dearomatize()
+print("Dearomatized: %s\n" % m.smarts())
+
+m = indigo.loadSmarts("[#6]12:[#7]:[#7;h0]:[#6]:[#6]:1:[#6]:[#6]:[#6]:[#6]:2")
+print("Aromatic: %s" % m.smarts())
+m.dearomatize()
+print("Dearomatized: %s\n" % m.smarts())
+
+m = indigo.loadSmarts(
+    "[#6]12:[#7;h0]:[#7;h0]:[#6]:[#6]:1:[#6]:[#6]:[#6]:[#6]:2"
+)
+print("Aromatic: %s" % m.smarts())
+m.dearomatize()
+print("Cannot be dearomatized: %s\n" % m.smarts())
