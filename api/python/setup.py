d40579f6cf1d8f1552a70c09ca3a78e225c2c900
Roman Porozhnetov <roman.porozhnetov@gmail.com>
2024-01-08 15:34:34 +0100

    bump indigo version to 1.18.0-dev.3

--- api/python/setup.py
+++ api/python/setup.py
@@ -98,7 +98,7 @@ if os.path.exists("epam.indigo.egg-info"):
 
 setup(
     name="epam.indigo",
-    version="1.18.0.dev2",
+    version="1.18.0.dev3",
     description="Indigo universal cheminformatics toolkit",
     author="EPAM Systems Life Science Department",
     author_email="lifescience.opensource@epam.com",
