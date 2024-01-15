a72f6578ed0aa33596ec3df3a1a55aebc90abea3
even1024 <roman.porozhnetov@gmail.com>
2024-01-15 19:07:13 +0100

    conflict fix

--- core/indigo-core/molecule/src/query_molecule.cpp
+++ core/indigo-core/molecule/src/query_molecule.cpp
@@ -1030,7 +1030,7 @@ const int QueryMolecule::getTemplateAtomTemplateIndex(int /*idx*/)
     return -1;
 }
 
-void QueryMolecule::getTemplatesMap(std::unordered_map<std::pair<std::string, std::string>, std::reference_wrapper<TGroup>, pair_hash>& templates_map)
+void QueryMolecule::getTemplatesMap(std::unordered_map<std::pair<std::string, std::string>, std::reference_wrapper<TGroup>, pair_hash>& /*templates_map*/)
 {
     throw Error("QueryMolecule::getTemplatesMap not implemented");
 }
