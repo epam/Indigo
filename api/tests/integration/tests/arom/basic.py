9af5f13c71a350c113abbc7f835845c8c5a05d7a
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2024-01-18 00:37:24 +0300

    Backmerge: #1478 Dearomatization causes exception in case of Implicit H count query feature set to 4 (i.e. more than 2)
    Backmerge by cherry-pick

--- api/tests/integration/tests/arom/basic.py
+++ api/tests/integration/tests/arom/basic.py
@@ -352,3 +352,19 @@ print(styr.molfile())
 indigo.setOption("dearomatize-on-load", "true")
 q = indigo.loadQueryMolecule("[#6]=,:[#6]")
 print(q.json())
+
+print("***** Dearomatize molecule with custom query without label  *****")
+m = indigo.loadQueryMoleculeFromFile(
+    joinPathPy("molecules/issue_1524.ket", __file__)
+)
+m.dearomatize()
+print(m.smarts())
+
+print(
+    "***** Dearomatize molecule with atom_aromatic_connectivity < 0 should not cause exception  *****"
+)
+m = indigo.loadMoleculeFromFile(
+    joinPathPy("molecules/issue_1478.ket", __file__)
+)
+m.dearomatize()
+print(m.smiles())
