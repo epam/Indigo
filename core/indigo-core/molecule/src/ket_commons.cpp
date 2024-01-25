90f342217ea5e083ef61ed0dadb0b79ff4bdd4bd
even1024 <roman.porozhnetov@gmail.com>
2024-01-25 14:46:14 +0100

    v3000fix

--- core/indigo-core/molecule/src/ket_commons.cpp
+++ core/indigo-core/molecule/src/ket_commons.cpp
@@ -113,7 +113,7 @@ namespace indigo
     {
         int id = extract_id(atp_id_str, "R");
         if (id < 0)
-            throw std::invalid_argument("convertAPFromHELM: prefix 'R' not found in 'atp_id_str'.");
+            throw std::invalid_argument(std::string("convertAPFromHELM: prefix 'R' not found in :") + atp_id_str);
         char ap_symbol = static_cast<char>(id) + '@'; // convert number to ASCII letter
         std::string res(1, ap_symbol);
         switch (ap_symbol)
