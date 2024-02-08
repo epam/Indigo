9eec6504809f98a72ddb63ea03d89e12bed10c28
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2024-02-08 15:40:40 +0300

    Fix UT

--- api/tests/integration/tests/basic/unfold_molecules.py
+++ api/tests/integration/tests/basic/unfold_molecules.py
@@ -23,36 +23,36 @@ indigo.setOption("json-use-native-precision", "1")
 def test_unfold(filename, load_function, auto=False):
     print("testing filename:\n%s" % filename)
     molecule = load_function(ket_path + filename)
-    ket_json = molecule.json()
-    ket_list = ket_json.split("\n")
+    init_smarts = molecule.smarts()
     if auto:
         molecule.foldUnfoldHydrogens()
     else:
         molecule.unfoldHydrogens()
-    unfolded = molecule.json()
-    unfolded_list = unfolded.split("\n")
+    unfolded = molecule.smarts()
     # out_path = joinPathPy("out", __file__) + "/"
     # with open(out_path + filename, "w") as out_file:
-    #     out_file.write(unfolded)
-    with open(ref_path + filename) as ref_file:
-        ref_json = ref_file.read()
-    expected_list = ref_json.split("\n")
-    diff = "\n".join(difflib.context_diff(unfolded_list, expected_list))
-    if diff:
-        print("Diff between expected and after unfold:\n%s" % diff)
+    #     out_file.write(molecule.json())
+    expected_mol = load_function(ref_path + filename)
+    expected = expected_mol.smarts()
+    if expected == unfolded:
+        print("Unfolded molecule equal to expected")
     else:
-        print("Unfolded KET equal to expected")
+        print(
+            "Diff between expected and after unfold.\nExpected:%s\nUnfolded:%s\n"
+            % (expected, unfolded)
+        )
     if auto:
         molecule.foldUnfoldHydrogens()
     else:
         molecule.foldHydrogens()
-    folded = molecule.json()
-    folded_list = folded.split("\n")
-    diff = "\n".join(difflib.context_diff(folded_list, ket_list))
-    if diff:
-        print("Diff between original ket and after fold:\n%s" % diff)
-    else:
+    folded = molecule.smarts()
+    if folded == init_smarts:
         print("Folded KET equal to original")
+    else:
+        print(
+            "Diff between origin and after fold.\nOrigin:%s\nFolded:%s\n"
+            % (expected, unfolded)
+        )
 
 
 def test_qmol_unfold(filename, auto=False):
