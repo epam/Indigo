ba29fda9204a467966286fe91428e9686ca48fbd
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2024-02-13 16:00:42 +0300

    Bump to version 1.18.0-rc.7
    [skip ci]

--- utils/indigo-ml/setup.py
+++ utils/indigo-ml/setup.py
@@ -95,7 +95,7 @@ if os.path.exists("indigo_chem.egg-info"):
 
 setup(
     name="epam.indigo",
-    version="1.18.0.rc6",
+    version="1.18.0.rc7",
     description="Indigo universal cheminformatics toolkit",
     author="EPAM Systems Life Science Department",
     author_email="lifescience.opensource@epam.com",
