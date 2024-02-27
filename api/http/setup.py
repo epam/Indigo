7c97b4fc3963c36d410154ec27b48b7d12d89568
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2024-02-27 15:46:23 +0300

    Bump to version 1.18.0-rc.9
    [no ci]

--- api/http/setup.py
+++ api/http/setup.py
@@ -19,7 +19,7 @@ entry_points = {
 
 setup_kwargs = {
     "name": "indigo-service",
-    "version": "1.18.0.rc8",
+    "version": "1.18.0.rc9",
     "description": "",
     "long_description": None,
     "author": "Ruslan Khyurri",
