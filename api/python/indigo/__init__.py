c7efb4e718065736fb27e03eec8759fbf085a607
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2024-02-27 16:24:10 +0300

    Bump to version 1.18.0-rc.9 (#1751)

--- api/python/indigo/__init__.py
+++ api/python/indigo/__init__.py
@@ -3,4 +3,4 @@ from .indigo.indigo import Indigo  # noqa
 from .indigo.indigo_exception import IndigoException  # noqa
 from .indigo.indigo_object import IndigoObject  # noqa
 
-__version__ = "1.18.0.rc8"
+__version__ = "1.18.0.rc9"
