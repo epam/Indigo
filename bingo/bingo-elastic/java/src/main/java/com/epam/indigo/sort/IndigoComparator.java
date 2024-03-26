e7ed0a2630d0b1b0dc0ab5c7fb7045a5a63130c4
uladkaminski <kaminski.uladzislau@gmail.com>
2024-03-26 22:40:08 +0000

    #1826 - CombinedComparator is created. Added builders for comparators.

--- bingo/bingo-elastic/java/src/main/java/com/epam/indigo/sort/IndigoComparator.java
+++ bingo/bingo-elastic/java/src/main/java/com/epam/indigo/sort/IndigoComparator.java
@@ -2,18 +2,19 @@ package com.epam.indigo.sort;
 
 import com.epam.indigo.model.IndigoRecord;
 import org.elasticsearch.search.sort.SortBuilder;
-import org.elasticsearch.search.sort.SortOrder;
 
+import java.util.Collection;
 import java.util.Comparator;
 
 
 public abstract class IndigoComparator<T extends IndigoRecord> implements Comparator<T> {
-    protected SortOrder sortOrder;
 
-    public IndigoComparator(SortOrder sortOrder) {
-        this.sortOrder = sortOrder;
+    public abstract Collection<SortBuilder<?>> toSortBuilders();
+
+    @Override
+    public int compare(final T o1, final T o2) {
+        // does not expect to be called
+        return 0;
     }
-
-    public abstract SortBuilder toSortBuilder();
 
 }
