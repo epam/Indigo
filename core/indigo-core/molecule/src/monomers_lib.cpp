83299c218b51f64a6693ebcceddccf2f54b9c940
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2024-06-06 04:12:23 +0300

    Fix typo

--- core/indigo-core/molecule/src/monomers_lib.cpp
+++ core/indigo-core/molecule/src/monomers_lib.cpp
@@ -172,62 +172,6 @@ namespace indigo
         }
     }
 
-    IMPL_ERROR(IdtAlias, "IDT alias");
-
-    const std::string& IdtAlias::getModification(IdtModification modification) const
-    {
-        static std::string empty;
-        switch (modification)
-        {
-        case IdtModification::FIVE_PRIME_END:
-            return getFivePrimeEnd();
-        case IdtModification::INTERNAL:
-            return getInternal();
-        case IdtModification::THREE_PRIME_END:
-            return getThreePrimeEnd();
-        };
-        throw Error("Unknown IDT modification: %s.", modification);
-        return empty;
-    }
-
-    const std::string& IdtAlias::getFivePrimeEnd() const
-    {
-        if (_five_prime_end != "")
-            return _five_prime_end;
-        else
-            throw Error("IDT alias %s has no five-prime end modification.", _base.c_str());
-    }
-
-    const std::string& IdtAlias::getInternal() const
-    {
-        if (_internal != "")
-            return _internal;
-        else
-            throw Error("IDT alias %s has no internal modification.", _base.c_str());
-    }
-
-    const std::string& IdtAlias::getThreePrimeEnd() const
-    {
-        if (_three_prime_end != "")
-            return _three_prime_end;
-        else
-            throw Error("IDT alias %s has no three-prime end modification.", _base.c_str());
-    }
-
-    std::string IdtAlias::getBaseForMod(const std::string& alias)
-    {
-        if (alias.size() < 2)
-            return alias;
-        switch (alias[0])
-        {
-        case '3':
-        case 'i':
-        case '5':
-            return alias.substr(1, alias.size() - 1);
-        }
-        return alias;
-    }
-
     IMPL_ERROR(MonomerTemplate, "MonomerTemplate");
 
     void MonomerTemplate::AddAttachmentPoint(const std::string& id, const std::string& ap_type, int att_atom, std::vector<int>& leaving_group)
