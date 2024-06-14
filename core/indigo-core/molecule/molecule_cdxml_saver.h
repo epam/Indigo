69721e7f756fa3da7fa54920a8511b554fc248e0
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2024-06-14 17:16:56 +0300

    #2000 Exporting a CDX file to ChemDraw loses information about charge on atoms
    Add charge to labes. Fix UTs

--- core/indigo-core/molecule/molecule_cdxml_saver.h
+++ core/indigo-core/molecule/molecule_cdxml_saver.h
@@ -128,6 +128,11 @@ namespace indigo
     protected:
         void _collectSuperatoms(BaseMolecule& mol);
         int _getAttachmentPoint(BaseMolecule& mol, int atom_idx);
+
+        tinyxml2::XMLElement* create_text(tinyxml2::XMLElement* parent, float x, float y, const char* label_justification);
+        void add_style_str(tinyxml2::XMLElement* parent, int font, int size, int face, const char* str);
+        void add_charge(tinyxml2::XMLElement* parent, int font, int size, int charge);
+
         Output& _output;
 
         float _bond_length;
