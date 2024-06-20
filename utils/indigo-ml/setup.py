553db3d31983b75af86d66838c0304a4ba677698
even1024 <roman_porozhnetov@epam.com>
2024-06-20 09:49:49 +0200

    Bump to version 1.20.0 (#2012)

--- utils/indigo-ml/setup.py
+++ utils/indigo-ml/setup.py
@@ -95,7 +95,7 @@ if os.path.exists("indigo_chem.egg-info"):
 
 setup(
     name="epam.indigo",
-    version="1.20.0.rc3",
+    version="1.20.0",
     description="Indigo universal cheminformatics toolkit",
     author="EPAM Systems Life Science Department",
     author_email="lifescience.opensource@epam.com",
