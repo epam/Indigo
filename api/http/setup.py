37dd854b43e659ae05b6e7d4c40c6f5de8754758
Roman Porozhnetov <roman.porozhnetov@gmail.com>
2024-06-20 07:40:10 +0200

    Bump to version 1.20.0
    [no ci]

--- api/http/setup.py
+++ api/http/setup.py
@@ -20,7 +20,7 @@ entry_points = {
 
 setup_kwargs = {
     "name": "indigo-service",
-    "version": "1.20.0.rc3",
+    "version": "1.20.0",
     "description": "",
     "long_description": None,
     "author": "Ruslan Khyurri",
