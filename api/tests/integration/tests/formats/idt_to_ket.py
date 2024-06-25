f9b886df44fb4e1d1e5df8bc60145b4d720aef7c
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2024-06-25 20:05:55 +0300

    #2021 Roll back SEQID changes in MOL export (18 failed autotests - CHEMs on the canvas) (#2022)
    Co-authored-by: Aliakasndr Dziarkach <Aliakasndr.Dziarkach@gmail.com>

--- api/tests/integration/tests/formats/idt_to_ket.py
+++ api/tests/integration/tests/formats/idt_to_ket.py
@@ -54,6 +54,7 @@ idt_data = {
     "idt_i2moera_sp_32moera": "/i2MOErA/*/32MOErA/",
     "idt_52moera_sp_i2moera_sp": "/52MOErA/*/i2MOErA/*",
     "idt_unresolved": "/unr1//unr2/",
+    "idt_unresolved_many": "/unr0/A/unr1/C/unr2/ACTG/unr3/G/unr4/",
     "idt_unsplit": "/5UNSPLIT/A",
 }
 
