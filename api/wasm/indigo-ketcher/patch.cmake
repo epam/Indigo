7b2d1c145204fee386e0e5065970d97295de3804
Roman Porozhnetov <roman.porozhnetov@gmail.com>
2024-06-25 13:43:03 +0200

    separate versions

--- api/wasm/indigo-ketcher/patch.cmake
+++ api/wasm/indigo-ketcher/patch.cmake
@@ -3,7 +3,7 @@ file(READ package.json packageJson)
 string(REGEX REPLACE ".*\"version\": \"([^\"]*)\".*" "\\1" version ${packageJson})
 message("Version is: ${version}")
 # patch package.json
-string(REPLACE "indigo-ketcher.wasm" "indigo-ketcher-${version}.wasm" modifiedPackageJson ${packageJson})
+string(REPLACE "indigo-ketcher-separate-wasm.wasm" "indigo-ketcher-${version}.wasm" modifiedPackageJson ${packageJson})
 file(WRITE package.json "${modifiedPackageJson}")
 
 # patch indigo-ketcher-separate-wasm.js
@@ -12,4 +12,4 @@ string(REPLACE "indigo-ketcher-separate.wasm" "indigo-ketcher-${version}.wasm" c
 file(WRITE "indigo-ketcher-separate-wasm.js" "${content}")
 
 # rename indigo-ketcher.wasm
-file(RENAME indigo-ketcher.wasm indigo-ketcher-${version}.wasm)
+file(RENAME indigo-ketcher-separate.wasm indigo-ketcher-${version}.wasm)
