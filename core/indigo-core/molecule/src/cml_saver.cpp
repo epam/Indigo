301c342811c9d74dd8cc5e818dce9de855d9742f
even1024 <roman.porozhnetov@gmail.com>
2024-06-26 13:39:51 +0200

    fragment fix

--- core/indigo-core/molecule/src/cml_saver.cpp
+++ core/indigo-core/molecule/src/cml_saver.cpp
@@ -53,7 +53,7 @@ void CmlSaver::_saveMolecule(BaseMolecule& bmol, bool query)
     LocaleGuard locale_guard;
     std::unique_ptr<BaseMolecule> mol(bmol.neu());
     mol->clone_KeepIndices(bmol);
-    mol->transformTemplatesToSuperatoms();
+    // mol->transformTemplatesToSuperatoms();
 
     std::unique_ptr<XMLDocument> doc = std::make_unique<XMLDocument>();
     _doc = doc->GetDocument();
@@ -121,6 +121,8 @@ void CmlSaver::_addMoleculeElement(XMLElement* elem, BaseMolecule& mol, bool que
                 atom_str = "R";
             else if (_mol->isPseudoAtom(i))
                 atom_str = _mol->getPseudoAtom(i);
+            else if (_mol->isTemplateAtom(i))
+                atom_str = _mol->getTemplateAtom(i);
             else if (atom_number > 0)
                 atom_str = Element::toString(atom_number);
             else if (qmol != 0)
