49cc8743c7a3c6a079832dcac0864152d908784a
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2024-07-08 14:46:56 +0300

    Bump to version 1.22.0-rc.2
    [no ci]

--- api/python/indigo/__init__.py
+++ api/python/indigo/__init__.py
@@ -3,4 +3,4 @@ from .indigo.indigo import Indigo  # noqa
 from .indigo.indigo_exception import IndigoException  # noqa
 from .indigo.indigo_object import IndigoObject  # noqa
 
-__version__ = "1.22.0.rc1"
+__version__ = "1.22.0.rc2"
