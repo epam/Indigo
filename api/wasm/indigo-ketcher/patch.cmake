04b65f33d80e0b1d48e3d0003ed06610749ed4a9
even1024 <roman.porozhnetov@gmail.com>
2024-07-19 08:47:53 +0200

    norender

--- api/wasm/indigo-ketcher/patch.cmake
+++ api/wasm/indigo-ketcher/patch.cmake
@@ -4,14 +4,24 @@ string(REGEX REPLACE ".*\"version\": \"([^\"]*)\".*" "\\1" version ${packageJson
 message("Version is: ${version}")
 # patch package.json
 string(REPLACE "indigo-ketcher-separate-wasm.wasm" "indigo-ketcher-${version}.wasm" modifiedPackageJson ${packageJson})
+string(REPLACE "indigo-ketcher-separate-norender.wasm" "indigo-ketcher-norender-${version}.wasm" modifiedPackageJson ${packageJson})
+
 file(WRITE package.json "${modifiedPackageJson}")
 
 # patch indigo-ketcher-separate-wasm.js
 file(READ indigo-ketcher-separate-wasm.js content)
 string(REPLACE "indigo-ketcher-separate-wasm.wasm" "/indigo-ketcher-${version}.wasm" content "${content}")
 string(REPLACE "fetch(binaryFile" "fetch(new URL(binaryFile, self.location.origin)" content "${content}")
-
 file(WRITE "indigo-ketcher-separate-wasm.js" "${content}")
 
-# rename indigo-ketcher.wasm
+# patch indigo-ketcher-separate-norender.js
+file(READ indigo-ketcher-separate-norender.js content)
+string(REPLACE "indigo-ketcher-separate-norender.wasm" "/indigo-ketcher-norender-${version}.wasm" content "${content}")
+string(REPLACE "fetch(binaryFile" "fetch(new URL(binaryFile, self.location.origin)" content "${content}")
+file(WRITE "indigo-ketcher-separate-norender.js" "${content}")
+
+# rename indigo-ketcher-separate-wasm.wasm
 file(RENAME indigo-ketcher-separate-wasm.wasm indigo-ketcher-${version}.wasm)
+
+# rename indigo-ketcher-separate-norender.wasm
+file(RENAME indigo-ketcher-separate-norender.wasm indigo-ketcher-norender-${version}.wasm)
