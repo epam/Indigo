0ab0a98414b4f135489dff8c2a011fac9f8cdaff
even1024 <roman.porozhnetov@gmail.com>
2024-07-19 09:37:56 +0200

    norender

--- api/wasm/indigo-ketcher/patch.cmake
+++ api/wasm/indigo-ketcher/patch.cmake
@@ -14,14 +14,14 @@ string(REPLACE "indigo-ketcher-separate-wasm.wasm" "/indigo-ketcher-${version}.w
 string(REPLACE "fetch(binaryFile" "fetch(new URL(binaryFile, self.location.origin)" content "${content}")
 file(WRITE "indigo-ketcher-separate-wasm.js" "${content}")
 
-# patch indigo-ketcher-separate-norender.js
-file(READ indigo-ketcher-separate-norender.js content)
-string(REPLACE "indigo-ketcher-separate-norender.wasm" "/indigo-ketcher-norender-${version}.wasm" content "${content}")
+# patch indigo-ketcher-separate-wasm-norender.js
+file(READ indigo-ketcher-separate-wasm-norender.js content)
+string(REPLACE "indigo-ketcher-separate-wasm-norender.wasm" "/indigo-ketcher-norender-${version}.wasm" content "${content}")
 string(REPLACE "fetch(binaryFile" "fetch(new URL(binaryFile, self.location.origin)" content "${content}")
-file(WRITE "indigo-ketcher-separate-norender.js" "${content}")
+file(WRITE "indigo-ketcher-separate-wasm-norender.js" "${content}")
 
 # rename indigo-ketcher-separate-wasm.wasm
 file(RENAME indigo-ketcher-separate-wasm.wasm indigo-ketcher-${version}.wasm)
 
-# rename indigo-ketcher-separate-norender.wasm
-file(RENAME indigo-ketcher-separate-norender.wasm indigo-ketcher-norender-${version}.wasm)
+# rename indigo-ketcher-separate-wasm-norender.wasm
+file(RENAME indigo-ketcher-separate-wasm-norender.wasm indigo-ketcher-norender-${version}.wasm)
