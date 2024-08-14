23f7b67b450ee52d6c8a5396c6e0c6350b3d1ad2
Ilya Zaytsev <ilya_zaytsev@epam.com>
2024-08-14 12:17:55 +0400

    Fix static analyser warning.

--- api/tests/integration/tests/reaction/pathway.py
+++ api/tests/integration/tests/reaction/pathway.py
@@ -18,7 +18,15 @@ indigo = Indigo()
 input_path = joinPathPy("reactions/", __file__)
 ref_path = joinPathPy("ref/", __file__)
 
-files = ["pathway1", "pathway2", "pathway3", "pathway4", "pathway5", "pathway6", "pathway7"]
+files = [
+    "pathway1",
+    "pathway2",
+    "pathway3",
+    "pathway4",
+    "pathway5",
+    "pathway6",
+    "pathway7",
+]
 
 for filename in files:
     rxn = indigo.loadReactionFromFile(
