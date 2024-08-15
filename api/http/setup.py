568dd9f368e266c6e539e34f9b472d83d9111d2b
even1024 <roman_porozhnetov@epam.com>
2024-08-15 21:30:36 +0200

    Bump to version 1.21.0 (#2241)

--- api/http/setup.py
+++ api/http/setup.py
@@ -20,7 +20,7 @@ entry_points = {
 
 setup_kwargs = {
     "name": "indigo-service",
-    "version": "1.21.0.rc3",
+    "version": "1.21.0",
     "description": "",
     "long_description": None,
     "author": "Ruslan Khyurri",
