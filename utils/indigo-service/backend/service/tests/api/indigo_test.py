71d471190b9049ed278c9fc0eeb972808fd7c119
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2024-09-04 19:15:10 +0300

    fix typo

--- utils/indigo-service/backend/service/tests/api/indigo_test.py
+++ utils/indigo-service/backend/service/tests/api/indigo_test.py
@@ -479,7 +479,7 @@ chemical/x-daylight-smiles, chemical/x-cml, chemical/x-inchi, chemical/x-inchi-k
 chemical/x-iupac, chemical/x-daylight-smarts, chemical/x-inchi-aux, chemical/x-chemaxon-cxsmiles, \
 chemical/x-cdxml, chemical/x-cdx, chemical/x-sdf, chemical/x-peptide-sequence, \
 chemical/x-rna-sequence, chemical/x-dna-sequence, chemical/x-sequence, chemical/x-peptide-fasta, \
-chemical/x-rna-fasta, chemical/x-dna-fasta, chemical/x-fasta, chemical/x-idt."
+chemical/x-rna-fasta, chemical/x-dna-fasta, chemical/x-fasta, chemical/x-idt, chemical/x-helm."
         expected_text = (
             "ValidationError: {'input_format': ['Must be one of: %s']}"
             % formats
