837de32cee657add48135f89b73d442bb1ac5cdf
Aleksandr Savelev <Aleksandr_Savelev@epam.com>
2024-09-10 15:31:52 +0300

    fix cairo build on mingw x86_64 (#2323)

--- cmake/setup.cmake
+++ cmake/setup.cmake
@@ -45,6 +45,11 @@ if (APPLE)
     string(APPEND CMAKE_CXX_FLAGS " -Wno-register")
 endif()
 
+if (MINGW)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-error=incompatible-pointer-types -Wno-error=int-conversion -Wno-error=implicit-int")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-error=incompatible-pointer-types -Wno-error=int-conversion -Wno-error=implicit-int")
+endif()
+
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake")
 
 # Compile flags
