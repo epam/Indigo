e98b36a2e0db66da6c5565b0e643a7479132a866
Roman Porozhnetov <roman_porozhnetov@epam.com>
2024-09-19 22:58:16 +0200

    all fixed

--- core/indigo-core/reaction/src/reaction_multistep_detector.cpp
+++ core/indigo-core/reaction/src/reaction_multistep_detector.cpp
@@ -656,7 +656,10 @@ void ReactionMultistepDetector::constructMultipleArrowReaction(BaseReaction& rxn
             {
                 auto r_it = copied_components.find(ridx);
                 if (r_it != copied_components.end())
+                {
+                    std::cout << "reaction:" << i << " reactant:" << r_it->second << std::endl;
                     rb.reactants.push(r_it->second);
+                }
             }
 
             // add products
@@ -665,7 +668,10 @@ void ReactionMultistepDetector::constructMultipleArrowReaction(BaseReaction& rxn
             {
                 auto p_it = copied_components.find(pidx);
                 if (p_it != copied_components.end())
+                {
+                    std::cout << "reaction:" << i << " product:" << p_it->second << std::endl;
                     rb.products.push(p_it->second);
+                }
             }
         }
     }
