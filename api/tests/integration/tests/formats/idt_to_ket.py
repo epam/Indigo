18100872d18728ee245f0739e968422314ca81a1
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2024-10-17 18:25:17 +0300

    #2331 - System should throw an error in case of wrong IUBcode
    Fix code. Add UT.

--- api/tests/integration/tests/formats/idt_to_ket.py
+++ api/tests/integration/tests/formats/idt_to_ket.py
@@ -101,6 +101,7 @@ idt_errors = {
     "/52MOErA//32MOErA/*": "Monomer /32MOErA/ doesn't have phosphate, so '*' couldn't be applied.",
     "/3Phos/*": "Symbol '*' could be placed only between two nucleotides/nucleosides.",
     "r(B1:50003000)(B1)": "Unknown mixed base 'B1'",
+    "(YY:00330067)": "Invalid mixed base - only numerical index allowed.",
 }
 for idt_seq in sorted(idt_errors.keys()):
     error = idt_errors[idt_seq]
