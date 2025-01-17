c865a2ee1c744380098e595e0647c4f7d82d8a25
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2025-01-17 19:03:50 +0300

    Fix separate WASM path from root to cuurent dir

--- api/wasm/indigo-ketcher/patch.cmake
+++ api/wasm/indigo-ketcher/patch.cmake
@@ -10,13 +10,13 @@ file(WRITE package.json "${packageJson}")
 
 # patch indigo-ketcher-separate-wasm.js
 file(READ indigo-ketcher-separate-wasm.js content)
-string(REPLACE "indigo-ketcher-separate-wasm.wasm" "/indigo-ketcher-${version}.wasm" content "${content}")
+string(REPLACE "indigo-ketcher-separate-wasm.wasm" "indigo-ketcher-${version}.wasm" content "${content}")
 string(REPLACE "fetch(binaryFile" "fetch(new URL(binaryFile, self.location.origin)" content "${content}")
 file(WRITE "indigo-ketcher-separate-wasm.js" "${content}")
 
 # patch indigo-ketcher-separate-wasm-norender.js
 file(READ indigo-ketcher-separate-wasm-norender.js content)
-string(REPLACE "indigo-ketcher-separate-wasm-norender.wasm" "/indigo-ketcher-norender-${version}.wasm" content "${content}")
+string(REPLACE "indigo-ketcher-separate-wasm-norender.wasm" "indigo-ketcher-norender-${version}.wasm" content "${content}")
 string(REPLACE "fetch(binaryFile" "fetch(new URL(binaryFile, self.location.origin)" content "${content}")
 file(WRITE "indigo-ketcher-separate-wasm-norender.js" "${content}")
 
