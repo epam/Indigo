dc5331d6386d6b985e1dea86b244b052b7a03fea
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2025-01-29 12:49:14 +0300

    Fix typo

--- core/indigo-core/reaction/src/base_reaction.cpp
+++ core/indigo-core/reaction/src/base_reaction.cpp
@@ -98,7 +98,7 @@ SideIter SideAuto::end()
 
 BaseReaction::BaseReaction()
     : reactants(*this, REACTANT), catalysts(*this, CATALYST), products(*this, PRODUCT), intermediates(*this, INTERMEDIATE), undefined(*this, UNDEFINED),
-      original_format(BaseMolecule::UNKNOWN)
+      original_format(BaseMolecule::UNKNOWN), _document(nullptr)
 {
     clear();
 }
