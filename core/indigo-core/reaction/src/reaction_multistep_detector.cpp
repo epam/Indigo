802ccb5b4af7dc2b3761f759ade0317d4fe656d4
even1024 <roman.porozhnetov@gmail.com>
2025-01-29 07:54:16 +0100

    sort fix

--- core/indigo-core/reaction/src/reaction_multistep_detector.cpp
+++ core/indigo-core/reaction/src/reaction_multistep_detector.cpp
@@ -732,8 +732,8 @@ ReactionMultistepDetector::ReactionType ReactionMultistepDetector::detectReactio
     createSummBlocks();
     bool has_multistep = mapReactionComponents();
     bool has_multitail = mapMultitailReactionComponents();
-    dumpDetectionResults();
     sortSummblocks();
+    dumpDetectionResults();
     mergeUndefinedComponents();
     return has_multitail ? ReactionType::EPathwayReaction : (has_multistep ? ReactionType ::EMutistepReaction : ReactionType::ESimpleReaction);
 }
