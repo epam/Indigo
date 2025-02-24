0aa079a4db31b2f8f7f6ec68dba0b967be2df493
Joshua Black <j.black@mestrelab.com>
2025-02-24 11:19:40 +0100

    #425 add rendering chemaxon smiles tests

---
+++ api/tests/integration/tests/rendering/render_chemaxon_smiles.py
@@ -0,0 +1,51 @@
+import errno
+import os
+import sys
+
+sys.path.append(
+    os.path.normpath(
+        os.path.join(os.path.abspath(__file__), "..", "..", "..", "common")
+    )
+)
+from env_indigo import Indigo, IndigoRenderer, isIronPython, joinPathPy  # noqa
+from rendering import checkImageSimilarity  # noqa
+
+indigo = Indigo()
+renderer = IndigoRenderer(indigo)
+out_dir = "out/chemaxon_smiles/"
+
+if not os.path.exists(joinPathPy(out_dir, __file__)):
+    try:
+        os.makedirs(joinPathPy(out_dir, __file__))
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+
+print("issue 425 smiles with attachment points")
+indigo.resetOptions()
+indigo.setOption("render-output-format", "png")
+
+smiles = {"O": "[O-]%91.[*:1]%91 |$;_AP1$|",
+          "S": "S%91C(C1C=CC=CC=1)C1C=CC=CC=1.[*:1]%91 |$;;;;;;;;;;;;;;_AP1$|",
+		  "N": "[NH3+]%91.[*:1]%91 |$;_AP1$|"}
+
+for key, value in smiles.items():
+    png_fname = "smiles_attachment_points_452_" + key + ".png"
+    mol = indigo.loadMolecule(value)
+    renderer.renderToFile(mol, joinPathPy(out_dir + png_fname, __file__))
+    print(checkImageSimilarity("chemaxon_smiles/" + png_fname))
+
+#png_fname = "smiles_attachment_points_452_S.png"
+#mol = indigo.loadMolecule("S%91C(C1C=CC=CC=1)C1C=CC=CC=1.[*:1]%91 |$;;;;;;;;;;;;;;_AP1$|")
+#renderer.renderToFile(mol, joinPathPy(out_dir + png_fname, __file__))
+#print(checkImageSimilarity("chemaxon_smiles/" + png_fname))
+#
+#png_fname = "smiles_attachment_points_452_N.png"
+#mol = indigo.loadMolecule("[NH3+]%91.[*:1]%91 |$;_AP1$|")
+#renderer.renderToFile(mol, joinPathPy(out_dir + png_fname, __file__))
+#print(checkImageSimilarity("chemaxon_smiles/" + png_fname))
+
+if isIronPython():
+    renderer.Dispose()
+    indigo.Dispose()
