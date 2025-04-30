df27a0c7d9e5f34b278d7c4db2f6a293138711ce
Roman Porozhnetov <roman.porozhnetov@gmail.com>
2025-04-30 17:51:22 +0200

    tests fix

--- utils/indigo-service/backend/service/tests/api/indigo_test.py
+++ utils/indigo-service/backend/service/tests/api/indigo_test.py
@@ -18,7 +18,7 @@ def joinPathPy(args, file_py):
 # @unittest.skip("Skip libraries test case")
 class IndigoTestCase(unittest.TestCase):
     def setUp(self):
-        service_url = "http://localhost:8080/v2"
+        service_url = "http://localhost/v2"
         if (
             "INDIGO_SERVICE_URL" in os.environ
             and len(os.environ["INDIGO_SERVICE_URL"]) > 0
