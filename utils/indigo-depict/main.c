f3b9c736f19b907384e209936fa6c2f4481fc47d
even1024 <roman.porozhnetov@gmail.com>
2025-05-18 16:02:12 +0200

    undefined calc

--- utils/indigo-depict/main.c
+++ utils/indigo-depict/main.c
@@ -957,7 +957,7 @@ int main(int argc, char* argv[])
         _prepare(obj, p.aromatization);
         if (p.action == ACTION_LAYOUT)
         {
-            indigoLayout(obj);
+            // indigoLayout(obj);
             if (p.out_ext == OEXT_MOL)
                 indigoSaveMolfileToFile(obj, p.outfile);
             else if (p.out_ext == OEXT_KET)
@@ -1060,7 +1060,13 @@ int main(int argc, char* argv[])
         _prepare(obj, p.aromatization);
         if (p.action == ACTION_LAYOUT)
         {
-            indigoLayout(obj);
+            auto mols = indigoIterateMolecules(obj);
+            while (indigoHasNext(mols))
+            {
+                int mol = indigoNext(mols);
+                printf("%d\n", mol);
+            }
+            //indigoLayout(obj);
             if (p.out_ext == OEXT_CML)
                 indigoSaveCmlToFile(obj, p.outfile);
             else if (p.out_ext == OEXT_RXN)
