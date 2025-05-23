5a2dbb24cb95bc33052abb7f48528fbbdfd63094
Martin Mullins <martin@uncountable.com>
2025-05-23 10:49:14 +0100

    only bump, no CI change

--- api/python/setup.py
+++ api/python/setup.py
@@ -97,8 +97,8 @@ if os.path.exists("epam.indigo.egg-info"):
     shutil.rmtree("epam.indigo.egg-info")
 
 setup(
-    name="epam_indigo",
-    version="1.34.0",
+    name="epam.indigo",
+    version="1.28.0.rc3-unc11",
     description="Indigo universal cheminformatics toolkit",
     author="EPAM Systems Life Science Department",
     author_email="lifescience.opensource@epam.com",
