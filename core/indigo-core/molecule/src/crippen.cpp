ddf4c146fe15b0dc914fc189c894ec9e22792645
Roman Porozhnetov <roman.porozhnetov@gmail.com>
2025-06-25 16:53:22 +0200

    commit fix

--- core/indigo-core/molecule/src/crippen.cpp
+++ core/indigo-core/molecule/src/crippen.cpp
@@ -370,7 +370,8 @@ namespace
             const int amino_index = 1427; // Index of the amino group decision tree
             const int thiol_index = 675;  // Index of the thiol group decision tree
             const int carboxyl_index = 0; // Index of the carboxyl group decision tree
-            std::vector<std::pair<std::string, int>> ionizing_groups = {{"[C;X3](=O)[O;H1]", carboxyl_index},{"[S;v2;H1]", thiol_index}, {"[N;H2;X3]", amino_index}};
+            std::vector<std::pair<std::string, int>> ionizing_groups = {
+                {"[C;X3](=O)[O;H1]", carboxyl_index}, {"[S;v2;H1]", thiol_index}, {"[N;H2;X3]", amino_index}};
 
             for (const auto& group : ionizing_groups)
             {
