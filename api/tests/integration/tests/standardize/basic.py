d8b2258d8775482b8e63e5e15db1d2b9953fe40a
Joshua Black <j.black@mestrelab.com>
2025-08-22 10:46:19 +0200

    #3135 fix tests for java

--- api/tests/integration/tests/standardize/basic.py
+++ api/tests/integration/tests/standardize/basic.py
@@ -432,26 +432,32 @@ mol = indigo.loadMoleculeFromFile(
     joinPathPy("molecules/3135_mark_undefined_stereo1.mol", __file__)
 )
 print(mol.smiles())
-print("checkStereo():", mol.checkStereo())
+print("checkStereo(): " + str(mol.checkStereo()))
 mol.standardize()
 print(mol.smiles())
-print("checkeStereo()", mol.checkStereo())
-print("stereoceneters:", mol.countStereocenters())
+print("checkeStereo() " + str(mol.checkStereo()))
+print("stereoceneters: " + str(mol.countStereocenters()))
 for atom in mol.iterateStereocenters():
     print(
-        "atom", atom.index(), "-- stereocenter type", atom.stereocenterType()
+        "atom "
+        + str(atom.index())
+        + " -- stereocenter type "
+        + str(atom.stereocenterType())
     )
 mol = indigo.loadMoleculeFromFile(
     joinPathPy("molecules/3135_mark_undefined_stereo2.mol", __file__)
 )
 print(mol.smiles())
-print("checkStereo():", mol.checkStereo())
+print("checkStereo(): " + str(mol.checkStereo()))
 mol.standardize()
 print(mol.smiles())
-print("checkeStereo()", mol.checkStereo())
-print("stereoceneters:", mol.countStereocenters())
+print("checkeStereo() " + str(mol.checkStereo()))
+print("stereoceneters: " + str(mol.countStereocenters()))
 for atom in mol.iterateStereocenters():
     print(
-        "atom", atom.index(), "-- stereocenter type", atom.stereocenterType()
+        "atom "
+        + str(atom.index())
+        + " -- stereocenter type "
+        + str(atom.stereocenterType())
     )
 indigo.setOption("standardize-stereo-mark-undefined", "false")
