7e7fbb42b51fc2d9c296089b208f821b871e6c58
Roman Porozhnetov <roman.porozhnetov@gmail.com>
2025-09-19 03:06:13 +0200

    explicit val

--- api/tests/integration/tests/formats/ket_to_mol.py
+++ api/tests/integration/tests/formats/ket_to_mol.py
@@ -78,6 +78,7 @@ files = [
     ("flip_rotate_2000", "2000"),
     ("flip_rotate_rna", "3000"),
     ("3068-star-issue", "3000"),
+    ("exp-valence-0", "2000"),
 ]
 
 files.sort(key=lambda x: x[0])
