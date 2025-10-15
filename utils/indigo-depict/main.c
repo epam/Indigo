0328e3a0ce22a156ca40470b53dae513f5315405
even1024 <roman.porozhnetov@gmail.com>
2025-10-15 11:39:33 +0200

    step 10

--- utils/indigo-depict/main.c
+++ utils/indigo-depict/main.c
@@ -925,10 +925,8 @@ int main(int argc, char* argv[])
     // read in the input
     reader = (p.file_to_load != NULL) ? indigoReadFile(p.file_to_load) : indigoReadString(p.string_to_load);
 
-    // int lib = indigoLoadMonomerLibraryFromString("{\"root\":{}}");
-    int lib = indigoLoadMonomerLibraryFromFile("monomer_library.ket");
-    // int lib = indigoLoadMonomerLibraryFromFile("phos.sdf");
-    // indigoSaveMonomerLibraryToFile("phos.ket", lib);
+    int lib = indigoLoadMonomerLibraryFromString("{\"root\":{}}");
+    // int lib = indigoLoadMonomerLibraryFromFile("monomer_library.ket");
 
     if (p.mode == MODE_SINGLE_MOLECULE)
     {
