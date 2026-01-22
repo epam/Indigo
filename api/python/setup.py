923c4b2d49ada82ed7ced98b1ff075327925f2b1
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2026-01-22 17:42:14 +0300

    Bump to version 1.38.0
    [no ci]

--- api/python/setup.py
+++ api/python/setup.py
@@ -98,7 +98,7 @@ if os.path.exists("epam.indigo.egg-info"):
 
 setup(
     name="epam_indigo",
-    version="1.38.0.rc3",
+    version="1.38.0",
     description="Indigo universal cheminformatics toolkit",
     author="EPAM Systems Life Science Department",
     author_email="lifescience.opensource@epam.com",
