716b03c3a2cefdcaab427c2cb9087a7f17eb3f8a
Aliakasndr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2026-02-06 22:48:57 +0300

    fix typo

--- core/indigo-core/molecule/meta_commons.h
+++ core/indigo-core/molecule/meta_commons.h
@@ -659,7 +659,7 @@ namespace indigo
         };
 
         ReactionArrowObject(int arrow_type, const Vec2f& begin, const Vec2f& end, float height = 0)
-            : MetaObject(CID), _arrow_type(arrow_type), _begin(begin), _end(end), _height(height) {};
+            : MetaObject(CID), _arrow_type(arrow_type), _begin(begin), _end(end), _height(height){};
 
         MetaObject* clone() const override
         {
@@ -799,7 +799,7 @@ namespace indigo
     {
     public:
         static const std::uint32_t CID = "Reaction plus object"_hash;
-        ReactionPlusObject(const Vec2f& pos) : MetaObject(CID), _pos(pos) {};
+        ReactionPlusObject(const Vec2f& pos) : MetaObject(CID), _pos(pos){};
 
         MetaObject* clone() const override
         {
@@ -883,8 +883,8 @@ namespace indigo
 
     struct MolSumm
     {
-        MolSumm() : bbox(Vec2f(0, 0), Vec2f(0, 0)), role(BaseReaction::UNDEFINED), reaction_idx(-1) {};
-        MolSumm(const Rect2f& box) : bbox(box), role(BaseReaction::UNDEFINED), reaction_idx(-1) {};
+        MolSumm() : bbox(Vec2f(0, 0), Vec2f(0, 0)), role(BaseReaction::UNDEFINED), reaction_idx(-1){};
+        MolSumm(const Rect2f& box) : bbox(box), role(BaseReaction::UNDEFINED), reaction_idx(-1){};
 
         Rect2f bbox;
         std::vector<int> indexes;
@@ -937,7 +937,7 @@ namespace indigo
         };
 
         ReactionComponent(int ctype, const Rect2f& box, int idx, std::unique_ptr<BaseMolecule> mol)
-            : component_type(ctype), bbox(box), molecule(std::move(mol)), summ_block_idx(NOT_CONNECTED), index(idx) {};
+            : component_type(ctype), bbox(box), molecule(std::move(mol)), summ_block_idx(NOT_CONNECTED), index(idx){};
 
         int component_type;
         Rect2f bbox;
