b5fb2d3cc0e0a582fa7711aff4199458a00b738b
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2026-02-19 13:12:15 +0300

    Fix elasticsearch UT

--- third_party/libpng/include/pngpriv.h
+++ third_party/libpng/include/pngpriv.h
@@ -524,7 +524,8 @@
     * <fp.h> if possible.
     */
 #    if !defined(__MATH_H__) && !defined(__MATH_H) && !defined(__cmath__)
-#      include <fp.h>
+#    include <math.h>
+// #      include <fp.h>
 #    endif
 #  else
 #    include <math.h>
