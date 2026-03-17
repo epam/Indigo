c2daf33f79a8e63f6488915f0be70609d05454e9
Aliaksandr Dziarkach <18146690+AliaksandrDziarkach@users.noreply.github.com>
2026-03-17 16:21:39 +0300

    Bump to version 1.42.0-rc.3
    [no ci]

--- api/c/tests/unit/tests/bingo_nosql.cpp
+++ api/c/tests/unit/tests/bingo_nosql.cpp
@@ -33,10 +33,67 @@ class BingoNosqlTest : public IndigoApiTest
 {
 };
 
+TEST_F(BingoNosqlTest, test_search_sim)
+{
+    /*
+    int db = bingoCreateDatabaseFile("molecules", "molecule", "");
+    int iterator = indigoIterateSDFile("bingo_mols.sdf");
+    for (int mol = indigoNext(iterator); mol != 0; mol = indigoNext(iterator))
+    {
+        try
+        {
+            bingoInsertRecordObj(db, mol);
+        }
+        catch (...)
+        {
+        }
+    }
+    bingoCloseDatabase(db); */
+    int db = bingoLoadDatabaseFile("molecules", "");
+    int qiterator = indigoIterateSDFile("rand_queries_small.sdf");
+    int count = 0;
+    for (int mol = indigoNext(qiterator); mol != 0; mol = indigoNext(qiterator))
+    {
+        const char* smiles = indigoSmiles(mol);
+        int qmol = indigoLoadQueryMoleculeFromString(indigoRawData(mol));
+        int res = bingoSearchSub(db, qmol, "");
+        res = bingoSearchSim(db, mol, 0.9f, 1.0f, "tanimoto");
+        res = bingoSearchSim(db, mol, 0.9f, 1.0f, "tversky 0.3 0.7");
+        res = bingoSearchSim(db, mol, 0.9f, 1.0f, "euclid-sub");
+        printf("count = %d\n", count);
+        count++;
+    }
+}
+
+TEST_F(BingoNosqlTest, test_subsearch_tau)
+{
+    indigoClearTautomerRules();
+    indigoSetTautomerRule(1, "N,O,P,S,As,Se,Sb,Te", "N,O,P,S,As,Se,Sb,Te");
+    indigoSetTautomerRule(2, "0C", "N,O,P,S");
+    indigoSetTautomerRule(3, "1C", "N,O");
+    int db = bingoLoadDatabaseFile("epam_structures50_1", "");
+    // int query = indigoLoadQueryMoleculeFromFile("epam.mol");
+    int query = indigoLoadQueryMoleculeFromString("[Si-2](F)(F)(F)(F)F");
+    // int sub_matcher = bingoSearchSub(db, query, "TAU INNER R*");
+    int sub_matcher = bingoSearchSim(db, query, 0.9, 1, "tanimoto");
+    int count = 0;
+    int res = bingoGetObject(sub_matcher);
+    while (bingoNext(sub_matcher))
+    {
+        count++;
+        int id = bingoGetCurrentId(sub_matcher);
+        printf("%d: id=%d %s\n", count, id, indigoSmiles(res));
+    }
+
+    bingoEndSearch(sub_matcher);
+    bingoCloseDatabase(db);
+    printf("%s", bingoProfilingGetStatistics(db));
+}
+
 TEST_F(BingoNosqlTest, test_enumerate_id)
 {
     int db = bingoCreateDatabaseFile(::testing::UnitTest::GetInstance()->current_test_info()->name(), "molecule", "");
-    int obj = indigoLoadMoleculeFromString("C1CCNCC1");
+    int obj = indigoLoadQueryMoleculeFromString("C1CCNCC1");
 
     // checking next() of empty enumerator
     int e_empty = bingoEnumerateId(db);
