c53405e1312976557a7f1ec3832af858f211c5f7
Nikolai Balabanov <Nikolai_Balabanov@epam.com>
2026-05-05 23:07:54 +0200

    Issue #3333: Fix clang format issue.

--- core/indigo-core/molecule/elements.h
+++ core/indigo-core/molecule/elements.h
@@ -169,8 +169,7 @@ namespace indigo
         return el == ELEM_O || el == ELEM_F || el == ELEM_S || el == ELEM_Cl || el == ELEM_Se || el == ELEM_Br || el == ELEM_I;
     }
 
-    [[deprecated("Misspelled, use ElementHydrogenOnLeft instead")]]
-    inline bool ElementHygrodenOnLeft(int el)
+    [[deprecated("Misspelled, use ElementHydrogenOnLeft instead")]] inline bool ElementHygrodenOnLeft(int el)
     {
         return ElementHydrogenOnLeft(el);
     }
