9a42f9ef388cd7788afc86797d11002056a69b66
Roman Porozhnetov <roman.porozhnetov@gmail.com>
2026-05-31 12:55:13 +0400

    Fix Jython KET test order

--- api/tests/integration/tests/formats/smiles_to_ket.py
+++ api/tests/integration/tests/formats/smiles_to_ket.py
@@ -16,19 +16,21 @@ print("*** SMILES to KET ***")
 
 ref_path = joinPathPy("ref/", __file__)
 
-tests = {
-    "sgroup_data_all_fields": (
+tests = [
+    (
+        "sgroup_data_all_fields",
         "CCCCC |SgD:1,2,3:"
         "name&#44;&#59;&#58;&#124;&#123;&#125;&#36;field:"
         "data&#44;&#59;&#58;&#124;&#123;&#125;&#36;value:"
         "query&#44;&#59;&#58;&#124;&#123;&#125;&#36;op:"
         "unit&#44;&#59;&#58;&#124;&#123;&#125;&#36;text: :|"
     ),
-    "sgroup_sru_subscript": (
-        "CCCCC |Sg:n:1,2,3:sub&#44;&#59;&#58;&#124;&#123;&#125;&#36;script:hh|"
+    (
+        "sgroup_sru_subscript",
+        "CCCCC |Sg:n:1,2,3:sub&#44;&#59;&#58;&#124;&#123;&#125;&#36;script:hh|",
     ),
-}
+]
 
-for filename, smiles in tests.items():
+for filename, smiles in tests:
     ket = indigo.loadMolecule(smiles).json()
     compare_diff(ref_path, filename + ".ket", ket)
